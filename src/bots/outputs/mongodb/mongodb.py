5c294afd78d967adff7a059bdb4d4b0b99293747
Tomás Lima <synchroack@gmail.com>
2014-06-24 15:04:18 +0100

    mongodb bot
    Former-commit-id: ef24878a400d857c3dbf237f31df419b5ff8ef46

---
+++ src/bots/outputs/mongodb/mongodb.py
@@ -0,0 +1,27 @@
+import sys
+import pymongo
+from lib.bot import *
+from lib.utils import *
+from lib.event import *
+from lib.cache import *
+
+class MongoDBBot(Bot):
+
+    def __init__(self, bot_id):
+        super(MongoDBBot, self).__init__(bot_id)
+        client = pymongo.MongoClient(self.parameters.host, int(self.parameters.port))
+        db = client[self.parameters.database]
+        self.collection = db[self.parameters.collection]
+        
+
+    def process(self):
+        event = self.pipeline.receive()
+        
+        if event:
+            self.collection.insert(event.to_dict())
+        self.pipeline.acknowledge()
+
+
+if __name__ == "__main__":
+    bot = MongoDBBot(sys.argv[1])
+    bot.start()
