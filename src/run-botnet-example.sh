c3c62a1774cbc8432904f6d3cf6bb8e17e30cd98
Tomás Lima <synchroack@gmail.com>
2014-06-25 20:28:06 +0100

    minimal cleaning and fixing
    Former-commit-id: 7f419edba64e793fd03ad15689bae58bd56dac7c

--- src/run-botnet-example.sh
+++ src/run-botnet-example.sh
@@ -2,13 +2,19 @@
 python -m bots.inputs.arbor.feed arbor-feed &
 python -m bots.inputs.arbor.parser arbor-parser &
 python -m bots.inputs.arbor.harmonizer arbor-harmonizer &
+python -m bots.inputs.vxvault.feed vxvault-feed &
+python -m bots.inputs.vxvault.parser vxvault-parser &
+python -m bots.inputs.vxvault.harmonizer vxvault-harmonizer &
+python -m bots.inputs.malwaredomainlist.feed malwaredomainlist-feed &
+python -m bots.inputs.malwaredomainlist.parser malwaredomainlist-parser &
+python -m bots.inputs.malwaredomainlist.harmonizer malwaredomainlist-harmonizer &
 python -m bots.experts.deduplicator.deduplicator deduplicator &
 python -m bots.experts.sanitizer.sanitizer sanitizer &
 python -m bots.experts.taxonomy.taxonomy taxonomy-expert &
 python -m bots.experts.geoip.geoip geoip-expert &
 python -m bots.experts.cymru.cymru cymru-expert &
 python -m bots.outputs.file.file file-output &
-python -m bots.outputs.mongodb.mongodb mongodb &
+#python -m bots.outputs.mongodb.mongodb mongodb &
 
 echo -en "\ec"
 echo ""
