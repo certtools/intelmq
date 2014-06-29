7b27b4ef2bff07e7643be8e5b42936a56346929f
Tomás Lima <synchroack@gmail.com>
2014-06-29 05:04:04 +0100

    fixing code and detect errors
    Former-commit-id: ff7bd14e1a388fdbbfa2da83fd19ad088723f0fb

--- src/bots/inputs/arbor/feed.py
+++ src/bots/inputs/arbor/feed.py
@@ -9,7 +9,7 @@ class ArborFeedBot(Bot):
     def process(self):
         url = "http://atlas-public.ec2.arbor.net/public/ssh_attackers"
         report = fetch_url(url, timeout = 60.0, chunk_size = 16384)
-        self.send_message(force_decode(report))
+        self.send_message(report)
 
 
 if __name__ == "__main__":
