e3f7cc85c0ea1c300f07b207dc7b9cad949bec7a
Tomás Lima <synchroack@gmail.com>
2014-07-08 10:53:56 +0100

    Update deduplicator.py

--- src/bots/experts/deduplicator/deduplicator.py
+++ src/bots/experts/deduplicator/deduplicator.py
@@ -21,10 +21,18 @@ class DeduplicatorBot(Bot):
 
         if message:
             message_hash = hash(message)
+            
+            try:
+                event = Event.from_unicode(message)
+                event.clear("observation_time")
+                message_hash = hash(event)                            
+            except:
+                message_hash = hash(message)            
 
             if not self.cache.exists(message_hash):
                 self.send_message(message)
                 self.cache.set(message_hash, 'hash')
+                
         self.acknowledge_message()
 
 
