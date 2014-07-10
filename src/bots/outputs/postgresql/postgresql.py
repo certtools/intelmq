03c57d5265a5eea03a6b42af5e2be807e71a17d7
Tomás Lima <synchroack@gmail.com>
2014-07-10 18:17:04 +0100

    Update postgresql.py
    Former-commit-id: a8528f514d0a13130108ae041d29b015d14de7ba

--- src/bots/outputs/postgresql/postgresql.py
+++ src/bots/outputs/postgresql/postgresql.py
@@ -7,8 +7,8 @@ from lib.event import *
 class PostgreSQLBot(Bot):
 
     def init(self):
-        con = None
-        try:    
+        try:
+            self.logger.debug("Connecting to PostgreSQL")
             self.con = psycopg2.connect(
                                    database=self.parameters.database,
                                    user=self.parameters.user,
@@ -16,28 +16,30 @@ class PostgreSQLBot(Bot):
                                    host=self.parameters.host,
                                    port=self.parameters.port
                                   )
-            self.logger.info("info: con = %r" %self.con)
+            self.cur = self.con.cursor()
+            self.logger.info("Connected to PostgreSQL")
 
         except psycopg2.DatabaseError, e:
-            self.logger.error("Postgresql Problem. Could not connect to the database. Error: %s " % e.pgerror)
+            self.logger.error("Problem found when bot tried to connect to PostgreSQL. Error: %s " % e.pgerror)
             self.stop()
-        self.cur = self.con.cursor() 
 
 
     def process(self):
         event = self.receive_message()
-        
         if event:
+            
             evdict = event.to_dict2()  # FIXME: rename the method or use to_dict()
             KEYS = ", ".join(evdict.keys())
             VALUES = evdict.values()
             FVALUES = len(VALUES) * "%s, "
             QUERY = "INSERT INTO logentry (" + KEYS + ") VALUES (" + FVALUES[:-2] + ")"
+            
             try:
                 self.cur.execute(QUERY, VALUES)
             except psycopg2.DatabaseError, e:
-                # FIXME: try to use the try:except from start method at lib/bot.py
-                self.logger.error("Postgresql Problem. Could not INSERT. Error: %s " % e.pgerror)
+                self.logger.error("Problem found when bot tried to insert data into PostgreSQL. Error: %s " % e.pgerror)
+                self.stop()
+                
             self.con.commit()
         self.acknowledge_message()
 
