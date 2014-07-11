d8d326ce3ca64a24ae618e453d30eb496eb91478
Tomás Lima <synchroack@gmail.com>
2014-07-11 11:16:48 +0100

    Update postgresql.py
    Former-commit-id: 2512c40dcb6106b5f6762cd131c00ebea3eca0e4

--- src/bots/outputs/postgresql/postgresql.py
+++ src/bots/outputs/postgresql/postgresql.py
@@ -28,16 +28,16 @@ class PostgreSQLBot(Bot):
         event = self.receive_message()
         if event:
             
-            evdict = event.to_dict2()  # FIXME: rename the method or use to_dict()
-            KEYS = ", ".join(evdict.keys())
-            VALUES = evdict.values()
-            FVALUES = len(VALUES) * "%s, "
-            QUERY = "INSERT INTO logentry (" + KEYS + ") VALUES (" + FVALUES[:-2] + ")"
+            evdict  = event.to_dict()
+            keys    = ", ".join(evdict.keys())
+            values  = evdict.values()
+            fvalues = len(VALUES) * "%s, "
+            query   = "INSERT INTO logentry (" + keys + ") VALUES (" + fvalues[:-2] + ")"
             
             try:
-                self.cur.execute(QUERY, VALUES)
+                self.cur.execute(query, values)
             except psycopg2.DatabaseError, e:
-                self.logger.error("Problem found when bot tried to insert data into PostgreSQL. Error: %s " % e.pgerror)
+                self.logger.error("Problem found when bot tried to insert data into PostgreSQL (%s)." % e)
                 self.stop()
                 
             self.con.commit()
