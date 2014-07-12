156082d339ebc7b7410c58791027e7af15318bc8
root <root@server.local>
2014-07-12 18:37:48 +0100

    improve RabbitMQ timeouts and errors handling
    Former-commit-id: 3b8b3ca3a8bf476c37965e77adc9462fc7c2f8cf

--- src/lib/pipeline.py
+++ src/lib/pipeline.py
@@ -5,19 +5,37 @@ class Pipeline():
     def __init__(self, source_queue, destination_queues, host=None, port=None):
         self.source_queue = source_queue
         self.destination_queues = destination_queues
+
+        # Documentation: http://pika.readthedocs.org/en/latest/modules/parameters.html
+        self.pika_parameters = pika.ConnectionParameters(
+                                host = None,
+                                port = None,
+                                virtual_host = None,
+                                credentials = None,
+                                channel_max = None,
+                                frame_max = None,
+                                heartbeat_interval = None,
+                                ssl = None,
+                                ssl_options = None,
+                                connection_attempts = None,
+                                retry_delay = None,
+                                socket_timeout = None,
+                                locale = None,
+                                backpressure_detection = None
+                               )        
         self.connect()
 
 
     def connect(self):
         if self.source_queue:
-            self.source_connection = pika.BlockingConnection()
+            self.source_connection = pika.BlockingConnection(self.pika_parameters)
             self.source_channel = self.source_connection.channel()
             self.source_channel.queue_declare(queue=self.source_queue, durable=True)
             self.source_generator = self.source_channel.consume(self.source_queue)
 
         if self.destination_queues:
             self.destination_exchange = '-'.join(self.destination_queues)
-            self.destination_connection = pika.BlockingConnection()
+            self.destination_connection = pika.BlockingConnection(self.pika_parameters)
             self.destination_channel = self.destination_connection.channel()
             self.destination_channel.exchange_declare(exchange=self.destination_exchange, type='fanout')
 
@@ -29,6 +47,23 @@ class Pipeline():
                 self.destination_channel.queue_bind(exchange=self.destination_exchange, queue=destination_queue)
 
 
+
+    def disconnect(self):
+        if hasattr(self, 'source_connection'):
+            self.source_connection.close(reply_code=200, reply_text='Disconnecting from source queue')
+        if hasattr(self, 'destination_connection'):
+            self.destination_connection.close(reply_code=200, reply_text='Disconnecting from destination queue')
+
+
+
+    def sleep(self, interval):
+        if hasattr(self, 'source_connection'):
+            self.source_connection.sleep(interval)
+            return
+        if hasattr(self, 'destination_connection'):
+            self.destination_connection.sleep(interval)
+            return
+        
 
     # Send a message to queue
 
