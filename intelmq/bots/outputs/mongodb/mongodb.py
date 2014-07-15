9b0677b3c2a3421cd0799193d303abe2a4892fcc
Tomás Lima <synchroack@gmail.com>
2014-07-15 13:42:29 +0100

    beta release
    Former-commit-id: fa4a004538799db9ac90eb1247a6e99e254e62f3

---
+++ intelmq/bots/outputs/mongodb/mongodb.py
@@ -0,0 +1,26 @@
+import sys
+import pymongo
+from intelmq.lib.bot import *
+from intelmq.lib.utils import *
+from intelmq.lib.event import *
+from intelmq.lib.cache import *
+
+class MongoDBBot(Bot):
+
+    def init(self):
+        client = pymongo.MongoClient(self.parameters.host, int(self.parameters.port))
+        db = client[self.parameters.database]
+        self.collection = db[self.parameters.collection]
+        
+
+    def process(self):
+        event = self.receive_message()
+        
+        if event:
+            self.collection.insert(event.to_dict())
+        self.acknowledge_message()
+
+
+if __name__ == "__main__":
+    bot = MongoDBBot(sys.argv[1])
+    bot.start()
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='IntelMQ',
+    version='0.1.0',
+    maintainer='Tomas Lima',
+    maintainer_email='tomas.lima@cert.pt',
+    packages=[
+                  'intelmq',
+                  'intelmq.lib',
+                  'intelmq.bots',
+                  'intelmq.bots.inputs',
+                  'intelmq.bots.inputs.arbor',
+                  'intelmq.bots.inputs.abusehelper',
+    ],
+    scripts=['bin/intelmqctl'],
+    url='http://pypi.python.org/pypi/IntelMQ/',
+    license='MPL v2.0',
+    description="IntelMQ Tool",
+    long_description='IntelMQ is a solution for CERTs to process data feeds, pastebins, tweets throught a message queue.',
+    package_data={'': ['LICENSE', 'README.md', 'CONTRIBUTORS.md']},
+    install_requires=[
+        "pika==0.9.14",
+        "python-dateutil==1.5",
+        "geoip2==0.5.1",
+        "dnspython==1.11.1",
+        "redis==2.4.9",
+        "pymongo==2.7.1",
+        "xmpppy==0.5.0rc1",
+    ],
+)
