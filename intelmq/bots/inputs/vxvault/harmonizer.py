fa4a004538799db9ac90eb1247a6e99e254e62f3
Tomás Lima <synchroack@gmail.com>
2014-07-15 13:42:29 +0100

    beta release

---
+++ intelmq/bots/inputs/vxvault/harmonizer.py
@@ -0,0 +1,26 @@
+import sys
+from intelmq.lib.bot import *
+from intelmq.lib.utils import *
+from intelmq.lib.event import *
+from intelmq.lib.cache import *
+
+class VXVaultHarmonizerBot(Bot):
+
+    def process(self):
+        event = self.receive_message()
+        
+        if event:
+            event.add('feed', 'vxvault')
+            event.add('feed_url', 'http://vxvault.siri-urz.net/URL_List.php')
+            ip_value = event.value('ip')
+            event.add('source_ip', ip_value)
+            event.add('reported_ip', ip_value)
+            event.add('type', 'malware')
+
+            self.send_message(event)
+        self.acknowledge_message()
+
+
+if __name__ == "__main__":
+    bot = VXVaultHarmonizerBot(sys.argv[1])
+    bot.start()
