783d5c4d370bbd7cf087d8ef31415950ea1b64c3
Tomás Lima <synchroack@gmail.com>
2014-07-15 15:09:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,14 +13,25 @@ setup(
     maintainer_email='tomas.lima@cert.pt',
     packages=find_packages(),
     scripts=['bin/intelmqctl'],
-    url='http://pypi.python.org/pypi/IntelMQ/',
-    license='MPL v2.0',
+    url='http://pypi.python.org/pypi/intelmq/',
+    license='GPLv3',
     description="IntelMQ Tool",
     long_description='IntelMQ is a solution for CERTs to process data feeds, pastebins, tweets throught a message queue.',
     package_data={
-                  '': ['LICENSE', 'README.md', 'CONTRIBUTORS.md']
-    },
-    data_files=[('/etc/intelmq/', ['bin/conf/pipeline.conf.default', 'bin/conf/bots.conf.default', 'bin/conf/system.conf'])],
+                    '': [
+                            'LICENSE',
+                            'README.md',
+                            'CONTRIBUTORS.md'
+                        ]
+                 },
+    data_files=[
+                ('/etc/intelmq/', [
+                                   'bin/conf/pipeline.conf.default',
+                                   'bin/conf/bots.conf.default',
+                                   'bin/conf/system.conf'
+                                  ]
+                )
+    ],
     install_requires=[
         "pika==0.9.14",
         "python-dateutil==1.5",
