834da12c224778360bb90aa4ee26360a8d0d2404
Dalila Lima <dcrypt3d@gmail.com>
2014-07-25 00:36:03 +0100

    OpenBL input bot
    Former-commit-id: 62d488e7efe0c4b821396e2839459189b27462ea

---
+++ intelmq/bots/inputs/openbl/parser.py
@@ -0,0 +1,32 @@
+from intelmq.lib.bot import Bot, sys
+from intelmq.lib.utils import force_decode
+from intelmq.lib.event import Event
+
+class OpenBLParserBot(Bot):
+
+    def process(self):
+        report = self.receive_message()
+
+        if report:
+            for row in report.split('\n'):
+                row = row.strip()              
+
+                if len(row) == 0 or row.startswith('#'): # ignore all lines starting with comment mark
+                    continue
+                
+                row = force_decode(row).split()
+                event = Event()
+
+                columns = ["reported_ip", "source_time"]
+                for key, value in zip(columns, row):
+                    event.add(key, value.strip())
+
+                self.send_message(event)
+
+        self.acknowledge_message()
+
+
+if __name__ == "__main__":
+    bot = OpenBLParserBot(sys.argv[1])
+    bot.start()
+
