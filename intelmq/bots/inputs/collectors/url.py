4b3c7bd5fbfbd43d8643515409c0b60259da245f
Tomás Lima <synchroack@gmail.com>
2014-08-06 03:56:58 +0100

    changing arch
    Former-commit-id: 793e1d210f5f351aa9e1a4c875b1fca401cbf4ad

--- intelmq/bots/inputs/collectors/url.py
+++ intelmq/bots/inputs/collectors/url.py
@@ -1,1 +1,13 @@
- 
+from intelmq.lib.bot import Bot, sys
+from intelmq.lib.utils import fetch_url
+
+class URLCollectorBot(Bot):
+
+    def process(self):
+        report = fetch_url(self.parameters.url, timeout = 60.0, chunk_size = 16384)
+        self.send_message(report)
+
+
+if __name__ == "__main__":
+    bot = URLCollectorBot(sys.argv[1])
+    bot.start()
