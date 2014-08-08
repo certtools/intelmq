5c40d3b096c30883a4fe0ad11ee8d80c4f0a723a
Tomás Lima <synchroack@gmail.com>
2014-08-08 13:20:01 +0100

    ShadowServer Support - Drone Report
    Former-commit-id: 6ce05d199d15c2b0139257c651edc5639a467fb8

---
+++ intelmq/bots/collectors/mail/shadowserver.py
@@ -0,0 +1,54 @@
+# TODO: subject needs to be regex
+# TODO: attachments needs to be regex
+# TODO: accept multiple attachment names
+# TODO: add method to mark email as read
+
+from intelmq.lib.bot import Bot, sys
+from intelmq.bots.collectors.mail.lib import Mail
+import zipfile
+
+# Fields Required:
+# ---------------------
+# * mail_host - imap server
+# * mail_user - user of email account
+# * mail_password - password of email account
+# * mail_ssl - use secure connenctions (True/False)
+# * folder - email folder (e.g. Inbox.ShadowServer)
+# * unread - get just unread emails (True/False)
+# * sent_from - source email from message that was sent
+# * subject_regex - subject of the message
+# * attach_regex - name of the attach to fetch
+# * unzip - unzip attach (True/False)
+
+class ShadowServerCollectorBot(Bot):
+
+    def process(self):
+        additional_parameters = ["folder", "unread", "sent_from", "subject_regex", "attach_regex"]
+        
+        for param in additional_parameters:
+            if not hasattr(self.parameters, param):
+                setattr(self.parameters, param, None)
+
+        mailbox = imbox.Imbox(self.parameters.mail_host, self.parameters.mail_user, self.parameters.mail_password, bool(self.parameters.mail_ssl))
+        uid, message = mailbox.messages(folder=self.parameters.folder, unread=self.parameters.unread, sent_from=self.parameters.sent_from)
+
+        if uid:
+            if self.parameters.subject_regex and not self.parameters.subject_regex in message.subject: # Apply regex
+                return
+            
+            for attach in message.attachments:
+                if self.parameters.attach_regex in attach['filename']: # Apply regex
+                    
+                    zipped = zipfile.ZipFile(attach['content'])
+                    report_csv = zipped.read(zipped.namelist()[0])
+                    
+                    report_link = "# Feed: http://dl.shadowserver.org/CHANGEME" # URL (dl.shadowserver.org/) regex apllied to message.body['plain'] or message.body['html']
+                    report = report_link + report_csv
+                    
+                    self.send_message(report)
+                    mailbox.mark_seen(uid)
+
+
+if __name__ == "__main__":
+    bot = EmailAttachmentCollectorBot(sys.argv[1])
+    bot.start()
--- intelmq/lib/utils.py
+++ intelmq/lib/utils.py
@@ -51,43 +51,6 @@ def fetch_url(url, timeout=60.0, chunk_size=16384):
     return decode(value, force=True)
 
 
-def fetch_imap(server, username, password, ssl, folder, unread, sent_from, sent_to, subject, max_emails):
-    '''
-        Additional information: https://github.com/martinrusev/imbox
-    '''
-
-    emails = list()
-    
-    if not max_emails:
-        max_emails = 1
-
-    mailbox = imbox.Imbox(server, username, password, bool(ssl))
-
-    messages_folder = mailbox.messages(folder=folder, unread=unread, sent_from=sent_from, sent_to=sent_to)
-
-    counter = 0
-    for uid, message in messages_folder:
-        
-        if max_emails <= counter:
-            return emails
-
-        email = dict()
-    
-        if subject and not subject in message.subject:
-            continue
-        
-        emails.append(message)
-        counter += 1
-        
-        # TODO: add method to mark email as read
-        
-    return emails
-
-
-def fetch_pgp():
-    '''__placeholder'''
-
-
 def hashgen(data, func=hashlib.sha1):
     result = func()
     result.update(data)
@@ -177,6 +140,3 @@ def is_url(url, force_http = False):
 # FIXME: is not correct.
 def is_domain_name(domain_name):
     return not is_url(domain_name)
-
-
-
