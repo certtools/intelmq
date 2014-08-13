43f134a8de2966b61886ef4124c770bf481e7a19
Tomás Lima <synchroack@gmail.com>
2014-08-13 22:33:08 +0100

    Update utils.py

--- intelmq/lib/utils.py
+++ intelmq/lib/utils.py
@@ -41,21 +41,6 @@ def encode(text, encodings=["utf-8", "ascii"], force=False):
     raise Exception("Found a problem when encoding.")
 
 
-def fetch_url(url, timeout=60.0, chunk_size=16384):
-    req = urllib2.urlopen(url, timeout = timeout)
-    strio = StringIO.StringIO()
-    shutil.copyfileobj(req, strio, chunk_size)
-    value = strio.getvalue()
-    strio.close()
-    return decode(value, force=True)
-
-
-def hashgen(data, func=hashlib.sha1):
-    result = func()
-    result.update(data)
-    return result.hexdigest()
-
-
 def log(logs_path, name, loglevel="DEBUG"):
     logger = logging.getLogger(name)
     logger.setLevel(loglevel)
@@ -70,11 +55,17 @@ def log(logs_path, name, loglevel="DEBUG"):
     return logger  
 
 
-def post_url(url, data, timeout=60.0, chunk_size=16384):
-    encoded_data = urllib.urlencode(data)
-    req = urllib2.urlopen(url, encoded_data, timeout)
-    return req.read()
+def hashgen(data, func=hashlib.sha1):
+    result = func()
+    result.update(data)
+    return result.hexdigest()
 
+
+
+
+
+
+# ------ Bots Utils (intelmq.bots.utils) -----------------------------
 def is_ip(ip):
     if is_ipv4(ip):
         return True
@@ -90,14 +81,12 @@ def is_ipv4(ip):
     except socket.error:
         return False
 
-
 def is_ipv6(ip):
     try:
         socket.inet_pton(socket.AF_INET6, ip)
         return True
     except socket.error:
         return False
-    
 
 def ip_to_int(ip):
     try:
@@ -111,7 +100,6 @@ def ip_to_int(ip):
     ip_integer = int(binascii.hexlify(ip_integer), 16)
     return ip_integer    
 
-
 def reverse_ip(ip):
     import dns.reversename
     result = str(dns.reversename.from_address(ip))
@@ -119,7 +107,6 @@ def reverse_ip(ip):
     if not reverse:
         reverse = result.split('.ip6.arpa.')
     return reverse[0]
-
 
 def is_url(url, force_http = False):
     from urlparse import urlparse  
@@ -135,7 +122,23 @@ def is_url(url, force_http = False):
     
     return False
 
-
 # FIXME: is not correct.
 def is_domain_name(domain_name):
     return not is_url(domain_name)
+# -----------------------------------
+    
+    
+# ------ intelmq/bots/collectors/url/lib.py ---
+def post_url(url, data, timeout=60.0, chunk_size=16384):
+    encoded_data = urllib.urlencode(data)
+    req = urllib2.urlopen(url, encoded_data, timeout)
+    return req.read()
+    
+
+def fetch_url(url, timeout=60.0, chunk_size=16384):
+    req = urllib2.urlopen(url, timeout = timeout)
+    strio = StringIO.StringIO()
+    shutil.copyfileobj(req, strio, chunk_size)
+    value = strio.getvalue()
+    strio.close()
+    return decode(value, force=True)
