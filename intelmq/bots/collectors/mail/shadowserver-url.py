a7dc62d7fd0cad5af6c5248f198617edc7b117b5
Tomás Lima <synchroack@gmail.com>
2014-08-14 01:05:29 +0100

    ShadowServer URL Support
    Former-commit-id: bbd112d9058ab7ada725d93ca597b5fdb88f6cb0

---
+++ intelmq/bots/collectors/mail/shadowserver-url.py
@@ -0,0 +1,39 @@
+import re
+import imbox
+import zipfile
+from intelmq.lib.bot import Bot, sys
+from intelmq.bots.collectors.mail.lib import Mail
+from intelmq.bots.collectors.url.lib import fetch_url
+
+class ShadowServerCollectorBot(Bot):
+
+    def process(self):
+        additional_parameters = ["folder", "unread", "sent_from", "subject_regex", "url_regex"]
+        
+        for param in additional_parameters:
+            if not hasattr(self.parameters, param):
+                setattr(self.parameters, param, None)
+
+        mailbox = imbox.Imbox(self.parameters.mail_host, self.parameters.mail_user, self.parameters.mail_password, bool(self.parameters.mail_ssl))
+        emails = mailbox.messages(folder=self.parameters.folder, unread=self.parameters.unread, sent_from=self.parameters.sent_from)
+
+        if emails:
+            for uid, message in emails:
+                
+                for body in message.body['plain']:
+                    match = re.search(self.parameters.url_regex, body)
+                    if match:
+                        url = match.group()
+                        
+                        self.logger.info("Downloading report from %s" % url)
+                        report = fetch_url(url, timeout = 60.0, chunk_size = 16384)
+                        self.logger.info("Report downloaded.")
+                        
+                        self.send_message(report)
+                        
+                mailbox.mark_seen(uid)
+
+
+if __name__ == "__main__":
+    bot = ShadowServerCollectorBot(sys.argv[1])
+    bot.start()
