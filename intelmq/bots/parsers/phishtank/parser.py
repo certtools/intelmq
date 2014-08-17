04b34c2c51d3af7035c2d1012c90080c1b48743b
Tomás Lima <synchroack@gmail.com>
2014-08-17 14:59:53 +0100

    improvements
    Former-commit-id: 1d490d60d85537aefba4a0a311abf0a58001bc8b

--- intelmq/bots/parsers/phishtank/parser.py
+++ intelmq/bots/parsers/phishtank/parser.py
@@ -1,26 +1,36 @@
-import StringIO, csv
+import csv
+import StringIO
 from intelmq.lib.bot import Bot, sys
 from intelmq.lib.event import Event
+from intelmq.bots import utils
 
 class PhishTankParserBot(Bot):
 
     def process(self):
         report = self.receive_message()
-        report = report.strip()
 
         if report:
-            columns = ["__IGNORE__", "url", "description_url", "source_time", "__IGNORE__", "__IGNORE__", "__IGNORE__", "target"] # ignore (__IGNORE__) fields specific to the source and other irrelevant fields
+            report = report.strip()
+            
+            columns = ["__IGNORE__", "source_url", "description_url", "source_time", "__IGNORE__", "__IGNORE__", "__IGNORE__", "target"]
             rows = csv.DictReader(StringIO.StringIO(report), fieldnames = columns)
 
             for row in rows:
                 event = Event()
                 
                 for key, value in row.items():
-
                     if key is "__IGNORE__":
                         continue
-
+                    
                     event.add(key, value)
+                    
+                event.add('feed', 'phishtank')
+                event.add('type', 'phishing')
+                
+                event = utils.parse_source_time(event, "source_time")
+                event = utils.generate_observation_time(event, "observation_time")
+                event = utils.generate_reported_fields(event)
+                    
                 self.send_message(event)
                 
         self.acknowledge_message()
