fa7ea497e7d4ea5e39982c5068f8b0f19daadde5
Tomás Lima <synchroack@gmail.com>
2014-08-17 17:26:07 +0100

    fix openbl and phishtank bots

--- intelmq/bots/parsers/phishtank/parser.py
+++ intelmq/bots/parsers/phishtank/parser.py
@@ -1,38 +1,42 @@
-import csv
-import StringIO
+import unicodecsv
+from cStringIO import StringIO
 from intelmq.lib.bot import Bot, sys
 from intelmq.lib.event import Event
+from intelmq.lib.utils import encode
 from intelmq.bots import utils
 
 class PhishTankParserBot(Bot):
 
     def process(self):
         report = self.receive_message()
-
+        
         if report:
-            report = report.strip()
-            
-            columns = ["__IGNORE__", "source_url", "description_url", "source_time", "__IGNORE__", "__IGNORE__", "__IGNORE__", "target"]
-            rows = csv.DictReader(StringIO.StringIO(report), fieldnames = columns)
+            event = Event()
+            report = encode(report)
 
-            for row in rows:
-                event = Event()
+            columns = ["__IGNORE__", "source_url", "description_url", "source_time", "__IGNORE__", "__IGNORE__", "__IGNORE__", "target"]
+            
+            for row in unicodecsv.reader(StringIO(report), encoding='utf-8'):
+
+                if "phish_id" in row:
+                    continue
                 
-                for key, value in row.items():
-                    if key is "__IGNORE__":
+                for key, value in zip(columns, row):
+
+                    if key == "__IGNORE__":
                         continue
                     
-                    event.add(key, value)
-                    
+                    event.add(key, value.strip())
+                
                 event.add('feed', 'phishtank')
                 event.add('type', 'phishing')
-                
+
                 event = utils.parse_source_time(event, "source_time")
                 event = utils.generate_observation_time(event, "observation_time")
                 event = utils.generate_reported_fields(event)
                     
                 self.send_message(event)
-                
+             
         self.acknowledge_message()
 
 
