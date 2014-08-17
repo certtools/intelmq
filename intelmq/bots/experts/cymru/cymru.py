1a78bad97137f2167c4a6ef8f1dd9f4207e8e09b
Tomás Lima <synchroack@gmail.com>
2014-08-17 05:38:03 +0100

    improve sanitization feature

--- intelmq/bots/experts/cymru/cymru.py
+++ intelmq/bots/experts/cymru/cymru.py
@@ -24,63 +24,65 @@ class CymruExpertBot(Bot):
         if not event:
             self.acknowledge_message()
             return
-            
-        if not event.contains("ip"):
-            self.send_message(event)
-            self.acknowledge_message()
-            return
+
+        keys = ["source_%s", "destination_%s"]
         
-        ip = event.value("ip")
-
-        if is_ipv4(ip):
-            ip_version = 4
-            ip_integer = ip_to_int(ip)
-            cache_key = bin(ip_integer)[2 : MINIMUM_BGP_PREFIX_IPV4 + 2]
-
-        elif is_ipv6(ip):
-            ip_version = 6
-            ip_integer = ip_to_int(ip)
-            cache_key = bin(ip_integer)[2 : MINIMUM_BGP_PREFIX_IPV6 + 2]
-
-        else:
-            self.send_message(event)
-            self.acknowledge_message()
-            return
-
-
-        result_json = self.cache.get(cache_key)
-
-        if result_json:
-            result = json.loads(result_json)
-        else:
-            result = Cymru.query(ip, ip_version)
-            result_json = json.dumps(result)
-            self.cache.set(cache_key, result_json)
-        
-        event.clear('asn')
-        event.clear('bgp_prefix')
-        event.clear('registry')
-        event.clear('allocated')
-        event.clear('as_name')
-        event.clear('cymru_cc')
-
-        if "asn" in result:
-            event.add('asn',        result['asn'])
+        for key in keys:
+            ip = event.value(key % "ip")
             
-        if "bgp_prefix" in result:
-            event.add('bgp_prefix', result['bgp_prefix'])
+            if not ip:
+                self.send_message(event)
+                self.acknowledge_message()
+                return
+
+            elif is_ipv4(ip):
+                ip_version = 4
+                ip_integer = ip_to_int(ip)
+                cache_key = bin(ip_integer)[2 : MINIMUM_BGP_PREFIX_IPV4 + 2]
+
+            elif is_ipv6(ip):
+                ip_version = 6
+                ip_integer = ip_to_int(ip)
+                cache_key = bin(ip_integer)[2 : MINIMUM_BGP_PREFIX_IPV6 + 2]
+
+            else:
+                self.send_message(event)
+                self.acknowledge_message()
+                return
+
+
+            result_json = self.cache.get(cache_key)
+
+            if result_json:
+                result = json.loads(result_json)
+            else:
+                result = Cymru.query(ip, ip_version)
+                result_json = json.dumps(result)
+                self.cache.set(cache_key, result_json)
             
-        if "registry" in result:
-            event.add('registry',   result['registry'])
-            
-        if "allocated" in result:
-            event.add('allocated',  result['allocated'])
-            
-        if "as_name" in result:
-            event.add('as_name',    result['as_name'])
-            
-        if "cc" in result:
-            event.add('cymru_cc',   result['cc'])
+            if "asn" in result:
+                event.clear(key % 'asn')
+                event.add(key % 'asn',        result['asn'])
+                
+            if "bgp_prefix" in result:
+                event.clear(key % 'bgp_prefix')
+                event.add(key % 'bgp_prefix', result['bgp_prefix'])
+                
+            if "registry" in result:
+                event.clear(key % 'registry')
+                event.add(key % 'registry',   result['registry'])
+                
+            if "allocated" in result:
+                event.clear(key % 'allocated')
+                event.add(key % 'allocated',  result['allocated'])
+                
+            if "as_name" in result:
+                event.clear(key % 'as_name')
+                event.add(key % 'as_name',    result['as_name'])
+                
+            if "cc" in result:
+                event.clear(key % 'cymru_cc')
+                event.add(key % 'cymru_cc',   result['cc'])
 
         self.send_message(event)
         self.acknowledge_message()
