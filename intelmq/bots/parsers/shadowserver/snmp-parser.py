0c47ba2bf2c6ff3c314b3c9ead76599176209ad3
Tomás Lima <synchroack@gmail.com>
2014-08-20 16:18:22 +0100

    improve shadowserver parser bots

--- intelmq/bots/parsers/shadowserver/snmp-parser.py
+++ intelmq/bots/parsers/shadowserver/snmp-parser.py
@@ -4,39 +4,27 @@ from intelmq.lib.bot import Bot, sys
 from intelmq.lib.event import Event
 from intelmq.bots import utils
 
-class ShadowServerDroneParserBot(Bot):
+class ShadowServerSNMPParserBot(Bot):
 
     def process(self):
         report = self.receive_message()
 
         if report:
             report = report.strip()
-
+            
             columns = {
                 "timestamp": "source_time",
                 "ip": "source_ip",
-                "port": "source_port",
+                "protocol" : "transport_protocol",
+                "port" : "source_port",
+                "hostname": "source_reverse_dns",
+                "sysdesc" : "__TDB__",
+                "sysname" : "__TDB__",
                 "asn": "source_asn",
                 "geo": "source_cc",
-                "region": "source_region",
-                "city": "source_city",
-                "hostname": "source_reverse_dns",
-                "type": "__IGNORE__",
-                "infection": "malware",
-                "url": "__TBD__",
-                "agent": "__TBD__",
-                "cc": "destination_ip",
-                "cc_port": "destination_port",
-                "cc_asn": "destination_asn",
-                "cc_geo": "destination_cc",
-                "cc_dns": "destination_reverse_dns",
-                "count": "__TBD__",
-                "proxy": "__TBD__",
-                "application": "__TBD__",
-                "p0f_genre": "__TBD__",
-                "p0f_detail": "__TBD__",
-                "machine_name": "__TBD__",
-                "id": "__TBD__"
+                "region" : "source_region",
+                "city" : "source_city",
+                "version" : "__IGNORE__"
             }
             
             rows = csv.DictReader(StringIO.StringIO(report))
@@ -53,17 +41,15 @@ class ShadowServerDroneParserBot(Bot):
 
                     value = value.strip()
                     
-                    if key is "__IGNORE__" or key is "__TBD__":
+                    if key is "__IGNORE__" or key is "__TDB__":
                         continue
-                    
-                    if key is "malware":
-                        value = value.strip().lower()                    
                     
                     event.add(key, value)
             
-                event.add('feed', 'shadowserver-drone')
-                event.add('type', 'botnet drone')
-                
+                event.add('feed', 'shadowserver-snmp')
+                event.add('type', 'vulnerable service')
+                event.add('protocol', 'snmp')
+
                 event = utils.parse_source_time(event, "source_time")  
                 event = utils.generate_observation_time(event, "observation_time")
                 event = utils.generate_reported_fields(event)
@@ -73,5 +59,5 @@ class ShadowServerDroneParserBot(Bot):
    
 
 if __name__ == "__main__":
-    bot = ShadowServerDroneParserBot(sys.argv[1])
+    bot = ShadowServerSNMPParserBot(sys.argv[1])
     bot.start()
