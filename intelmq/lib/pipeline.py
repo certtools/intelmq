5e5f4ed24f8b41871ced3a128a53caae95c77927
Tomás Lima <synchroack@gmail.com>
2014-08-28 19:26:18 +0100

    Update pipeline.py
    Former-commit-id: 3d1011629e78fbb492c21b9731172bdd76c0322b

--- intelmq/lib/pipeline.py
+++ intelmq/lib/pipeline.py
@@ -1,94 +1,50 @@
-import pika
+import redis
+import time
 
 class Pipeline():
+    def __init__(self, source_queue, destination_queues, host="127.0.0.1", port="6379", db=2):
         
-    def __init__(self, source_queue, destination_queues, host=None, port=None):
+        if destination_queues and type(destination_queues) is not list:
+            destination_queues = destination_queues.split()
+        
         self.source_queue = source_queue
+        if source_queue:
+            self.internal_queue = source_queue + "-internal"
+            
         self.destination_queues = destination_queues
-
-        # Documentation: http://pika.readthedocs.org/en/latest/modules/parameters.html
-        self.pika_parameters = pika.ConnectionParameters(
-                                host = None,
-                                port = None,
-                                virtual_host = None,
-                                credentials = None,
-                                channel_max = None,
-                                frame_max = None,
-                                heartbeat_interval = None,
-                                ssl = None,
-                                ssl_options = None,
-                                connection_attempts = None,
-                                retry_delay = None,
-                                socket_timeout = None,
-                                locale = None,
-                                backpressure_detection = None
-                               )        
-        self.connect()
-
+        
+        self.redis = redis.Redis( host = host,
+                                  port = int(port),
+                                  db = db,
+                                  socket_timeout = 50000
+                                )
 
     def connect(self):
-        if self.source_queue:
-            self.source_connection = pika.BlockingConnection(self.pika_parameters)
-            self.source_channel = self.source_connection.channel()
-            self.source_channel.queue_declare(queue=self.source_queue, durable=True)
-            self.source_generator = self.source_channel.consume(self.source_queue)
-
-        if self.destination_queues:
-            self.destination_exchange = '-'.join(self.destination_queues)
-            self.destination_connection = pika.BlockingConnection(self.pika_parameters)
-            self.destination_channel = self.destination_connection.channel()
-            self.destination_channel.exchange_declare(exchange=self.destination_exchange, type='fanout')
-
-            if type(self.destination_queues) is not list:
-                self.destination_queues = self.destination_queues.split()
-                
-            for destination_queue in self.destination_queues:
-                self.destination_channel.queue_declare(queue=destination_queue, durable=True)
-                self.destination_channel.queue_bind(exchange=self.destination_exchange, queue=destination_queue)
-
-
-
+        pass
+    
     def disconnect(self):
-        if hasattr(self, 'source_connection'):
-            self.source_connection.close(reply_code=200, reply_text='Disconnecting from source queue')
-        if hasattr(self, 'destination_connection'):
-            self.destination_connection.close(reply_code=200, reply_text='Disconnecting from destination queue')
-
-
-
+        pass
+    
     def sleep(self, interval):
-        if hasattr(self, 'source_connection'):
-            self.source_connection.sleep(interval)
-            return
-        if hasattr(self, 'destination_connection'):
-            self.destination_connection.sleep(interval)
-            return
-        
-
-    # Send a message to queue
+        time.sleep(interval)
 
     def send(self, message):
-        if not hasattr(self, 'destination_channel'):
-            return
-            
-        self.destination_channel.basic_publish(exchange=self.destination_exchange, routing_key='', body=unicode(message))
-
-
-
-    # Get a message from queue without remove it from queue.
+        for destination_queue in self.destination_queues:
+            self.redis.rpush(destination_queue, message)
 
     def receive(self):
-        if not hasattr(self, 'source_generator'):
-            return
-
-        self.last_method_frame, self.last_properties, self.last_body = self.source_generator.next()
-        return self.last_body
-
-
-
-    # Get a message from queue and remove it from queue.
+        #return self.redis.brpoplpush(self.source_queue, self.internal_queue, 0)
+        return self.redis.lpop(self.source_queue)
         
     def acknowledge(self):
-        if not hasattr(self, 'source_channel'):
-            return
-        self.source_channel.basic_ack(self.last_method_frame.delivery_tag)
+        pass
+        #return self.redis.rpop(self.internal_queue)
+
+# Receive
+# B RPOP LPUSH  source_queue  ->  source_queue_internal
+
+# Send
+# LPUSH          object        ->  destination_queue
+
+# Acknowledge
+# RPOP           remove from 'source_queue_internal'
