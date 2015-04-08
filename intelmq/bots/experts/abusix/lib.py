e7f28e3a8131d320d926fdb215e41677902ba520
rsefr <robert.sefr@outlook.com>
2015-04-08 11:57:20 +0200

    new expert bot to resolve abuse contact from abusix, could substitute RIPENCC bot
    Former-commit-id: 4ba6d180c9dff2e13729e6f6367560414a78a0ac

---
+++ intelmq/bots/experts/abusix/lib.py
@@ -0,0 +1,23 @@
+import re
+#sudo pip install dnspython
+import dnspython 
+
+class Abusix():
+
+    @staticmethod
+    def query(ip):
+
+        ipbytes = ip.split('.')
+        if len(ipbytes) != 4:
+	    return None
+	
+	query = ipbytes[3] + '.' + ipbytes[2] + '.' + ipbytes[1] + '.' + ipbytes[0] + '.abuse-contacts.abusix.org'
+
+        try:
+            response = dns.resolver.query(query, 'TXT')
+            if len(response) >= 1 and re.match(r"[^@]+@[^@]+\.[^@]+", str(response[0])):
+                return str(response[0])                
+            else
+                return None
+        except:
+            return None
