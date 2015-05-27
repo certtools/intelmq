3d04287d5bfbb854e3d80ba21f26c131b0fa3db3
Josef P. Bernhart <github@phantasus.at>
2015-05-27 15:31:13 +0200

    Fix dcu-collector to pep8, mostly. Leaving out long lines
    Former-commit-id: 6a59b88602c0b6347979d6d4c4577ba20cc0b752

--- intelmq/bots/collectors/dcu/collector.py
+++ intelmq/bots/collectors/dcu/collector.py
@@ -12,9 +12,9 @@ import StringIO
 
 class DCUCollectorBot(Bot):
     # This IntelMQ collector is for getting a blob
-    # from an azure account (Microsoft dcu). 
+    # from an azure account (Microsoft dcu).
     # It opens the account and
-    # reads all containers, which aren't ignored. 
+    # reads all containers, which aren't ignored.
 
     def process(self):
         account_name = getattr(self.parameters, "azure_account_name")
@@ -22,7 +22,7 @@ class DCUCollectorBot(Bot):
 
         ts_format = getattr(self.parameters, "timestamp_format", "%Y-%m-%d")
         dt_str = getattr(self.parameters, "date", None)
-    
+
         # parsing the configured date string before usage
         if dt_str:
             cal = parsedatetime.Calendar()
@@ -31,20 +31,22 @@ class DCUCollectorBot(Bot):
             date = None
 
         delete_flag = getattr(self.parameters, "delete", False)
-        
+
         blob_service = BlobService(account_name, account_key)
 
         for container in blob_service.list_containers():
             container_name = container.name
-            if container_name == "heartbeat": continue
-            if date and not (container_name == "processed-" +  date): continue
-            
+            if container_name == "heartbeat":
+                continue
+            if date and not (container_name == "processed-" + date):
+                continue
+
             for blob in blob_service.list_blobs(container_name):
                 self.logger.info("Fetching blob %s in container %s" % (container_name, blob.name))
                 data = blob_service.get_blob(container_name, blob.name)
                 cs = StringIO.StringIO(data)
                 report = gzip.GzipFile(fileobj=cs).read()
-                
+
                 self.send_message(report)
 
                 if delete_flag:
