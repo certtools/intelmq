2a7f2b8df290872c8e8c0d3ec1a8e6c9e04b76ac
Tomás Lima <synchroack@gmail.com>
2015-05-31 04:23:42 +0100

    translate bots - taichung

--- intelmq/bots/parsers/taichungcitynetflow/parser.py
+++ intelmq/bots/parsers/taichungcitynetflow/parser.py
@@ -15,6 +15,7 @@ CLASSIFICATION = {
 
 class TaichungCityNetflowParserBot(Bot):
     
+    
     def get_type(self, value):
         value = value.lower()
         for event_type, keywords in CLASSIFICATION.iteritems():
@@ -22,6 +23,7 @@ class TaichungCityNetflowParserBot(Bot):
                 if unicode(keyword) in value:
                     return event_type
         return "unknown"
+    
 
     def process(self):
         report = self.receive_message()
@@ -29,35 +31,35 @@ class TaichungCityNetflowParserBot(Bot):
         if not report.contains("raw"):
             self.acknowledge_message()
 
-        if len(report.value("raw").strip()) == 0:
-            self.acknowledge_message()
-
-        for row in report.value("raw").split('<tr>'):
+        raw_report = utils.base64_decode(report.value("raw"))
+        for row in raw_report.split('<tr>'):
 
             # Get IP and Type
             info1 = re.search(">[\ ]*(\d+\.\d+\.\d+\.\d+)[\ ]*<.*</td><td>([^<]+)</td>", row)
             
+            if not info1:
+                continue
+
             # Get Timestamp
             info2 = re.search("<td>[\ ]*(\d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2})[\ ]*</td>", row)
 
-            if info1:
-                event = Event()
+            event = Event()
 
-                description = info1.group(2)
-                event_type = self.get_type(description)
-                time_observation = DateTime().generate_datetime_now()
-                time_source = info2.group(1) + " UTC-8"
+            description = info1.group(2)
+            event_type = self.get_type(description)
+            time_observation = DateTime().generate_datetime_now()
+            time_source = info2.group(1) + " UTC-8"
 
-                event.add("time.source", time_source, sanitize=True)
-                event.add('time.observation', time_observation, sanitize=True)
-                event.add("source.ip", info1.group(1), sanitize=True)
-                event.add('classification.type', event_type, sanitize=True)
-                event.add('description', description, sanitize=True)
-                event.add('feed.name', u'taichungcitynetflow')
-                event.add('feed.url', u'https://tc.edu.tw/net/netflow/lkout/recent/30')
-                event.add("raw", row, sanitize=True)
-            
-                self.send_message(event)
+            event.add("time.source", time_source, sanitize=True)
+            event.add('time.observation', time_observation, sanitize=True)
+            event.add("source.ip", info1.group(1), sanitize=True)
+            event.add('classification.type', event_type, sanitize=True)
+            event.add('description', description, sanitize=True)
+            event.add('feed.name', u'taichungcitynetflow')
+            event.add('feed.url', u'https://tc.edu.tw/net/netflow/lkout/recent/30')
+            event.add("raw", row, sanitize=True)
+        
+            self.send_message(event)
         self.acknowledge_message()
 
 if __name__ == "__main__":
