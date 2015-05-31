c0a597c8cd3f66b9c77761e1d300a60c6e29e287
Tomás Lima <synchroack@gmail.com>
2015-05-31 20:11:58 +0100

    abusix expert translation

--- intelmq/bots/experts/abusix/expert.py
+++ intelmq/bots/experts/abusix/expert.py
@@ -6,21 +6,24 @@ Reference: https://abusix.com/contactdb.html
 RIPE abuse contacts resolving through DNS TXT queries
 '''
 
+
 class AbusixExpertBot(Bot):
 
     def process(self):
-
         event = self.receive_message()
 
-        for key in ['source_','destination_']:
-            if event.contains(key + "ip"):
-                ip = event.value(key + "ip")
+        for key in ['source.', 'destination.']:
+            ip_key = key + "ip"
+            if event.contains(ip_key):
+                ip = event.value(ip_key)
                 email = Abusix.query(ip)
                 if email:
-                    event.add(key + "abuse_contact", email)
+                    abuse_contact_key = key + "abuse_contact"
+                    event.add(abuse_contact_key, email)
 
         self.send_message(event)
         self.acknowledge_message()
+
 
 if __name__ == "__main__":
     bot = AbusixExpertBot(sys.argv[1])
