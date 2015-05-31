c35319d1e5f1944833b49a0a24e973a253fc369f
Tomás Lima <synchroack@gmail.com>
2015-05-31 20:24:01 +0100

    asn lookup expert translation

--- intelmq/bots/experts/asn_lookup/expert.py
+++ intelmq/bots/experts/asn_lookup/expert.py
@@ -1,6 +1,7 @@
 import pyasn
 from intelmq.lib.bot import Bot, sys
 
+
 class ASNLookupExpertBot(Bot):
 
     def init(self):
@@ -10,28 +11,32 @@ class ASNLookupExpertBot(Bot):
             self.logger.error("pyasn data file does not exist or could not be accessed in '%s'" % self.parameters.database)
             self.logger.error("Read 'bots/experts/asnlookup/README' and follow the procedure")
             self.stop()
-    
+
     def process(self):
         event = self.receive_message()
-            
-        keys = ["source_%s", "destination_%s"]
-            
-        for key in keys:
-            ip = event.value(key % "ip")
-            
+
+        for key in ["source.", "destination."]:
+
+            ip_key = key + "ip"
+            asn_key = key + "asn"
+            bgp_key = key + "bgp_prefix"
+
+            ip = event.value(ip_key)
+
             if not ip:
                 continue
-                
+
             info = self.database.lookup(ip)
-        
+
             if info:
                 if info[0]:
-                    event.update(key % "asn", unicode(info[0]))
+                    event.update(asn_key, unicode(info[0]))
                 if info[1]:
-                    event.update(key % "bgp_prefix", unicode(info[1]))
-            
+                    event.update(bgp_key, unicode(info[1]))
+
             self.send_message(event)
         self.acknowledge_message()
+
 
 if __name__ == "__main__":
     bot = ASNLookupExpertBot(sys.argv[1])
