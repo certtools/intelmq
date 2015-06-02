999ef8edbbc82eecbbd44be7c977906abb1d8e8e
Josef P. Bernhart <github@phantasus.at>
2015-06-02 15:43:54 +0200

    Remove unnecessary deletion, date processing code of dcu-collector

--- intelmq/bots/collectors/dcu/collector.py
+++ intelmq/bots/collectors/dcu/collector.py
@@ -4,8 +4,6 @@ from intelmq.bots import utils
 
 from azure.storage import BlobService
 
-import parsedatetime
-import time
 import gzip
 import StringIO
 
@@ -14,33 +12,18 @@ class DCUCollectorBot(Bot):
     """ 
       This IntelMQ collector is for getting a blob
       from an azure account (Microsoft dcu).
-      It opens the account and
-      reads all containers, which aren't ignored.
+      It opens the account and reads all containers.
     """
 
     def process(self):
-        account_name = getattr(self.parameters, "azure_account_name")
-        account_key = getattr(self.parameters, "azure_account_key")
-
-        ts_format = getattr(self.parameters, "timestamp_format", "%Y-%m-%d")
-        dt_str = getattr(self.parameters, "date", None)
-
-        # parsing the configured date string before usage
-        if dt_str:
-            cal = parsedatetime.Calendar()
-            date = time.strftime(ts_format, cal.parse(dt_str)[0])
-        else:
-            date = None
-
-        delete_flag = getattr(self.parameters, "delete", False)
+        account_name = self.parameters.azure_account_name
+        account_key = self.parameters.azure_account_key
 
         blob_service = BlobService(account_name, account_key)
 
         for container in blob_service.list_containers():
             container_name = container.name
             if container_name == "heartbeat":
-                continue
-            if date and not (container_name == "processed-" + date):
                 continue
 
             for blob in blob_service.list_blobs(container_name):
@@ -50,13 +33,6 @@ class DCUCollectorBot(Bot):
                 report = gzip.GzipFile(fileobj=cs).read()
 
                 self.send_message(report)
-
-                if delete_flag:
-                    self.logger.info("Deleting DCU blob \"%s\"" % blob.name)
-                    blob_service.delete_blob(container_name, blob.name)
-            if delete_flag:
-                self.logger.info("Deleting azure container \"%s\"" % container_name)
-                blob_service.delete_container(container_name)
 
 if __name__ == "__main__":
     bot = DCUCollectorBot(sys.argv[1])
