75024c2f3ddc452bc151df1c1ac304a636ed2dec
Thanat0s <Thanspam@trollprod.org>
2015-06-05 00:47:07 +0200

    pep8 check

--- intelmq/bots/parsers/generic/parser.py
+++ intelmq/bots/parsers/generic/parser.py
@@ -1,13 +1,7 @@
-#import unicodecsv
-#from cStringIO import StringIO
 from intelmq.lib import utils
 from intelmq.lib.bot import Bot, sys
 from intelmq.lib.message import Event
 from intelmq.lib.harmonization import DateTime
-
-#from intelmq.lib.bot import Bot, sys
-#from intelmq.lib.message import Event
-#from intelmq.bots import utils
 import re
 
 
@@ -29,16 +23,14 @@ class GenericBot(Bot):
 
         mapping_dict = {}
         # Will convert regex name to field
-        for mapping in self.parameters.mapping.split(","):  # Convert config to array
-            mkey, mvalue = mapping.split(":")
-            mapping_dict[mkey]=mvalue
+        for mapping in self.parameters.mapping.split(","):
+            mkey, mvalue = mapping.split(":")  # convert config to array
+            mapping_dict[mkey] = mvalue
 
         # Invert Dict for defaults values
-        rev_mapping_dict = dict(zip(mapping_dict.values(),mapping_dict.keys()))
-
-
+        rev_mapping_dict = dict(zip(mapping_dict.values(),
+          mapping_dict.keys()))
         raw_report = utils.base64_decode(report.value("raw"))
-
         self.logger.debug("Will apply regex %s" % self.parameters.regex)
         if report:
             rowcount = 0
@@ -52,14 +44,17 @@ class GenericBot(Bot):
                 else:
                     continue  # skip lines without matching regex
                 rowcount += 1
-                # Get detail from parser parameters, will be nice to have it by
-                # source parameters.. Avoid adding if parsed
+                # Get detail from parser parameters, will be nice to have
+                # it by source parameters.. Avoid adding if parsed
                 if not 'feed.name' in rev_mapping_dict:
-                  event.add('feed.name', self.parameters.feed_name, sanitize=True)
+                    event.add('feed.name', self.parameters.feed_name,
+                      sanitize=True)
                 if not 'feed.url' in rev_mapping_dict:
-                  event.add('feed.url', self.parameters.feed_url, sanitize=True)
+                    event.add('feed.url', self.parameters.feed_url,
+                      sanitize=True)
                 if not 'classification.type' in rev_mapping_dict:
-                  event.add('classification.type', self.parameters.classification_type, sanitize=True)
+                    event.add('classification.type',
+                      self.parameters.classification_type, sanitize=True)
                 time_observation = DateTime().generate_datetime_now()
                 event.add('time.observation', time_observation, sanitize=True)
                 self.send_message(event)
