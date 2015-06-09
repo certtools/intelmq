9382b924ba6eddb3e64594a1343bfd115d2a4943
Josef P. Bernhart <github@phantasus.at>
2015-06-09 14:46:36 +0200

    Add docstrings to lib/cache.py and pep8 mods
    Former-commit-id: 2fe176bfd2e2ce98bdc6d0728fba3e24cde1bb69

--- intelmq/lib/cache.py
+++ intelmq/lib/cache.py
@@ -4,27 +4,30 @@ import redis
     This provides a way, for example, to remove duplicated events
     and reports in system or cache some results from experts like
     Cymru Whois. It's possible to define a TTL value in each information
-    inserted in cache. This TTL means how much time the system will keep an 
+    inserted in cache. This TTL means how much time the system will keep an
     information in the cache.
 """
 
+
 class Cache():
     def __init__(self, host, port, db, ttl):
-        self.redis = redis.Redis(host = host,
-                                 port = int(port),
-                                 db = db,
-                                 socket_timeout = 5)
-        
+        self.redis = redis.Redis(host=host,
+                                 port=int(port),
+                                 db=db,
+                                 socket_timeout=5)
+
         self.ttl = ttl
 
-
     def exists(self, key):
+        """Returns True if key exists in the cache"""
         return self.redis.exists(key)
 
     def get(self, key):
+        """Returns the value of key in cache"""
         return self.redis.get(key)
 
     def set(self, key, value):
+        """Sets key to value in cache"""
         # backward compatibility (Redis v2.2)
         self.redis.setnx(key, value)
         self.redis.expire(key, self.ttl)
