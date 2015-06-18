c9cb491ecbdb06f4075f6e963150c95cecd6b13d
National CyberSecurity Center <oper@cncs.gov.pt>
2015-06-18 16:27:03 +0100

    Malc0de feed
    Former-commit-id: 0fab72160bdf54423b9b532ebb5ba87315d424f8

---
+++ intelmq/bots/parsers/malc0de/parser_ip_blacklist.py
@@ -0,0 +1,38 @@
+from intelmq.lib import utils
+from intelmq.lib.bot import Bot, sys
+from intelmq.lib.message import Event
+from intelmq.lib.harmonization import DateTime
+
+
+class Malc0deIPBlacklistParserBot(Bot):
+
+    def process(self):
+        report = self.receive_message()
+
+        if not report.contains("raw"):
+            self.acknowledge_message()
+
+        raw_report = utils.base64_decode(report.value("raw"))
+
+        for row in raw_report.split('\n'):
+
+            row = row.strip()
+            if row == "" or row[:2] == "//":
+                continue
+
+            event = Event()
+
+            time_observation = DateTime().generate_datetime_now()
+            event.add('time.observation', time_observation, sanitize=True)
+            event.add('classification.type', u'malware')
+            event.add('feed.name', report.value("feed.name"))
+            event.add('feed.url', report.value("feed.url"))
+            event.add('source.ip', row, sanitize=True)
+            event.add('raw', row, sanitize=True)
+
+            self.send_message(event)
+        self.acknowledge_message()
+
+if __name__ == "__main__":
+    bot = Malc0deIPBlacklistParserBot(sys.argv[1])
+    bot.start()
