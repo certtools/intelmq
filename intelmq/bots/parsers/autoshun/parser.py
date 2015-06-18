d135a7405707f392719721815e9c7838de3c6421
National CyberSecurity Center <oper@cncs.gov.pt>
2015-06-18 03:02:04 +0100

    Autoshun feed - pep8 applied

--- intelmq/bots/parsers/autoshun/parser.py
+++ intelmq/bots/parsers/autoshun/parser.py
@@ -5,14 +5,15 @@ from intelmq.lib.message import Event
 from intelmq.lib.harmonization import DateTime
 
 TAXONOMY = {
-		"brute force": "brute-force",
-		"bruteforce": "brute-force",
-		"scan": "scanner",
-		"cve": "exploit",
-		"sql inject": "exploit",
-		"c&c": "c&c",
-		"spam": "spam"
+    "brute force": "brute-force",
+    "bruteforce": "brute-force",
+    "scan": "scanner",
+    "cve": "exploit",
+    "sql inject": "exploit",
+    "c&c": "c&c",
+    "spam": "spam"
 }
+
 
 class AutoshunParserBot(Bot):
 
@@ -23,30 +24,30 @@ class AutoshunParserBot(Bot):
             self.acknowledge_message()
 
         raw_report = utils.base64_decode(report.value("raw"))
-		raw_report_splitted = raw_report.split("</tr>")[2:]
+        raw_report_splitted = raw_report.split("</tr>")[2:]
 
-		parser = HTMLParser.HTMLParser()
+        parser = HTMLParser.HTMLParser()
 
-		for row in raw_report_splitted:
+        for row in raw_report_splitted:
             event = Event()
 
-			row = row.strip()
+            row = row.strip()
 
-			if len(row) <= 0:
+            if len(row) <= 0:
                 continue
 
-		    info = row.split("<td>")
-		    if len(info) < 3:
-			    continue
+            info = row.split("<td>")
+            if len(info) < 3:
+                continue
 
-		    ip = info[1].split('</td>')[0].strip()
-		    last_seen = info[2].split('</td>')[0].strip()
-		    description = parser.unescape(info[3].split('</td>')[0].strip())
+            ip = info[1].split('</td>')[0].strip()
+            last_seen = info[2].split('</td>')[0].strip()
+            description = parser.unescape(info[3].split('</td>')[0].strip())
 
-		    for key in Parser.taxonomy.keys():
-			    if description.lower().find(key.lower()) > -1:
+            for key in Parser.taxonomy.keys():
+                if description.lower().find(key.lower()) > -1:
                     event.add("classification.type", TAXONOMY[key], sanitize=True)
-				    break
+                    break
 
             if not event.contains("classification.type"):
                 event.add("classification.type", u'unknown')
@@ -59,7 +60,7 @@ class AutoshunParserBot(Bot):
             event.add("source.ip", ip, sanitize=True)
             event.add("description.text", description, sanitize=True)
             event.add("raw", row, sanitize=True)
-                
+
             self.send_message(event)
         self.acknowledge_message()
 
