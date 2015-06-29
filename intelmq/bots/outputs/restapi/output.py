b72d247e7aa7f5f63379fe0e85b426bebb873e8c
robcza <robert.sefr@outlook.com>
2015-06-29 18:46:53 +0200

    persistent session and configurable auth token
    Former-commit-id: dd3aa0bf2c9b44f8f5e44b37480edf0db5cfb625

--- intelmq/bots/outputs/restapi/output.py
+++ intelmq/bots/outputs/restapi/output.py
@@ -3,15 +3,20 @@ from intelmq.lib.bot import Bot, sys
 
 class RestApi(Bot):
 
+
     def init(self):
-        url = self.parameters.host
+        self.session = requests.Session()
+#        self.session.headers.update({'X-sinkit-token': '3c5e933f0dcfb53e69fed19a88ff61a3921e0142'})
+        if self.parameters.auth_token_name and self.parameters.auth_token:
+            self.session.headers.update({self.parameters.auth_token_name : self.parameters.auth_token})
         
     def process(self):
         event = self.receive_message()
         try:
-            r = requests.post(self.parameters.host, event.to_json())
+            r = self.session.post(self.parameters.host, event.to_json())
+            r.raise_for_status()
         except requests.exceptions.RequestException as e:
-            self.logger.error("Failed to send request: " + str(e))
+            self.logger.error("Request exception: " + str(e))
         self.acknowledge_message()
 
 
