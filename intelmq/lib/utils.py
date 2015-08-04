9f0f660cd1800641e553bb3cab2d244d68e75bda
Sebastian Wagner <sebix@sebix.at>
2015-08-04 13:46:15 +0200

    ENH: intelmqctl log parsing/reading simplified
    The logs are read and parsed backwards, which is more efficient than reading and parsing the entire file from the beginning
    The code to read the file is easier to read and understand now, however some code in utils reads the file backwards in chunks.
    This change is currently not compatible with intelmq-manager as it wants to read the line number which is not read anymore as its not possible to get it by reading the file backwards. Yet a line number does not make sense in log files.
    
    
    Former-commit-id: 9cd3205759f17182371919cf9a591e35eabc3f16

--- intelmq/lib/utils.py
+++ intelmq/lib/utils.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import base64
+import os
 
 
 def decode(text, encodings=["utf-8"], force=False):
@@ -12,7 +13,7 @@ def decode(text, encodings=["utf-8"], force=False):
             return text.decode(encoding)
         except ValueError as e:
             pass
-        
+
     if force:
         for encoding in encodings:
             try:
@@ -28,7 +29,7 @@ def encode(text, encodings=["utf-8"], force=False):
             return text.encode(encoding)
         except ValueError as e:
             pass
-        
+
     if force:
         for encoding in encodings:
             try:
@@ -54,12 +55,44 @@ def load_configuration(configuration_filepath):
 def log(logs_path, name, loglevel="DEBUG"):
     logger = logging.getLogger(name)
     logger.setLevel(loglevel)
-    
+
     handler = logging.FileHandler("%s/%s.log" % (logs_path, name))
     handler.setLevel(loglevel)
-    
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-    
-    logger.addHandler(handler)    
+
+    logger.addHandler(handler)
     return logger
+
+
+def reverse_readline(filename, buf_size=8192):
+    """a generator that returns the lines of a file in reverse order
+    http://stackoverflow.com/a/23646049/2851664"""
+    with open(filename) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        total_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(total_size, offset + buf_size)
+            fh.seek(-offset, os.SEEK_END)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split('\n')
+            # the first line of the buffer is probably not a complete line so
+            # we'll save it and append it to the last line of the next buffer
+            # we read
+            if segment is not None:
+                # if the previous chunk starts right from the beginning of line
+                # do not concact the segment to the last line of new chunk
+                # instead, yield the segment first
+                if buffer[-1] is not '\n':
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if len(lines[index]):
+                    yield lines[index]
+        yield segment
