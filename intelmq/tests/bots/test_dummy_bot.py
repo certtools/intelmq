8840eafeaad54f7972561fb7b2027cdc755fcd81
Sebastian Wagner <sebix@sebix.at>
2015-08-18 15:53:40 +0200

    BUG: Correcting report and event for dummy bot test

--- intelmq/tests/bots/test_dummy_bot.py
+++ intelmq/tests/bots/test_dummy_bot.py
@@ -8,28 +8,31 @@ import intelmq.lib.bot as bot
 from intelmq.lib.message import Event
 
 
-EXAMPLE_EVENT = {"source.ip": "192.0.2.3",
-                 "time.source": "2015-06-04T13:37:00+00:00",
-                 "feed.url": "http://www.example.com/",
-                 "source.reverse_domain_name": "reverse.example.net",
-                 "source.url": "http://example.org",
-                 "time.observation": "2015-08-11T13:03:40+00:00",
-                 "raw": "MjAxNS8wNi8wNF8xMzozNyxleGFtcGxlLm9yZywxOTIuMC4yLjMsc"
-                        "mV2ZXJzZS5leGFtcGxlLm5ldCxleGFtcGxlIGRlc2NyaXB0aW9uLH"
-                        "JlcG9ydEBleGFtcGxlLm9yZywwMDAwMAo=",
-                 "__type": "Event",
-                 "classification.type": "malware",
-                 "description.text": "example description",
-                 "source.asn": "00000",
-                 "feed.name": "Example"}
-EXAMPLE_MESSAGE = json.dumps(EXAMPLE_EVENT)
+EXAMPLE_REPORT = {"source.ip": "192.0.2.3",
+                  "time.source": "2015-06-04T13:37:00+00:00",
+                  "feed.url": "http://www.example.com/",
+                  "source.reverse_domain_name": "reverse.example.net",
+                  "source.url": "http://example.org",
+                  "time.observation": "2015-08-11T13:03:40+00:00",
+                  "raw": "MjAxNS8wNi8wNF8xMzozNyxleGFtcGxlLm9yZywxOTIuMC4yLjMs"
+                         "cmV2ZXJzZS5leGFtcGxlLm5ldCxleGFtcGxlIGRlc2NyaXB0aW9u"
+                         "LHJlcG9ydEBleGFtcGxlLm9yZywwMDAwMAo=",
+                  "__type": "Report",
+                  "classification.type": "malware",
+                  "description.text": "example description",
+                  "source.asn": "00000",
+                  "feed.name": "Example"}
+EXAMPLE_EVENT = EXAMPLE_REPORT
+EXAMPLE_EVENT['__type'] = 'Event'
 
 
 class DummyBot(bot.Bot):
-    """ A dummy bot only for testing porpuse. """
+    """
+    A dummy bot only for testing purpose.
+    """
 
     def process(self):
-        """ Passing through all information from report to Event. """
+        """ Passing through all information from Report to Event. """
         report = self.receive_message()
 
         event = Event()
@@ -51,7 +54,7 @@ class TestDummyBot(test.BotTestCase, unittest.TestCase):
     def reset_bot(self):
         self.bot_id = 'test-bot'
         self.bot_reference = DummyBot
-        self.input_message = EXAMPLE_MESSAGE
+        self.input_message = json.dumps(EXAMPLE_EVENT)
         super(TestDummyBot, self).reset_bot()
 
     def test_log_test_line(self):
@@ -65,7 +68,7 @@ class TestDummyBot(test.BotTestCase, unittest.TestCase):
         """ Test if correct Event has been produced. """
         self.reset_bot()
         self.run_bot()
-        self.assertEventAlmostEqual(0, EXAMPLE_EVENT)
+        self.assertEventAlmostEqual(0, EXAMPLE_REPORT)
 
 
 if __name__ == '__main__':
