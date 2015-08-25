e961c00c5ee3b251ad1306145ecd9755759c4316
robcza <robert.sefr@outlook.com>
2015-08-25 17:33:17 +0200

    pep8 compliance fixed

--- intelmq/bots/parsers/alienvault/parser-otx.py
+++ intelmq/bots/parsers/alienvault/parser-otx.py
@@ -9,9 +9,9 @@ from intelmq.lib.harmonization import DateTime
 from intelmq.lib.message import Event
 
 HASHES = {
-    'FileHash-SHA256' : 'SHA-256',
-    'FileHash-SHA1' : 'SHA-1',
-    'FileHash-MD5' : 'MD5'
+    'FileHash-SHA256': 'SHA-256',
+    'FileHash-SHA1': 'SHA-1',
+    'FileHash-MD5': 'MD5'
 }
 
 
@@ -23,36 +23,45 @@ class AlienVaultOTXParserBot(Bot):
                 len(report.value("raw").strip()) == 0):
             self.acknowledge_message()
             return
-        
+
         time_observation = DateTime().generate_datetime_now()
         raw_report = utils.base64_decode(report.value("raw"))
 
         for pulse in json.loads(raw_report):
-            comment = "author: " + pulse['author_name'] + "; name: " + pulse['name'] + "; description: " + pulse['description']
+            comment = "author: " + pulse['author_name'] + "; name: " + \
+                pulse['name'] + "; description: " + pulse['description']
             for indicator in pulse["indicators"]:
                 event = Event()
-                #hashes don't work - invalid keys
-                #if indicator["type"] in ['FileHash-SHA256', 'FileHash-SHA1', 'FileHash-MD5']:
-                #    event.add('source.artifact_hash', indicator["indicator"])
-                #    event.add('source.artifact_hash_type', HASHES[indicator["type"]], sanitize = True)
-                #fqdn
+                # fqdn
                 if indicator["type"] in ['hostname', 'domain']:
-                    event.add('source.fqdn', indicator["indicator"], sanitize = True)
+                    event.add(
+                        'source.fqdn',
+                        indicator["indicator"],
+                        sanitize=True)
                 # IP addresses
                 elif indicator["type"] in ['IPv4', 'IPv6']:
-                    event.add('source.ip', indicator["indicator"], sanitize = True)
-                #emails
+                    event.add(
+                        'source.ip',
+                        indicator["indicator"],
+                        sanitize=True)
+                # emails
                 elif indicator["type"] == 'email':
-                    event.add('source.email_address', indicator["indicator"], sanitize = True)
-                #URLs
+                    event.add(
+                        'source.email_address',
+                        indicator["indicator"],
+                        sanitize=True)
+                # URLs
                 elif indicator["type"] in ['URL', 'URI']:
-                    event.add('source.url', indicator["indicator"], sanitize = True)
-                #CIDR, FilePath, Mutex, CVE
+                    event.add(
+                        'source.url',
+                        indicator["indicator"],
+                        sanitize=True)
+                #CIDR, FilePath, Mutex, CVE, hashes
                 else:
                     continue
 
                 event.add('comment', comment)
-                event.add('classification.type', 'blacklist', sanitize = True)
+                event.add('classification.type', 'blacklist', sanitize=True)
                 event.add('time.observation', time_observation, sanitize=True)
                 event.add('feed.name', report.value("feed.name"))
                 event.add("raw", json.dumps(indicator), sanitize=True)
