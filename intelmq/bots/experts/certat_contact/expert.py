a69c0d4eb73a6b5f9568c00add23168a358c6e6b
Sebastian Wagner <sebix@sebix.at>
2015-08-28 16:42:29 +0200

    ENH+TST: certat contact db expert bot plus tests
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

---
+++ intelmq/bots/experts/certat_contact/expert.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+CERT.at geolocate the national CERT abuse service
+https://contacts.cert.at/cgi-bin/abuse-nationalcert.pl
+
+HTTP GET: https://contacts.cert.at/cgi-bin/abuse-nationalcert.pl?ip=1.2.3.4
+HTTP POST: https://contacts.cert.at/cgi-bin/abuse-nationalcert.pl
+
+Options:
+&bShowNationalCERT=on	Show national CERT contact info
+&bShowHeader=on	Display a CSV header
+&bVerbose=on	Display the source of the data, and other infos
+&bFilter=off	Act as a filter: only show lines which geolocate to "AT"
+&bKeepLoglines=off	Keep original log lines (separated by "#")
+&sep={TAB, comma, semicolon, pipe}  	Separator for the (output) CSV format
+"""
+from __future__ import unicode_literals
+import requests
+import sys
+
+from intelmq.lib.bot import Bot
+
+
+URL = 'https://contacts.cert.at/cgi-bin/abuse-nationalcert.pl'
+
+
+class CertAtContactExpertBot(Bot):
+
+    def process(self):
+        event = self.receive_message()
+
+        if event is None:
+            self.acknowledge_message()
+            return
+
+        for section, key, abuse in [
+                ('source', 'source.ip', 'source.abuse_contact'),
+                ('destination', 'destination.ip', 'destination.abuse_contact')
+                ]:
+            if key in event:
+                parameters = {
+                    'ip': event[key],
+                    'bFilter': 'on' if self.parameters.filter else 'off',
+                    'bShowNationalCERT': 'on',
+                    'sep': 'semicolon',
+                    }
+                req = requests.get(URL, params=parameters,
+                                   verify=self.parameters.verify_cert,
+                                   )
+                response = req.text.strip().split(';')
+
+                ccfield = '{}.geolocation.cc'.format(section)
+                if self.parameters.overwrite_cc or ccfield not in event:
+                    event.add(ccfield, response[1])
+
+                if abuse in event:
+                    old_abuse = event[abuse]
+                    event.update(abuse, old_abuse+','+response[3])
+                else:
+                    event.add(abuse, response[3])
+
+        self.send_message(event)
+        self.acknowledge_message()
+
+
+if __name__ == "__main__":
+    bot = CertAtContactExpertBot(sys.argv[1])
+    bot.start()
---
+++ intelmq/tests/bots/experts/certat_contact/test_expert.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Testing certat_contact
+"""
+from __future__ import unicode_literals
+
+import json
+import unittest
+
+import intelmq.lib.test as test
+from intelmq.bots.experts.certat_contact.expert import CertAtContactExpertBot
+
+
+EXAMPLE_INPUT = {"__type": "Event",
+                 "source.ip": "93.184.216.34",  # example.com
+                 "destination.ip": "83.136.38.146",  # cert.at
+                 "time.observation": "2015-01-01T00:00:00+00:00",
+                 }
+EXAMPLE_OUTPUT = {"__type": "Event",
+                  "source.ip": "93.184.216.34",
+                  "source.geolocation.cc": "US",
+                  "source.abuse_contact": "soc@us-cert.gov",
+                  "time.observation": "2015-01-01T00:00:00+00:00",
+                  "destination.ip": "83.136.38.146",
+                  "destination.geolocation.cc": "AT",
+                  "destination.abuse_contact": "reports@cert.at",
+                  }
+EXAMPLE_INPUT6 = {"__type": "Event",
+                  "source.ip": "2001:500:88:200::7",  # iana.org
+                  "time.observation": "2015-01-01T00:00:00+00:00",
+                  }
+EXAMPLE_OUTPUT6 = {"__type": "Event",
+                   "source.ip": "2001:500:88:200::7",
+                   "source.abuse_contact": "soc@us-cert.gov",
+                   "source.geolocation.cc": "US",
+                   "time.observation": "2015-01-01T00:00:00+00:00",
+                   }
+
+
+class TestCertAtContactExpertBot(test.BotTestCase, unittest.TestCase):
+    """
+    A TestCase for AbusixExpertBot.
+    """
+
+    @classmethod
+    def set_bot(self):
+        self.bot_reference = CertAtContactExpertBot
+        self.sysconfig = {'filter': False,
+                          'overwrite_cc': False,
+                          'verify_cert': False,
+                          }
+        self.default_input_message = json.dumps({'__type': 'Report'})
+
+    def test_ipv4_lookup(self):
+        self.input_message = json.dumps(EXAMPLE_INPUT)
+        self.run_bot()
+        self.assertMessageEqual(0, EXAMPLE_OUTPUT)
+
+    def test_ipv6_lookup(self):
+        self.input_message = json.dumps(EXAMPLE_INPUT6)
+        self.run_bot()
+        self.assertMessageEqual(0, EXAMPLE_OUTPUT6)
+
+
+if __name__ == '__main__':
+    unittest.main()
