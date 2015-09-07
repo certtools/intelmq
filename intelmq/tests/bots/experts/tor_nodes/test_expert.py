e662959c2a7ae40e9119d4d5cfff13a20c7e177b
Sebastian Wagner <sebix@sebix.at>
2015-09-07 14:04:31 +0200

    DEV: travis install redis, skip asn,tor tests
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/tests/bots/experts/tor_nodes/test_expert.py
+++ intelmq/tests/bots/experts/tor_nodes/test_expert.py
@@ -7,12 +7,14 @@ TODO: IPv6
 from __future__ import unicode_literals
 
 import json
+import os
 import unittest
 
 import intelmq.lib.test as test
 from intelmq.bots.experts.tor_nodes.expert import TorExpertBot
 
 
+TOR_DB = '/opt/intelmq/var/lib/bots/tor_nodes/tor_nodes.dat'
 EXAMPLE_INPUT = {"__type": "Event",
                  "source.ip": "37.130.227.133",
                  "destination.ip": "192.0.43.8",  # iana.org
@@ -26,6 +28,8 @@ EXAMPLE_OUTPUT = {"__type": "Event",
                   }
 
 
+@unittest.skipUnless(os.path.exists(TOR_DB), 'tor-nodes database does not'
+                                             'exist in {}.'.format(TOR_DB))
 class TestTorExpertBot(test.BotTestCase, unittest.TestCase):
     """
     A TestCase for AbusixExpertBot.
@@ -34,8 +38,7 @@ class TestTorExpertBot(test.BotTestCase, unittest.TestCase):
     @classmethod
     def set_bot(self):
         self.bot_reference = TorExpertBot
-        self.sysconfig = {'database': '/opt/intelmq/var/lib/bots/tor_nodes/'
-                                      'tor_nodes.dat'}
+        self.sysconfig = {'database': TOR_DB}
         self.default_input_message = json.dumps({'__type': 'Report'})
 
     def test_ipv4_lookup(self):
