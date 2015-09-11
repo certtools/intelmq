d2f8e3db822d6df198abd3e1ea29e3b523a7ba6e
Sebastian Wagner <sebix@sebix.at>
2015-09-11 09:26:37 +0200

    BUG: Adapt alienvault otx to current master
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/bots/parsers/alienvault/parser_otx.py
+++ intelmq/bots/parsers/alienvault/parser_otx.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-; Events are gathered based on user subscriptions in AlienVault OTX
-; The data structure is described in detail here:
-; https://github.com/AlienVault-Labs/OTX-Python-SDK/blob/master/howto_use_python_otx_api.ipynb
+Events are gathered based on user subscriptions in AlienVault OTX
+The data structure is described in detail here:
+https://github.com/AlienVault-Labs/OTX-Python-SDK/blob/master/
+howto_use_python_otx_api.ipynb
 """
 from __future__ import unicode_literals
-import sys
+
 import json
+import sys
 
 from intelmq.lib import utils
 from intelmq.lib.bot import Bot
-from intelmq.lib.harmonization import DateTime
 from intelmq.lib.message import Event
 
 HASHES = {
@@ -19,25 +20,27 @@ HASHES = {
     'FileHash-MD5': '$1$'
 }
 
+
 class AlienVaultOTXParserBot(Bot):
 
     def process(self):
         report = self.receive_message()
-        if (report is None or not report.contains("raw")):
+        if report is None or not report.contains("raw"):
             self.acknowledge_message()
             return
 
         raw_report = utils.base64_decode(report.value("raw"))
 
         for pulse in json.loads(raw_report):
-            additional = json.dumps(
-                {"author": pulse['author_name'], "pulse": pulse['name']})
+            additional = json.dumps({"author": pulse['author_name'],
+                                     "pulse": pulse['name']},
+                                    sort_keys=True)
             for indicator in pulse["indicators"]:
-                event = Event()
+                event = Event(report)
                 # hashes
                 if indicator["type"] in HASHES.keys():
-                    event.add('malware.hash',
-                              HASHES[indicator["type"]] + indicator["indicator"])
+                    event.add('malware.hash', HASHES[indicator["type"]] +
+                              indicator["indicator"])
                 # fqdn
                 if indicator["type"] in ['hostname', 'domain']:
                     event.add('source.fqdn',
@@ -63,16 +66,12 @@ class AlienVaultOTXParserBot(Bot):
                     continue
 
                 event.add('comment', pulse['description'])
-                event.add('additional', additional, sanitize=True)
+                event.add('extra', additional, sanitize=True)
                 event.add('classification.type', 'blacklist', sanitize=True)
-                event.add('time.observation', report.value(
-                    'time.observation'), sanitize=True)
-                event.add(
-                    'time.source',
-                    indicator["created"][:-4] + "+00:00",
-                    sanitize=True)
-                event.add('feed.name', report.value("feed.name"))
-                event.add("raw", json.dumps(indicator), sanitize=True)
+                event.add('time.source', indicator["created"][:-4] + "+00:00",
+                          sanitize=True)
+                event.add("raw", json.dumps(indicator, sort_keys=True),
+                          sanitize=True)
                 self.send_message(event)
         self.acknowledge_message()
 
--- intelmq/tests/bots/parsers/alienvault/test_parser_otx.py
+++ intelmq/tests/bots/parsers/alienvault/test_parser_otx.py
@@ -10,7 +10,9 @@ import intelmq.lib.utils as utils
 from intelmq.bots.parsers.alienvault.parser_otx import \
     AlienVaultOTXParserBot
 
-with open(os.path.join(os.path.dirname(__file__), 'test_parser_otx.data')) as handle:
+
+with open(os.path.join(os.path.dirname(__file__),
+                       'test_parser_otx.data')) as handle:
     EXAMPLE_FILE = handle.read()
 
 EXAMPLE_REPORT = {"feed.name": "AlienVault OTX",
@@ -20,12 +22,26 @@ EXAMPLE_REPORT = {"feed.name": "AlienVault OTX",
                   }
 EXAMPLE_EVENT = {
     "__type": "Event",
-    "additional": '{"pulse": "The Spy Kittens Are Back: Rocket Kitten 2", "author": "AlienVault"}',
-    "comment": "Our findings show that Rocket Kitten is still active, retains a growing level of persistence, and acts ever more aggressively in terms of attack method. We also found that recent publications on the group’s activity have done nothing to change their behavior or reduce their activity. They don’t seem to bother to have to “disappear.” With this paper, we feel fairly certain that Rocket Kitten’s prime targets are not companies and political organizations as entire bodies but individuals that operate in strategically interesting fields such as diplomacy, foreign policy research, and defense-related businesses. We believe the espionage factor and political context make their attacks unique and very different from traditional targeted attacks.",
+    "extra": '{"author": "AlienVault", "pulse": "The Spy Kittens Are Back: '
+             'Rocket Kitten 2"}',
+    "comment": """Our findings show that Rocket Kitten is still active, retains
+a growing level of persistence, and acts ever more aggressively in terms of
+attack method. We also found that recent publications on the group’s activity
+have done nothing to change their behavior or reduce their activity. They don’t
+seem to bother to have to “disappear.” With this paper, we feel fairly certain
+that Rocket Kitten’s prime targets are not companies and political
+organizations as entire bodies but individuals that operate in strategically
+interesting fields such as diplomacy, foreign policy research, and
+defense-related businesses. We believe the espionage factor and political
+context make their attacks unique and very different from traditional targeted
+attacks.""".replace('\n', ' '),
     "feed.name": "AlienVault OTX",
     "classification.type": "blacklist",
     "source.url": "http://107.6.172.54/woolen/",
-    "raw": "eyJpbmRpY2F0b3IiOiAiaHR0cDovLzEwNy42LjE3Mi41NC93b29sZW4vIiwgIl9pZCI6ICI1NWU2YmZiMTQ2MzdmMjJjYjYwNTc0NjYiLCAidHlwZSI6ICJVUkwiLCAiZGVzY3JpcHRpb24iOiAiIiwgImNyZWF0ZWQiOiAiMjAxNS0wOS0wMlQwOToyMTo1My4wOTMifQ==",
+    "raw": "eyJfaWQiOiAiNTVlNmJmYjE0NjM3ZjIyY2I2MDU3NDY2IiwgImNyZWF0ZWQiOiAiMj"
+           "AxNS0wOS0wMlQwOToyMTo1My4wOTMiLCAiZGVzY3JpcHRpb24iOiAiIiwgImluZGlj"
+           "YXRvciI6ICJodHRwOi8vMTA3LjYuMTcyLjU0L3dvb2xlbi8iLCAidHlwZSI6ICJVUk"
+           "wifQ==",
     "time.source": "2015-09-02T09:21:53+00:00",
     "time.observation": "2015-09-02T14:17:58+00:00"
 }
