9d71f1c64b7122db1ff42bc3ed84210b35480666
Sebastian Wagner <sebix@sebix.at>
2015-09-16 10:29:36 +0200

    ENH: adapt requirements to version3 by defaults
    specify minimal requirements in setup.py
    
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- setup.py
+++ setup.py
@@ -40,6 +40,15 @@ setup(
     version='1.0.0',
     maintainer='Tomas Lima',
     maintainer_email='synchroack@gmail.com',
+    install_requires=[
+       'ipaddress>=1.0.14',
+       'psutil>=2.1.1',
+       'python-dateutil>=1.5',
+       'pytz>=2012d',
+       'redis>=2.10.3'
+       'requests>=2.4.2',
+       'six>=1.7',
+    ],
     packages=find_packages(),
     package_data={'intelmq': ['conf/*.conf', 'bots/experts/modify/*.conf']},
     url='http://pypi.python.org/pypi/intelmq/',
