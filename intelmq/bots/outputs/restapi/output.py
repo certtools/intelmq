c8dad4d15fe2bff9e781d75e10c7a0b068119dc3
robcza <robert.sefr@outlook.com>
2015-09-21 10:31:24 +0200

    BUG: logging exception for non-existing http response

--- intelmq/bots/outputs/restapi/output.py
+++ intelmq/bots/outputs/restapi/output.py
@@ -14,7 +14,8 @@ class RestAPI(Bot):
         if self.parameters.auth_token_name and self.parameters.auth_token:
             self.session.headers.update(
                 {self.parameters.auth_token_name: self.parameters.auth_token})
-        self.session.headers.update({"content-type" : "application/json; charset=utf-8"})
+        self.session.headers.update({"content-type" : 
+                                     "application/json; charset=utf-8"})
 
     def process(self):
         event = self.receive_message()
@@ -28,9 +29,12 @@ class RestAPI(Bot):
                                   event.to_json().encode('utf-8'))
             r.raise_for_status()
         except requests.exceptions.RequestException as e:
-            self.logger.error('Event: {0}\nResponse code: {1}\nHeaders: {2}'
+            if r:
+                self.logger.error('Event: {0}\nResponse code: {1}\nHeaders: {2}'
                               '\nResponse body: {3}'
                               ''.format(event.to_json(), r, r.headers, r.text))
+            else:
+                self.logger.error(repr(e))
         self.acknowledge_message()
 
 
