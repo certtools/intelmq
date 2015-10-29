38a091dd61f96bd84f9c42c3708248b147b0d752
robcza <robert.sefr@outlook.com>
2015-10-29 14:46:32 +0100

    enh: standalone tests for different cases bug: pytz is imported now bug: relative time regex tuned

--- intelmq/tests/bots/experts/filter/test_expert_relative_after.py
+++ intelmq/tests/bots/experts/filter/test_expert_relative_after.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import json
 import unittest
 
 import intelmq.lib.test as test
@@ -9,18 +8,11 @@ from intelmq.bots.experts.filter.expert import FilterExpertBot
 
 EXAMPLE_INPUT = {"__type": "Event",
                  "classification.type": "defacement",
-                 "time.source": "2015-01-01T00:00:00+00:00",
-                 "time.observation": "2015-09-012T00:00:00+00:00",
+                 "time.source": "2005-01-01T00:00:00+00:00",
+                 "time.observation": "2015-09-12T00:00:00+00:00",
                  "feed.name": "test-feed",
                  "raw": "fds56gf4jh4jhgh4j6"
                  }
-EXAMPLE_OUTPUT = {"__type": "Event",
-                  "classification.type": "defacement",
-                  "time.source": "2015-01-01T00:00:00+00:00",
-                  "time.observation": "2015-09-012T00:00:00+00:00",
-                  "feed.name": "test-feed",
-                  "raw": "fds56gf4jh4jhgh4j6"
-                  }
 
 
 class TestFilterExpertBot(test.BotTestCase, unittest.TestCase):
@@ -31,14 +23,12 @@ class TestFilterExpertBot(test.BotTestCase, unittest.TestCase):
     @classmethod
     def set_bot(self):
         self.bot_reference = FilterExpertBot
-        self.sysconfig = {'not_before': '2014-01-01T00:00:00+00:00', 
-                          'not_after': '6 months'}
         self.input_message = EXAMPLE_INPUT
+        self.sysconfig = {'not_after': '1 month'}
 
-    def test_Filter(self):
+    def test_Relative_After(self):
         self.run_bot()
-        self.assertMessageEqual(0, EXAMPLE_OUTPUT)
-
+        self.assertOutputQueueLen(1)
 
 if __name__ == '__main__':
     unittest.main()
