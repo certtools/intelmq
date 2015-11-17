b64d568016d7a4ca860e91d5f062fd16c01a24ce
aaronkaplan <aaron@lo-res.org>
2015-11-17 14:27:48 +0100

    next version of the test script

--- intelmq/tests/bots/parsers/n6/test_parser.py
+++ intelmq/tests/bots/parsers/n6/test_parser.py
@@ -7,18 +7,27 @@ import intelmq.lib.test as test
 from intelmq.bots.parsers.n6.parser_n6stomp import N6StompParserBot
 
 EXAMPLE_REPORT = {"feed.name": "n6stomp",
-                  "feed.url": "http://vxvault.siri-urz.net/URL_List.php",
                   "raw": "eyJjYXRlZ29yeSI6ICJib3RzIiwgIm9yaWdpbiI6ICJzaW5raG9sZSIsICJjb25maWRlbmNlIjogImhpZ2giLCAibmFtZSI6ICJzYWxpdHkiLCAicHJvdG8iOiAidGNwIiwgImFkZHJlc3MiOiBbeyJjYyI6ICJVUyIsICJpcCI6ICI2OC42OC45Ni4yMzUiLCAiYXNuIjogMTg3Nzl9XSwgInNvdXJjZSI6ICJjZXJ0LXBsLnNpbmtob2xlIiwgImFkaXAiOiAieC54LjExMS45OSIsICJ0aW1lIjogIjIwMTUtMTEtMTdUMTI6MTc6MjcuMDQzNDUyWiIsICJkcG9ydCI6IDgwLCAic3BvcnQiOiAyMjMwOCwgInR5cGUiOiAiZXZlbnQiLCAiaWQiOiAiZDc3YWU4Y2Y2ODFkY2RiYjZlMjAwMTQ1ODE0MDFlZDUifQ==",
                   "__type": "Report",
                   "time.observation": "2015-11-17T12:17:27.043452Z"
                   }
 EXAMPLE_EVENT = {"feed.name": "n6stomp",
-                 "feed.url": "stomp://n6stream.cert.pl:61614//exchange/cert.at/#",
-                 "source.url": "http://example.com/bad/program.exe",
-                 "classification.type": "malware",
-                 "__type": "Event",
+                 "source.ip": "68.68.96.235",
+                 "time.source": "2015-11-17T12:17:27.043452+00:00",
+                 "classification.taxonomy": "Malicious Code",
+                 "extra": '{ "_comment": "JSON dict for extra info", "feed_id": "d77ae8cf681dcdbb6e20014581401ed5", "adip": "x.x.111.99" }',
+                 "source.port": 22308,
                  "time.observation": "2015-11-17T12:17:27.043452Z",
-                 "raw": "eyJjYXRlZ29yeSI6ICJib3RzIiwgIm9yaWdpbiI6ICJzaW5raG9sZSIsICJjb25maWRlbmNlIjogImhpZ2giLCAibmFtZSI6ICJzYWxpdHkiLCAicHJvdG8iOiAidGNwIiwgImFkZHJlc3MiOiBbeyJjYyI6ICJVUyIsICJpcCI6ICI2OC42OC45Ni4yMzUiLCAiYXNuIjogMTg3Nzl9XSwgInNvdXJjZSI6ICJjZXJ0LXBsLnNpbmtob2xlIiwgImFkaXAiOiAieC54LjExMS45OSIsICJ0aW1lIjogIjIwMTUtMTEtMTdUMTI6MTc6MjcuMDQzNDUyWiIsICJkcG9ydCI6IDgwLCAic3BvcnQiOiAyMjMwOCwgInR5cGUiOiAiZXZlbnQiLCAiaWQiOiAiZDc3YWU4Y2Y2ODFkY2RiYjZlMjAwMTQ1ODE0MDFlZDUifQ=="
+                 "source.geolocation.cc": "US",
+                 "raw": "eyJjYXRlZ29yeSI6ICJib3RzIiwgIm9yaWdpbiI6ICJzaW5raG9sZSIsICJjb25maWRlbmNlIjogImhpZ2giLCAibmFtZSI6ICJzYWxpdHkiLCAicHJvdG8iOiAidGNwIiwgImFkZHJlc3MiOiBbeyJjYyI6ICJVUyIsICJpcCI6ICI2OC42OC45Ni4yMzUiLCAiYXNuIjogMTg3Nzl9XSwgInNvdXJjZSI6ICJjZXJ0LXBsLnNpbmtob2xlIiwgImFkaXAiOiAieC54LjExMS45OSIsICJ0aW1lIjogIjIwMTUtMTEtMTdUMTI6MTc6MjcuMDQzNDUyWiIsICJkcG9ydCI6IDgwLCAic3BvcnQiOiAyMjMwOCwgInR5cGUiOiAiZXZlbnQiLCAiaWQiOiAiZDc3YWU4Y2Y2ODFkY2RiYjZlMjAwMTQ1ODE0MDFlZDUifQ==",
+                 "classification.identifier": "sality",
+                 "malware.name": "sality",
+                 "classification.type": "botnet drone",
+                 "destination.port": 80,
+                 "__type": "Event",
+                 "protocol.transport": "tcp",
+                 "source.asn": 18779,
+                 "feed.url": "stomp://n6stream.cert.pl:61614//exchange/cert.at/#"
                  }
 
 
