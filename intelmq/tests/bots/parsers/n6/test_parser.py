8b2947cec5a36a9e5c1243e642266b2f05be99fa
aaronkaplan <aaron@lo-res.org>
2015-11-17 14:32:54 +0100

    report needs to contain the url

--- intelmq/tests/bots/parsers/n6/test_parser.py
+++ intelmq/tests/bots/parsers/n6/test_parser.py
@@ -9,7 +9,8 @@ from intelmq.bots.parsers.n6.parser_n6stomp import N6StompParserBot
 EXAMPLE_REPORT = {"feed.name": "n6stomp",
                   "raw": "eyJjYXRlZ29yeSI6ICJib3RzIiwgIm9yaWdpbiI6ICJzaW5raG9sZSIsICJjb25maWRlbmNlIjogImhpZ2giLCAibmFtZSI6ICJzYWxpdHkiLCAicHJvdG8iOiAidGNwIiwgImFkZHJlc3MiOiBbeyJjYyI6ICJVUyIsICJpcCI6ICI2OC42OC45Ni4yMzUiLCAiYXNuIjogMTg3Nzl9XSwgInNvdXJjZSI6ICJjZXJ0LXBsLnNpbmtob2xlIiwgImFkaXAiOiAieC54LjExMS45OSIsICJ0aW1lIjogIjIwMTUtMTEtMTdUMTI6MTc6MjcuMDQzNDUyWiIsICJkcG9ydCI6IDgwLCAic3BvcnQiOiAyMjMwOCwgInR5cGUiOiAiZXZlbnQiLCAiaWQiOiAiZDc3YWU4Y2Y2ODFkY2RiYjZlMjAwMTQ1ODE0MDFlZDUifQ==",
                   "__type": "Report",
-                  "time.observation": "2015-11-17T12:17:27.043452Z"
+                  "time.observation": "2015-11-17T12:17:27.043452Z",
+                  "feed.url": "stomp://n6stream.cert.pl:61614//exchange/cert.at/#"
                   }
 EXAMPLE_EVENT = {"feed.name": "n6stomp",
                  "source.ip": "68.68.96.235",
