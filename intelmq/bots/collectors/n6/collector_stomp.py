8ed2bf1adf9d65ed93b01384b8bce96a94576f7c
aaronkaplan <aaron@lo-res.org>
2015-11-17 14:58:16 +0100

    cleaning up stupid <<< diffs . We all love git

--- intelmq/bots/collectors/n6/collector_stomp.py
+++ intelmq/bots/collectors/n6/collector_stomp.py
@@ -1,25 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-<<<<<<< HEAD
 
 import os.path
 import sys
-=======
-import sys
-import os.path
-import stomp
->>>>>>> fd510b5... initial n6stomp commit. PEP8 ready!
 
 from intelmq.lib.bot import Bot
 from intelmq.lib.harmonization import DateTime
 from intelmq.lib.message import Report
 
-<<<<<<< HEAD
 import stomp
 
-=======
->>>>>>> fd510b5... initial n6stomp commit. PEP8 ready!
 
 class StompListener(stomp.listener.PrintingListener):
     """ the stomp listener gets called asynchronously for
@@ -29,20 +20,9 @@ class StompListener(stomp.listener.PrintingListener):
         self.n6stomper = n6stompcollector
 
     def on_heartbeat_timeout(self):
-<<<<<<< HEAD
         self.n6stomper.logger.warn("Lost connection! Re-establishing.")
         self.n6stomper.conn.disconnect()
         self.n6stomper.conn.connect(wait=False)
-
-    def on_error(self, headers, message):
-        self.n6stomper.logger.warn('Received an error "%s".' % repr(message))
-
-    def on_message(self, headers, message):
-        self.n6stomper.logger.info("Got message %s." % repr(message))
-=======
-        # XXX FIXME: use logger instead of print
-        # XXX FIXME: need to reconnect after timeout
-        print "lost connection!"
 
     def on_error(self, headers, message):
         # XXX FIXME: use logger instead of print
@@ -50,7 +30,6 @@ class StompListener(stomp.listener.PrintingListener):
 
     def on_message(self, headers, message):
         self.n6stomper.logger.info("got message %s" % repr(message))
->>>>>>> fd510b5... initial n6stomp commit. PEP8 ready!
         report = Report()
         report.add("raw", message.rstrip(), sanitize=True)
         report.add("feed.name", self.n6stomper.parameters.feed,
@@ -84,12 +63,8 @@ class n6stompCollectorBot(Bot):
         # check if certificates exist
         for f in [self.ssl_ca_cert, self.ssl_cl_cert, self.ssl_cl_cert_key]:
             if (not os.path.isfile(f)):
-<<<<<<< HEAD
-                raise ValueError('Could not open file %s.' % f)
-=======
                 self.logger.exception('could not open file %s' % f)
                 raise Exception('could not open file %s' % f)
->>>>>>> fd510b5... initial n6stomp commit. PEP8 ready!
 
         _host = [(self.server, self.port)]
         self.conn = stomp.Connection(host_and_ports=_host, use_ssl=True,
