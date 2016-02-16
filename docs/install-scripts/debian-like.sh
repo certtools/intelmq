8909d2befbc24a7548f4c5977ad282f869a0611a
Sebastian Wagner <sebix@sebix.at>
2016-02-16 16:31:54 +0100

    DOC: Installation via pip
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- docs/install-scripts/debian-like.sh
+++ docs/install-scripts/debian-like.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 apt-get install python3 python3-pip
-apt-get install git build-essential libcurl4-gnutls-dev libffi-dev python-dev
+apt-get install git build-essential libcurl4-gnutls-dev libffi-dev
+apt-get install python-dev
 apt-get install redis-server
 
 pip3 install -r REQUIREMENTS
-python3 setup.py install
+pip3 install -U .
 
 useradd -d /opt/intelmq -U -s /bin/bash intelmq
-echo 'export PATH="$PATH:$HOME/bin"' > /opt/intelmq/.profile
+echo 'export PATH="$PATH:$HOME/bin"' >> /opt/intelmq/.profile
 chmod -R 0770 /opt/intelmq
 chown -R intelmq.intelmq /opt/intelmq
 echo 'export INTELMQ_PYTHON=/usr/bin/python3' >> /opt/intelmq/.profile
