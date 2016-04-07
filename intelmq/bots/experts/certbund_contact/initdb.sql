81a0cb8bba4acdd6e97f6cde632ab52265f33305
Bernhard Herzog <bernhard.herzog@intevation.de>
2016-04-07 20:59:20 +0200

    Add constraints to the datamodel of the contact bot
    Add constraints to columns where they make sense: Most of the columns
    are now NOT NULL and some, like the comments, have a default value. Some
    now have a UNIQUE constraint as well.

--- intelmq/bots/experts/certbund_contact/initdb.sql
+++ intelmq/bots/experts/certbund_contact/initdb.sql
@@ -7,7 +7,7 @@ CREATE TABLE format (
     id INTEGER PRIMARY KEY,
 
     -- Most likely a WKT or MIME-Type
-    name VARCHAR(80)
+    name VARCHAR(80) UNIQUE NOT NULL
 );
 
 
@@ -18,8 +18,9 @@ CREATE TABLE organisation (
     id INTEGER PRIMARY KEY,
 
     -- The name of the organisation.
-    name VARCHAR(80),
-    comment TEXT
+    name VARCHAR(80) UNIQUE NOT NULL,
+
+    comment TEXT NOT NULL DEFAULT ''
 );
 
 
@@ -27,15 +28,15 @@ CREATE TABLE contact (
     id INTEGER PRIMARY KEY,
 
     -- the email-address of the contact
-    email VARCHAR(100),
+    email VARCHAR(100) NOT NULL,
 
     -- The data format to be used in emails sent to this contact.
-    format_id INTEGER,
+    format_id INTEGER NOT NULL,
 
     -- Whether this contact tuple is maintained manually.
-    is_manual BOOLEAN,
+    is_manual BOOLEAN NOT NULL,
 
-    comment TEXT,
+    comment TEXT NOT NULL DEFAULT '',
 
     FOREIGN KEY (format_id) REFERENCES format (id)
 );
@@ -51,9 +52,9 @@ CREATE TABLE autonomous_system (
     number BIGINT PRIMARY KEY,
 
     -- Whether this autonomous system tuple is maintained manually.
-    is_manual BOOLEAN,
+    is_manual BOOLEAN NOT NULL,
 
-    comment TEXT
+    comment TEXT NOT NULL DEFAULT ''
 );
 
 
@@ -62,12 +63,12 @@ CREATE TABLE network (
     id INTEGER PRIMARY KEY,
 
     -- Network address as CIDR.
-    address cidr,
+    address cidr UNIQUE NOT NULL,
 
     -- Whether this network tuple is maintained manually.
-    is_manual BOOLEAN,
+    is_manual BOOLEAN NOT NULL,
 
-    comment TEXT
+    comment TEXT NOT NULL DEFAULT ''
 );
 
 
@@ -76,12 +77,12 @@ CREATE TABLE fqdn (
     id INTEGER PRIMARY KEY,
 
     -- The fully qualified domain name
-    fqdn TEXT,
+    fqdn TEXT UNIQUE NOT NULL,
 
     -- Whether this FQDN-tuple is maintained manually.
-    is_manual BOOLEAN,
+    is_manual BOOLEAN NOT NULL,
 
-    comment TEXT
+    comment TEXT NOT NULL DEFAULT ''
 );
 
 
@@ -91,7 +92,7 @@ CREATE TABLE fqdn (
 */
 CREATE TABLE classification_identifier (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(100)
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 
 /*
@@ -101,10 +102,10 @@ CREATE TABLE template (
     id INTEGER PRIMARY KEY,
 
     -- File-name of the template
-    path VARCHAR(200),
+    path VARCHAR(200) NOT NULL,
 
     -- The classification identifier for which this template can be used.
-    classification_identifier_id INTEGER,
+    classification_identifier_id INTEGER NOT NULL,
 
     FOREIGN KEY (classification_identifier_id)
      REFERENCES classification_identifier (id)
@@ -117,7 +118,7 @@ CREATE TABLE template (
 CREATE TABLE contact_to_asn (
     contact_id INTEGER,
     asn_id BIGINT,
-    ttl INTEGER,
+    ttl INTEGER NOT NULL,
 
     PRIMARY KEY (contact_id, asn_id),
 
@@ -128,7 +129,7 @@ CREATE TABLE contact_to_asn (
 CREATE TABLE contact_to_network (
     contact_id INTEGER,
     net_id INTEGER,
-    ttl INTEGER,
+    ttl INTEGER NOT NULL,
 
     PRIMARY KEY (contact_id, net_id),
 
@@ -139,7 +140,7 @@ CREATE TABLE contact_to_network (
 CREATE TABLE contact_to_fqdn (
     contact_id INTEGER,
     fqdn_id INTEGER,
-    ttl INTEGER,
+    ttl INTEGER NOT NULL,
 
     PRIMARY KEY (contact_id, fqdn_id),
 
@@ -160,8 +161,8 @@ CREATE TABLE contact_to_organisation (
 
 CREATE TABLE organisation_to_template (
     id INTEGER PRIMARY KEY,
-    organisation_id INTEGER,
-    template_id INTEGER,
+    organisation_id INTEGER NOT NULL,
+    template_id INTEGER NOT NULL,
 
     FOREIGN KEY (organisation_id) REFERENCES organisation (id),
     FOREIGN KEY (template_id) REFERENCES template (id)
