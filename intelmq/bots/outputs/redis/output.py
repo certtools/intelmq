0a66a7897d4eef10a4ef4042e064e6e2023f27ea
pedromreis <pedro.m.reis@gmail.com>
2016-04-18 23:13:12 +0100

    ready to test

--- intelmq/bots/outputs/redis/output.py
+++ intelmq/bots/outputs/redis/output.py
@@ -1,70 +1,37 @@
 # -*- coding: utf-8 -*-
-#import socket
 import sys
 import time
-#import redis
+import redis
 import intelmq.lib.utils as utils
 from intelmq.lib.bot import Bot
 
 
 class RedisOutputBot(Bot):
+
     def init(self)
-
-ver expert abusix/cymru
-    "parameters": {
-                "redis_server_ip": "127.0.0.1",
-                "redis_server_port": "6379",
-                "redis_db": "2",
-                "redis_queue": "external-redis-queue"
-            }
-def process(self):
-       self.output.lpush(self.myqueue,"ola"+str(self.count))
-       self.count += count
-       while self.output.llen(self.myqueue) > 0:
-             print(utils.decode(self.output.lpop(self.myqueue)))
-
-
-#################### tcp bot #########
+        self.destination_queue = self.parameters.redis_queue
+        self.conn = redis.ConnectionPool(host=self.parameters.redis_server_ip, 
+                                         port=int(self.parameters.redis_server_port), 
+                                         int(self.parameters.redis_db)
+                                        )
 
     def process(self):
         event = self.receive_message()
-
+        
         if event is None:
             self.acknowledge_message()
             return
 
-        data = event.to_json()
-        self.send_data(data)
-        self.acknowledge_message()
-
-    def connect(self):
-        address = (self.parameters.ip, int(self.parameters.port))
-        self.con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         while True:
             try:
-                self.con.connect(address)
+                output = redis.Redis(connection_pool=self.conn)
+                self.output.lpush(event.to_json)
                 break
-            except socket.error as exc:
-                self.logger.error(exc.args[1] + ". Retrying in 10 seconds.")
+            except redis.ConnectionError:
+                self.logger.error("Redis connection failled, retrying in 10 seconds")
                 time.sleep(10)
 
-        self.logger.info("Connected successfully to {!s}: {}"
-                         "".format(address[0], address[1]))
-
-    def send_data(self, data):
-        while True:
-            try:
-                self.con.send(utils.encode(data))
-                self.con.sendall(b"")
-                break
-            except socket.error as exc:
-                self.logger.error(exc.args[1] + ". Reconnecting..")
-                self.con.close()
-                self.connect()
-            except AttributeError:
-                self.connect()
-############################################
+        self.acknowledge_message()
 
 if __name__ == "__main__":
     bot = RedisOutputBot(sys.argv[1])
