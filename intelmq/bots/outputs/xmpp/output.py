3e01ee350dd17aa38d51e8d767739e863085106f
Dustin Demuth <dustin.demuth@intevation.de>
2016-04-22 11:50:19 +0200

    Refactored the XMPPOutputBot.
     * Connect to XMPP Servers
     * Send Events to a User
    
    ToDo:
     * Proper error Handling
     * Multi User Chat

--- intelmq/bots/outputs/xmpp/output.py
+++ intelmq/bots/outputs/xmpp/output.py
@@ -1,18 +1,37 @@
-# -*- coding: utf-8 -*-
+"""
+XMPP Output Bot
+Connects to a XMPP Server and sends data to a user.
+
+TODO: Introduce Multi User Chat like in XMPP Collector
+
+Requires Python >= 3.4
+Requires sleekxmpp >= 1.0.0-beta5
+
+Copyright (C) 2016 by Bundesamt für Sicherheit in der Informationstechnik
+Software engineering by Intevation GmbH
+
+Parameters:
+xmpp_user: string
+xmpp_server: string
+xmpp_password: boolean
+# TODO xmpp_room: string
+# TODO xmpp_room_password: string
+# TODO xmpp_room_nick: string
+"""
 import sys
 import json
 
 from intelmq.lib.bot import Bot
 from sleekxmpp import ClientXMPP
-from sleekxmpp.exceptions import IqError, IqTimeout
+from sleekxmpp.exceptions import XMPPError
 
 
-class XMPP(Bot):
+class XMPPOutputBot(Bot):
 
     def init(self):
-        self.xmpp = XMPPBot(self.parameters.xmpp_user + '@' + self.parameters.xmpp_server,
+        self.xmpp = XMPPClientBot(self.parameters.xmpp_user + '@'
+                            + self.parameters.xmpp_server,
                             self.parameters.xmpp_password,
-                            self.send_message,
                             self.logger)
         self.xmpp.connect(reattempt=True)
         self.xmpp.process()
@@ -20,46 +39,52 @@ class XMPP(Bot):
     def process(self):
         event = self.receive_message()
 
+        receiver = self.parameters.xmpp_to_user + '@' \
+                    + self.parameters.xmpp_to_server
+
         if event is None:
             self.acknowledge_message()
+        else:
+            jevent = event.to_json()
 
-        # TODO: catch appropriate exceptions
-        self.xmpp.send_message(mto=self.parameters.xmpp_to_user + '@' + self.parameters.xmpp_to_server,
-                               mbody=event.to_json())
+        try:
+            # TODO: proper error handling. Right now it cannot be detected if
+            # the message was sent successfully.
+            self.logger.debug("XMPP Trying to send Event: %r , to %r",
+                            jevent,
+                            receiver)
+            self.xmpp.send_message(mto=receiver, mbody=jevent)
+        except XMPPError as err:
+            self.logger.error('There was an error when sending the event')
+            self.logger.error(err.iq['error']['condition'])
 
         self.acknowledge_message()
 
 
-class XMPPBot(ClientXMPP):
+class XMPPClientBot(ClientXMPP):
 
-    def __init__(self, jid, password, send_message, logger):
+    def __init__(self,
+                jid,
+                password,
+                logger):
         ClientXMPP.__init__(self, jid, password)
-        self.add_event_handler("session_start", self.session_start)
-        self.add_event_handler("message", self.message_logging)
+
         self.logger = logger
         self.logger.info("XMPP connected")
-        self.send_message = send_message
+
+        self.add_event_handler("session_start", self.session_start)
 
     def session_start(self, event):
         self.send_presence()
+        self.logger.debug("XMPP Session started")
 
         try:
             self.get_roster()
-        except IqError as err:
+        except XMPPError as err:
             self.logger.error('There was an error getting the roster')
             self.logger.error(err.iq['error']['condition'])
             self.disconnect()
-        except IqTimeout:
-            self.logger.error('Server is taking too long to respond')
-            self.disconnect()
-
-    def message_logging(self, msg):
-
-        event = json.loads(msg['body'])
-        self.logger.info("Event received from {}".format(msg['from']))
-        self.send_message(event)
-
 
 if __name__ == "__main__":
-    bot = XMPP(sys.argv[1])
+    bot = XMPPOutputBot(sys.argv[1])
     bot.start()
