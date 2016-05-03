0dcc99335a571b411cede0d285a89752c7502141
Sascha Wilde <wilde@intevation.de>
2016-05-03 17:45:38 +0200

    Reworked data update script for tor nodes expert bot.
    Now the destination file is given as argument on the command line.

--- intelmq/bots/experts/tor_nodes/update_tor_nodes.sh
+++ intelmq/bots/experts/tor_nodes/update_tor_nodes.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 # Update tor nodes data
+# The target file must be given as only argument on the commandline.
 PATH=/bin:/usr/bin
-
 set -e
 trap cleanup EXIT
 
-DEST_FILE=/opt/intelmq/var/lib/bots/tor_nodes/tor_nodes.dat
-TMP_DIR=`mktemp -d`
-dest_dir=`dirname "$DEST_FILE"`
+fail()
+{
+  echo >&2 "$@"
+  exit 23
+}
+
+setup()
+{
+  [ "$#" -eq 1 ] || fail "Exactly one argument DESTINATION-FILE must be given."
+  DEST_FILE="$1"
+  TMP_DIR=`mktemp -d`
+  dest_dir=`dirname "$DEST_FILE"`
+  [ -d "$dest_dir" ] || mkdir -p "$dest_dir"
+}
 
 cleanup()
 {
-  [ -e "$TMP_DIR/latest.bz2" ] && rm "$TMP_DIR/latest.bz2"
-  rmdir "$TMP_DIR"
+  [ -d "$dest_dir" ] && rm -rf "$TMP_DIR"
 }
 
+fetch_and_install()
+{
+  cd "$TMP_DIR"
+  curl -s -O https://internet2.us/static/latest.bz2
+  bunzip2 -c latest.bz2 >tor_nodes.dat
+  mv tor_nodes.dat "$DEST_FILE"
+}
 
-[ -d "$dest_dir" ] || mkdir -p "$dest_dir"
-
-cd "$TMP_DIR"
-curl -O https://internet2.us/static/latest.bz2
-bunzip2 -c latest.bz2 >tor_nodes.dat
-mv tor_nodes.dat "$DEST_FILE"
+setup "$@"
+fetch_and_install
