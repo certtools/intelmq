e436c1f04cb3de5b2d293595f9b32d4933acad59
Gernot Schulz <gernot@intevation.de>
2016-05-04 10:10:13 +0200

    Add HTTP auth to mail collector bot
    Code copied from HTTP collector bot

--- intelmq/bots/collectors/mail/collector_mail_url.py
+++ intelmq/bots/collectors/mail/collector_mail_url.py
@@ -34,8 +34,34 @@ class MailURLCollectorBot(Bot):
                     if match:
                         url = match.group()
 
+                        # Build request
+                        self.http_header = getattr(self.parameters,
+                                'http_header', {})
+                        self.http_verify_cert = getattr(self.parameters,
+                                                        'http_verify_cert', True)
+
+                        if hasattr(self.parameters, 'http_username') and hasattr(
+                                self.parameters, 'http_password'):
+                            self.auth = (self.parameters.http_username,
+                                         self.parameters.http_password)
+                        else:
+                            self.auth = None
+
+                        http_proxy = getattr(self.parameters, 'http_proxy', None)
+                        https_proxy = getattr(self.parameters,
+                                              'http_ssl_proxy', None)
+                        if http_proxy and https_proxy:
+                            self.proxy = {'http': http_proxy, 'https': https_proxy}
+                        else:
+                            self.proxy = None
+
+                        self.http_header['User-agent'] = self.parameters.http_user_agent
+
                         self.logger.info("Downloading report from %s" % url)
-                        resp = requests.get(url=url)
+                        resp = requests.get(url=url,
+                                            auth=self.auth, proxies=self.proxy,
+                                            headers=self.http_header,
+                                            verify=self.http_verify_cert)
 
                         if resp.status_code // 100 != 2:
                             raise ValueError('HTTP response status code was {}.'
