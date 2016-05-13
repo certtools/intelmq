4122059f5642f78453c39e7f3a177c48cc06a39c
Dustin Demuth <dustin.demuth@intevation.de>
2016-05-13 19:03:13 +0200

    Fixed Unit Test

--- intelmq/tests/bots/experts/certbund_contact/test_expert.py
+++ intelmq/tests/bots/experts/certbund_contact/test_expert.py
@@ -15,13 +15,19 @@ from intelmq.bots.experts.certbund_contact.expert import CERTBundKontaktExpertBo
 EXAMPLE_INPUT = {"__type": "Event",
                  "source.ip": "192.168.42.23",
                  "destination.ip": "192.168.42.47",
-                 "time.observation": "2016-02-26T10:11:12+00:00"
+                 "time.observation": "2016-02-26T10:11:12+00:00",
+                 "feed.name": "test",
+                 "raw": "",
+                 "classification.type": "other"
                  }
 
 EXAMPLE_OUTPUT = {"__type": "Event",
                   "source.ip": "192.168.42.23",
                   "destination.ip": "192.168.42.47",
                   "time.observation": "2016-02-26T10:11:12+00:00",
+                  "feed.name": "test",
+                  "raw": "",
+                  "classification.type": "other",
                   'extra': ('{"certbund": {'
                             '"notify_destination": ['
                             '{"email": "foo@example.com",'
@@ -43,7 +49,7 @@ class CERTBundKontaktMockDBExpertBot(CERTBundKontaktExpertBot):
     def connect_to_database(self):
         pass
 
-    def lookup_ip(self, ip, classification):
+    def lookup_contact(self, classification, ip, fqdn, asn):
         if ip.startswith("192.168.42."):
             return [dict(email="foo@example.com",
                          organisation="Acme",
@@ -62,6 +68,7 @@ class TestCERTBundKontaktMockDBExpertBot(test.BotTestCase, unittest.TestCase):
     @classmethod
     def set_bot(cls):
         cls.bot_reference = CERTBundKontaktMockDBExpertBot
+        cls.default_input_message = EXAMPLE_INPUT
 
     def test_ipv4_lookup(self):
         self.input_message = EXAMPLE_INPUT
