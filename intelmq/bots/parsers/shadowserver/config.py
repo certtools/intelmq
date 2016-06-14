ce58d30e545e909e0bc61056e66a3d162454d31a
Dustin Demuth <mail@dmth.eu>
2016-06-14 16:24:53 +0200

    Added configuration for some additional feeds.
    I fear that not all attributes are evaluated in a sufficient manner.
    maybe we need to enhance the use of the 'extra' json attribute.
    
    TODOs:
     - implement more feeds (10 + left)
     - check exiting implementation of feeds, as the might not be correct.

--- intelmq/bots/parsers/shadowserver/config.py
+++ intelmq/bots/parsers/shadowserver/config.py
@@ -18,6 +18,9 @@ TODOs:
     There is a bunch of inline todos.
     Most of them show lines of code were the mapping  has to be validated
 
+    @ Check-Implementation Tags for parser configs.
+     dmth thinks it's not sufficient. I.e. a classification identifier would be great
+
 """
 
 
@@ -28,11 +31,11 @@ def get_feed(feedname):
         "Open-Memcached": open_memcached,
         "Ssl-Scan": ssl_scan,  # Aka Poodle
         "NTP-Monitor": ntp_monitor,
-        "DNS-open-resolvers": dns_open_resolvers,  # TODO not yet implemented
-        "Open-Elasticsearch": open_elasticsearch,  # TODO not yet implemented
-        "Open-Net BIOS": open_net_bios,  # TODO not yet implemented
-        "Open-Mongo DB": open_mongo_db,  # TODO not yet implemented
-        "Open-MSSQL": open_mssql,  # TODO not yet implemented
+        "DNS-open-resolvers": dns_open_resolvers, # TODO Check implementation.
+        "Open-Elasticsearch": open_elasticsearch,  # TODO Check implementation.
+        "Open-Net BIOS": open_net_bios,  # TODO Check implementation.
+        "Open-Mongo DB": open_mongo_db,  # TODO Check implementation.
+        "Open-MSSQL": open_mssql,  # TODO Check implementation.
         "Open-SNMP": open_snmp,  # TODO not yet implemented
         "Open-SSDP": open_ssdp,  # TODO not yet implemented
         "Open-IPMI": open_ipmi,  # TODO not yet implemented
@@ -50,6 +53,147 @@ def get_feed(feedname):
 
 def add_UTC_to_timestamp(value):
     return value + ' UTC'
+
+# https://www.shadowserver.org/wiki/pmwiki.php/Services/Open-MSSQL
+open_mssql = {
+    'required_fields': [
+        ('time.source', 'timestamp', add_UTC_to_timestamp),
+        ('source.ip', 'ip'),
+        ('source.port', 'port') # TODO:  check if this is really the source.port!
+    ],
+    'optional_fields': [
+        ('protocol.transport', 'protocol'),
+        ('source.reverse_dns', 'hostname'),
+        ('source.asn', 'asn'),
+        ('source.geolocation.cc', 'geo'),
+        ('source.geolocation.region', 'region'),
+        ('source.geolocation.city', 'city'),
+        ('source.local_hostname', 'server_name')
+        # Other known fields  which will go into "extra"
+        # tag
+        # version
+        # naics
+        # sic
+        # instance_name
+        # tcp_port  # TODO:  is this the source.port?
+        # named_pipe
+        # response_lenght
+        # sector
+    ],
+    'classification_type': 'Vulnerable Service'
+}
+
+# https://www.shadowserver.org/wiki/pmwiki.php/Services/Open-MongoDB
+open_mongo_db = {
+    'required_fields': [
+        ('time.source', 'timestamp', add_UTC_to_timestamp),
+        ('source.ip', 'ip'),
+        ('source.port', 'port')
+    ],
+    'optional_fields': [
+        ('protocol.transport', 'protocol'),
+        ('source.reverse_dns', 'hostname'),
+        ('source.asn', 'asn'),
+        ('source.geolocation.cc', 'geo'),
+        ('source.geolocation.region', 'region'),
+        ('source.geolocation.city', 'city'),
+        ('source.account', 'username'),
+        # Other known fields  which will go into "extra"
+        # tag
+        # version
+        # naics
+        # sic
+        # gitversion
+        # sysinfo
+        # opensslversion
+        # allocator
+        # javascriptengine
+        # bits
+        # maxbsonobjectsize
+        # ok
+        # visible_databases
+        # sector
+    ],
+    'classification_type': 'Vulnerable Service'
+}
+
+# https://www.shadowserver.org/wiki/pmwiki.php/Services/Open-NetBIOS
+open_net_bios = {
+    'required_fields': [
+        ('time.source', 'timestamp', add_UTC_to_timestamp),
+        ('source.ip', 'ip'),
+        ('source.port', 'port')
+    ],
+    'optional_fields': [
+        ('protocol.transport', 'protocol'),
+        ('source.reverse_dns', 'hostname'),
+        ('source.asn', 'asn'),
+        ('source.geolocation.cc', 'geo'),
+        ('source.geolocation.region', 'region'),
+        ('source.geolocation.city', 'city'),
+        ('source.account', 'username'),
+        ('source.local_hostname', 'machine_name')
+        # Other known fields  which will go into "extra"
+        # tag
+        # mac_address
+        # workgroup
+    ],
+    'classification_type': 'Vulnerable Service'
+}
+
+# https://www.shadowserver.org/wiki/pmwiki.php/Services/Open-Elasticsearch
+open_elasticsearch = {
+    'required_fields': [
+        ('time.source', 'timestamp', add_UTC_to_timestamp),
+        ('source.ip', 'ip'),
+        ('source.port', 'port')
+    ],
+    'optional_fields': [
+        ('source.asn', 'asn'),
+        ('source.geolocation.cc', 'geo'),
+        ('source.geolocation.region', 'region'),
+        ('source.geolocation.city', 'city'),
+        ('protocol.transport', 'protocol'),
+        ('source.reverse_dns', 'hostname'),
+        # Other known fields  which will go into "extra"
+        # min_amplification
+        # dns_version
+        # p0f_genre
+        # p0f_detail
+    ],
+    'classification_type': 'exploit'  # TODO
+}
+
+# https://www.shadowserver.org/wiki/pmwiki.php/Services/DNS-open-resolvers
+dns_open_resolvers =  {
+    'required_fields': [
+        ('time.source', 'timestamp', add_UTC_to_timestamp),
+        ('source.ip', 'ip'),
+        ('source.port', 'port')
+    ],
+    'optional_fields': [
+        ('protocol.transport', 'protocol'),
+        ('source.reverse_dns', 'hostname'),
+        ('source.asn', 'asn'),
+        ('source.geolocation.cc', 'geo'),
+        ('source.geolocation.region', 'region'),
+        ('source.geolocation.city', 'city'),
+        # Other known fields which will go into "extra"
+        # elasticsearch
+        # version
+        # naics
+        # sic
+        # ok
+        # name
+        # cluster_name
+        # status
+        # build_hash
+        # build_timestamp
+        # build_snaphost
+        # lucene_version
+    ],
+    'classification_type': 'exploit'  # TODO
+}
 
 # https://www.shadowserver.org/wiki/pmwiki.php/Services/NTP-Monitor
 ntp_monitor = {
