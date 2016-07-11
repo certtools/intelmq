35c0d52e03111af3f71f96b4779390ebd048edc8
Raphaël Vinot <raphael@vinot.info>
2016-07-11 18:37:58 +0200

    Cleanup & use PyMISP
    * PyMISP now has helpers to add and remove tags, use them and remove extra code
    * Avoid crash of the parser if the event has no tags.

--- intelmq/bots/collectors/misp/collector.py
+++ intelmq/bots/collectors/misp/collector.py
@@ -5,6 +5,7 @@ A collector for grabbing appropriately tagged events from MISP.
 Parameters:
   - misp_url: URL of the MISP server
   - misp_key: API key for accessing MISP
+  - misp_verify: true or false, check the validity of the certificate
   - misp_tag_to_process: MISP tag identifying events to be processed
   - misp_tag_processed: MISP tag identifying events that have been processed
 
@@ -13,7 +14,6 @@ import json
 import sys
 from urllib.parse import urljoin
 
-import requests
 from pymisp import PyMISP
 
 from intelmq.lib.bot import Bot
@@ -44,13 +44,12 @@ class MISPCollectorBot(Bot):
         if 'response' in misp_result:
 
             # Extract the MISP event details
-            misp_events = list()
-            for result in misp_result['response']:
-                misp_events.append(result['Event'])
+            misp_events = misp_result['response']
+            misp_events_details = [e['Event'] for e in misp_events]
 
             # Send the results to the parser
             report = Report()
-            report.add('raw', json.dumps(misp_events, sort_keys=True))
+            report.add('raw', json.dumps(misp_events_details, sort_keys=True))
             report.add('feed.name', self.parameters.feed)
             report.add('feed.url', self.parameters.misp_url)
             report.add('feed.accuracy', self.parameters.accuracy)
@@ -60,33 +59,14 @@ class MISPCollectorBot(Bot):
             # Note PyMISP does not currently support this so we use
             # the API URLs directly with the requests module.
 
-            session = requests.Session()
-            session.headers.update({
-                'Authorization': self.misp.key,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            })
-            post_data = {
-                'request': {
-                    'Event': {
-                        'tag': None,
-                        'id': None,
-                    }
-                }
-            }
-
             for misp_event in misp_events:
-                post_data['request']['Event']['id'] = misp_event['id']
-
                 # Remove the 'to be processed' tag
-                tag = self.parameters.misp_tag_to_process
-                post_data['request']['Event']['tag'] = tag
-                session.post(self.misp_del_tag_url, data=json.dumps(post_data))
+                self.misp.remove_tag(misp_event,
+                                     self.parameters.misp_tag_to_process)
 
                 # Add a 'processed' tag to the event
-                tag = self.parameters.misp_tag_processed
-                post_data['request']['Event']['tag'] = tag
-                session.post(self.misp_add_tag_url, data=json.dumps(post_data))
+                self.misp.add_tag(misp_event,
+                                  self.parameters.misp_tag_processed)
 
 
 if __name__ == '__main__':
