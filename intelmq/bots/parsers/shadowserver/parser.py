c90ff5701c46d6e570cd5074f71464778cccaaea
Sebastian Wagner <sebix@sebix.at>
2016-07-25 16:26:59 +0200

    BUG: shadowserver parser: correct format for dumps
    fixes certtools/intelmq#600
    
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/bots/parsers/shadowserver/parser.py
+++ intelmq/bots/parsers/shadowserver/parser.py
@@ -195,7 +195,8 @@ class ShadowserverParserBot(ParserBot):
 
     def recover_line(self, line):
         out = self.header + '\n'
-        ordered_line = [line[v] for v in self.fieldnames]
+        ordered_line = [self.conv_csv_shadowserver(line[v]) for v in
+                        self.fieldnames]
         out += ','.join(ordered_line) + '\n'
         return out
 
