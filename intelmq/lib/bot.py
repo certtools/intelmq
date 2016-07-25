9739d9622c6941ff2f5e1ed53d6140d9c0c00410
Sebastian Wagner <sebix@sebix.at>
2016-07-25 15:52:54 +0200

    BUG: ParserBot: Message dumping
    fixes certtools/intelmq#590
    fixes certtools/intelmq#603
    
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/lib/bot.py
+++ intelmq/lib/bot.py
@@ -453,14 +453,16 @@ class ParserBot(Bot):
             try:
                 # filter out None
                 events = list(filter(bool, self.parse_line(line, report)))
-            except Exception as exc:
+            except Exception:
                 self.logger.exception('Failed to parse line.')
-                self.__failed.append((exc, line))
+                self.__failed.append((traceback.format_exc(), line))
             else:
                 self.send_message(*events)
 
         for exc, line in self.__failed:
-            self._dump_message(exc, self.recover_line(line))
+            report_dump = report.copy()
+            report_dump.update('raw', self.recover_line(line))
+            self._dump_message(exc, report_dump)
 
         self.acknowledge_message()
 
