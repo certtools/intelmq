5e1e6f3b9fffaa261dd1e40f7fc29aa0b4774712
Sebastian Wagner <sebix@sebix.at>
2016-09-13 17:43:22 +0200

    ENH: tcp bot revised, new param separator
    see also certtools/intelmq#673
    
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/bots/outputs/tcp/output.py
+++ intelmq/bots/outputs/tcp/output.py
@@ -1,50 +1,46 @@
 # -*- coding: utf-8 -*-
 import socket
 import sys
-import time
 
 import intelmq.lib.utils as utils
 from intelmq.lib.bot import Bot
 
 
-class TCPBot(Bot):
+class TCPOutputBot(Bot):
+
+    def init(self):
+        self.address = (self.parameters.ip, int(self.parameters.port))
+        self.separator = utils.encode(self.parameters.separator)
+        self.connect()
 
     def process(self):
         event = self.receive_message()
 
         data = event.to_json()
-        self.send_data(data)
-        self.acknowledge_message()
+        try:
+            self.con.send(utils.encode(data))
+            self.con.sendall(self.separator)
+        except socket.error as exc:
+            self.logger.exception(exc.args[1] + ". Reconnecting..")
+            self.con.close()
+            self.connect()
+        except AttributeError:
+            self.logger.info('Reconnecting.')
+            self.connect()
+        else:
+            self.acknowledge_message()
 
     def connect(self):
-        address = (self.parameters.ip, int(self.parameters.port))
         self.con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        while True:
-            try:
-                self.con.connect(address)
-                break
-            except socket.error as exc:
-                self.logger.error(exc.args[1] + ". Retrying in 10 seconds.")
-                time.sleep(10)
-
-        self.logger.info("Connected successfully to {!s}: {}"
-                         "".format(address[0], address[1]))
-
-    def send_data(self, data):
-        while True:
-            try:
-                self.con.send(utils.encode(data))
-                self.con.sendall(b"")
-                break
-            except socket.error as exc:
-                self.logger.error(exc.args[1] + ". Reconnecting..")
-                self.con.close()
-                self.connect()
-            except AttributeError:
-                self.connect()
+        try:
+            self.con.connect(self.address)
+        except:
+            raise
+        else:
+            self.logger.info("Connected successfully to {!s}: {}"
+                             "".format(self.address[0], self.address[1]))
 
 
 if __name__ == "__main__":
-    bot = TCPBot(sys.argv[1])
+    bot = TCPOutputBot(sys.argv[1])
     bot.start()
