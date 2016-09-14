6086cacf17ff6b1d8070b596a7a1f58690502881
Sebastian Wagner <sebix@sebix.at>
2016-09-14 12:40:27 +0200

    BUG: revise mongodb output
    see also certtools/intelmq#673
    
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/bots/outputs/mongodb/output.py
+++ intelmq/bots/outputs/mongodb/output.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""
+pymongo library automatically tries to reconnect if connection has been lost
+"""
 import sys
 
 from intelmq.lib.bot import Bot
@@ -9,25 +12,42 @@ except ImportError:
     pymongo = None
 
 
-class MongoDBBot(Bot):
+class MongoDBOutputBot(Bot):
 
     def init(self):
         if pymongo is None:
             self.logger.error('Could not import pymongo. Please install it.')
             self.stop()
 
-        client = pymongo.MongoClient(self.parameters.host,
-                                     int(self.parameters.port))
-        db = client[self.parameters.database]
-        self.collection = db[self.parameters.collection]
+        self.connect()
+
+    def connect(self):
+        self.logger.info('Connecting to mongodb server.')
+        try:
+            self.client = pymongo.MongoClient(self.parameters.host,
+                                              int(self.parameters.port))
+        except pymongo.errors.ConnectionFailure:
+            raise ValueError('Connection to mongodb server failed.')
+        else:
+            db = self.client[self.parameters.database]
+            self.collection = db[self.parameters.collection]
+            self.logger.info('Successfully connected to mongodb server.')
 
     def process(self):
         event = self.receive_message()
 
-        self.collection.insert(event.to_dict())
-        self.acknowledge_message()
+        try:
+            self.collection.insert(event.to_dict())
+        except pymongo.errors.AutoReconnect:
+            self.logger.error('Connection Lost. Connecting again.')
+            self.connect()
+        else:
+            self.acknowledge_message()
+
+    def shutdown(self):
+        self.client.close()
 
 
 if __name__ == "__main__":
-    bot = MongoDBBot(sys.argv[1])
+    bot = MongoDBOutputBot(sys.argv[1])
     bot.start()
