5c32f80f2867a7b85012e9ec7af1f0db535f8a07
Sebastian Wagner <sebix@sebix.at>
2016-09-19 15:45:33 +0200

    ENH: coll rt, blueiv: missing import fail on init
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/bots/collectors/blueliv/collector_crimeserver.py
+++ intelmq/bots/collectors/blueliv/collector_crimeserver.py
@@ -5,10 +5,18 @@ import sys
 
 from intelmq.lib.bot import CollectorBot
 from intelmq.lib.message import Report
-from sdk.blueliv_api import BluelivAPI
+
+try:
+    from sdk.blueliv_api import BluelivAPI
+except ImportError:
+    BluelivAPI = None
 
 
 class BluelivCrimeserverCollectorBot(CollectorBot):
+    def init(self):
+        if BluelivAPI is None:
+            self.logger.error('Could not import BluelivAPI. Please install it.')
+            self.stop()
 
     def process(self):
         self.logger.info("Downloading report through API")
