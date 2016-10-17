9870788cf03a8c43723a9291e6f16a6c3eecd1d9
Sebastian Wagner <sebix@sebix.at>
2016-10-17 11:24:17 +0200

    ENH: amqp output: enhanced error handling
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/bots/outputs/amqptopic/output.py
+++ intelmq/bots/outputs/amqptopic/output.py
@@ -14,25 +14,23 @@ class AMQPTopicBot(Bot):
     def init(self):
         self.connection = None
         self.channel = None
-        self.keep_raw_field = bool(self.parameters.keep_raw_field)
-        self.delivery_mode = int(self.parameters.delivery_mode)
+        self.keep_raw_field = self.parameters.keep_raw_field
+        self.delivery_mode = self.parameters.delivery_mode
         self.content_type = self.parameters.content_type
         self.exchange = self.parameters.exchange_name
-        self.require_confirmation = bool(self.parameters.require_confirmation)
-        self.durable = bool(self.parameters.exchange_durable)
+        self.require_confirmation = self.parameters.require_confirmation
+        self.durable = self.parameters.exchange_durable
         self.exchange_type = self.parameters.exchange_type
         self.connection_host = self.parameters.connection_host
-        self.connection_port = int(self.parameters.connection_port)
+        self.connection_port = self.parameters.connection_port
         self.connection_vhost = self.parameters.connection_vhost
-        self.connection_attempts = int(self.parameters.connection_attempts)
-        self.connection_heartbeat = int(self.parameters.connection_heartbeat)
         self.credentials = pika.PlainCredentials(self.parameters.username, self.parameters.password)
         self.connection_parameters = pika.ConnectionParameters(
             host=self.connection_host,
             port=self.connection_port,
             virtual_host=self.connection_vhost,
-            connection_attempts=self.connection_attempts,
-            heartbeat_interval=self.connection_heartbeat,
+            connection_attempts=self.parameters.connection_attempts,
+            heartbeat_interval=self.parameters.connection_heartbeat,
             credentials=self.credentials)
         self.routing_key = self.parameters.routing_key
         self.properties = pika.BasicProperties(
@@ -40,32 +38,37 @@ class AMQPTopicBot(Bot):
         self.connect_server()
 
     def connect_server(self):
-        self.logger.info('AMQP Connecting to {}:{}/{} '.format(self.connection_host,
-                                                               self.connection_port, self.connection_vhost))
+        self.logger.info('AMQP Connecting to {}:{}{}.'.format(self.connection_host,
+                                                              self.connection_port,
+                                                              self.connection_vhost))
         try:
             self.connection = pika.BlockingConnection(self.connection_parameters)
-        except (pika.exceptions.AMQPConnectionError,pika.exceptions.ProbableAuthenticationError):
-            self.logger.exception(
-                'AMQP connection to {}:{}/{} failled!!'.format(
-                    self.connection_host,
-                    self.connection_port,
-                    self.connection_vhost))
+        except pika.exceptions.ProbableAuthenticationError:
+            self.logger.error('AMQP authentication failed!')
+            raise
+        except pika.exceptions.ProbableAccessDeniedError:
+            self.logger.error('AMQP authentication for virtual host failed!')
+            raise
+        except pika.exceptions.AMQPConnectionError:
+            self.logger.error('AMQP connection failed!')
+            raise
         else:
-            self.logger.info('AMQP Connected!! to {}:{}/{} '.format(self.connection_host,
-                                                                    self.connection_port, self.connection_vhost))
+            self.logger.info('AMQP connection succesfull.')
             self.channel = self.connection.channel()
-            self.channel.exchange_declare(exchange=self.exchange, type=self.exchange_type, durable=self.durable)
+            try:
+                self.channel.exchange_declare(exchange=self.exchange, type=self.exchange_type,
+                                              durable=self.durable)
+            except pika.exceptions.ChannelClosed:
+                self.logger.error('Access to exchange refused.')
+                raise
             self.channel.confirm_delivery()
 
     def process(self):
         ''' Stop the Bot if cannot connect to AMQP Server after the defined connection attempts '''
 
-        try:
-            if self.connection.is_closed or self.channel.is_closed:
+        # self.connection and self.channel can be None
+        if getattr(self.connection, 'is_closed', None) or getattr(self.channel, 'is_closed', None):
                 self.connect_server()
-        except AttributeError:
-            self.logger.exception('Bad configuration or server unavailable! Exiting...')
-            self.stop()
 
         event = self.receive_message()
 
@@ -81,11 +84,13 @@ class AMQPTopicBot(Bot):
                                               body=event.to_json(),
                                               properties=self.properties,
                                               mandatory=True):
-                self.logger.error("Message sent but not confirmed")
                 if self.require_confirmation:
-                    raise NameError('Message sent but not confirmed')
-        except (pika.exceptions.ChannelError, pika.exceptions.AMQPChannelError, pika.exceptions.NackError):
-            self.logger.exception("Error publishing the message")
+                    raise ValueError('Message sent but not confirmed.')
+                else:
+                    self.logger.info('Message sent but not confirmed.')
+        except (pika.exceptions.ChannelError, pika.exceptions.AMQPChannelError,
+                pika.exceptions.NackError):
+            self.logger.exception('Error publishing the message.')
         else:
             self.acknowledge_message()
 
