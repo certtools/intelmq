dcdf799f73a737647727ff04a873daf2c095b8e0
Sebastian Wagner <sebix@sebix.at>
2016-10-18 10:45:08 +0200

    BUG: make dedup hashes backwards compatible
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/bots/experts/deduplicator/expert.py
+++ intelmq/bots/experts/deduplicator/expert.py
@@ -21,7 +21,9 @@ class DeduplicatorExpertBot(Bot):
                           for k in self.parameters.ignore_keys.split(','))
         message_hash = message.hash(ignore_keys)
 
-        if not self.cache.exists(message_hash):
+        old_hash = hash(int(message_hash, 16))
+
+        if not (self.cache.exists(message_hash) or self.cache.exists(old_hash)):
             self.cache.set(message_hash, 'hash')
             self.send_message(message)
 
