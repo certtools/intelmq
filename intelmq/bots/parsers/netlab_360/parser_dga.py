3f8667fee2d3e50c43a159e306f1d9374d3aa1e7
jgedeon120 <joe.gedeon@gmail.com>
2016-11-13 09:11:57 -0500

    Added netlab 360 parsers

---
+++ intelmq/bots/parsers/netlab_360/parser_dga.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+feed:
+http://data.netlab.360.com/feeds/dga/dga.txt
+format:
+suppobox        difficultdress.net      2016-11-12 11:58:56     2016-11-13 00:04:15
+reference:
+http://data.netlab.360.com/dga
+"""
+
+import sys
+from intelmq.lib import utils
+from intelmq.lib.bot import Bot
+from intelmq.lib.message import Event
+
+class Netlab360DGAParserBot(Bot):
+
+    def process(self):
+        report = self.receive_message()
+        raw_report = utils.base64_decode(report.get("raw"))
+
+        for row in raw_report.splitlines():
+            if row.startswith('#') or len(row) == 0:
+                continue
+
+            row_split = row.split('\t')
+
+            event = Event(report)
+
+            event.add('event_description.text', row_split[0].lower())
+            event.add('time.source', row_split[3] + " UTC")
+            event.add('destination.fqdn', row_split[1])
+            event.add('raw', row)
+            event.add('classification.type', 'malware')
+            event.add('event_description.url', 'http://data.netlab.360.com/dga')
+
+            self.send_message(event)
+        self.acknowledge_message()
+
+if __name__ == "__main__":
+    bot = Netlab360DGAParserBot(sys.argv[1])
+    bot.start()
---
+++ intelmq/bots/parsers/netlab_360/parser_magnitude.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+feed:
+http://data.netlab.360.com/feeds/ek/magnitude.txt
+format:
+Magnitude	1478939365	178.32.227.12	f57idayaa1.notsite.faith	http://f57idayaa1.notsite.faith/1c472e11922f6a0a6c20fa5996aa35f4
+Magnitude	1478907804	178.32.227.12	3w212q83hay0ao51fi.courtall.party	N/A
+reference:
+http://data.netlab.360.com/ek
+"""
+
+import sys
+from intelmq.lib import utils
+from intelmq.lib.bot import Bot
+from intelmq.lib.message import Event
+
+class Netlab360MagnitudeParserBot(Bot):
+
+    def process(self):
+        report = self.receive_message()
+        raw_report = utils.base64_decode(report.get("raw"))
+
+        for row in raw_report.splitlines():
+            if row.startswith('#') or len(row) == 0:
+                continue
+
+            row_split = row.split('\t')
+
+            event = Event(report)
+
+            event.add('event_description.text', row_split[0].lower())
+            event.add('time.source', row_split[1])
+            event.add('destination.ip', row_split[2])
+            event.add('destination.fqdn', row_split[3])
+            event.add('destination.url', row_split[4])
+            event.add('raw', row)
+            event.add('classification.type', 'exploit')
+            event.add('event_description.url', 'http://data.netlab.360.com/ek')
+
+            self.send_message(event)
+        self.acknowledge_message()
+
+if __name__ == "__main__":
+    bot = Netlab360MagnitudeParserBot(sys.argv[1])
+    bot.start()
