18396f0b3d75a7a02f19456fb053206c9c6fd591
jgedeon120 <joe.gedeon@gmail.com>
2016-11-14 12:25:59 -0500

    Updated Netlab 360 parsers

--- intelmq/bots/parsers/netlab_360/parser_dga.py
+++ intelmq/bots/parsers/netlab_360/parser_dga.py
@@ -1,42 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-feed:
-http://data.netlab.360.com/feeds/dga/dga.txt
-format:
-suppobox        difficultdress.net      2016-11-12 11:58:56     2016-11-13 00:04:15
-reference:
-http://data.netlab.360.com/dga
+See docs/Feeds.md
 """
 
 import sys
-from intelmq.lib import utils
-from intelmq.lib.bot import Bot
+
+from intelmq.lib.bot import ParserBot
+from intelmq.lib.harmonization import FQDN
 from intelmq.lib.message import Event
 
 
-class Netlab360DGAParserBot(Bot):
+class Netlab360DGAParserBot(ParserBot):
 
-    def process(self):
-        report = self.receive_message()
-        raw_report = utils.base64_decode(report.get("raw"))
-
-        for row in raw_report.splitlines():
-            if row.startswith('#') or len(row) == 0:
-                continue
-
-            row_split = row.split('\t')
-
+    def parse_line(self, line, report):
+        if line.startswith('#') or len(line) == 0:
+            self.tempdata.append(line)
+        else:
+            lvalue = line.split('\t')
             event = Event(report)
 
-            event.add('classification.identifier', row_split[0].lower())
-            event.add('time.source', row_split[3] + " UTC")
-            event.add('destination.fqdn', row_split[1])
-            event.add('raw', row)
+            event.add('classification.identifier', lvalue[0].lower())
+            event.add('time.source', lvalue[3] + " UTC")
+
+            if FQDN.is_valid(lvalue[1]):
+                event.add('source.fqdn', lvalue[1])
+            else:
+                event.add('source.ip', lvalue[1])
+
+            event.add('raw', line)
             event.add('classification.type', 'malware')
             event.add('event_description.url', 'http://data.netlab.360.com/dga')
 
-            self.send_message(event)
-        self.acknowledge_message()
+            yield event
 
 if __name__ == "__main__":
     bot = Netlab360DGAParserBot(sys.argv[1])
--- intelmq/bots/parsers/netlab_360/parser_magnitude.py
+++ intelmq/bots/parsers/netlab_360/parser_magnitude.py
@@ -1,46 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-feed:
-http://data.netlab.360.com/feeds/ek/magnitude.txt
-format:
-Magnitude	1478939365	178.32.227.12	f57idayaa1.notsite.faith	http://f57idayaa1.notsite.faith/1c472e11922f6a0a6c20fa5996aa35f4
-Magnitude	1478907804	178.32.227.12	3w212q83hay0ao51fi.courtall.party	N/A
-reference:
-http://data.netlab.360.com/ek
+See docs/Feeds.md
 """
 
 import sys
+
 from datetime import datetime
-from intelmq.lib import utils
-from intelmq.lib.bot import Bot
+from intelmq.lib.bot import ParserBot
+from intelmq.lib.harmonization import FQDN, IPAddress, URL
 from intelmq.lib.message import Event
 
 
-class Netlab360MagnitudeParserBot(Bot):
+class Netlab360MagnitudeParserBot(ParserBot):
 
-    def process(self):
-        report = self.receive_message()
-        raw_report = utils.base64_decode(report.get("raw"))
-
-        for row in raw_report.splitlines():
-            if row.startswith('#') or len(row) == 0:
-                continue
-
-            row_split = row.split('\t')
-
+    def parse_line(self, line, report):
+        if line.startswith('#') or len(line) == 0:
+            self.tempdata.append(line)
+        else:
+            lvalue = line.split('\t')
             event = Event(report)
 
-            event.add('classification.identifier', row_split[0].lower())
-            event.add('time.source', datetime.utcfromtimestamp(int(row_split[1])).strftime('%Y-%m-%dT%H:%M:%S+00:00'))
-            event.add('destination.ip', row_split[2])
-            event.add('destination.fqdn', row_split[3])
-            event.add('destination.url', row_split[4])
-            event.add('raw', row)
+            event.add('classification.identifier', lvalue[0].lower())
+            event.add('time.source',
+                      datetime.utcfromtimestamp(int(lvalue[1])).strftime('%Y-%m-%dT%H:%M:%S+00:00'))
+
+            if IPAddress.is_valid(lvalue[2]):
+                event.add('source.ip', lvalue[2])
+
+            if FQDN.is_valid(lvalue[3]):
+                event.add('source.fqdn', lvalue[3])
+
+            if URL.is_valid(lvalue[4]):
+                event.add('source.url', lvalue[4])
+
+            event.add('raw', line)
             event.add('classification.type', 'exploit')
             event.add('event_description.url', 'http://data.netlab.360.com/ek')
 
-            self.send_message(event)
-        self.acknowledge_message()
+            yield event
 
 if __name__ == "__main__":
     bot = Netlab360MagnitudeParserBot(sys.argv[1])
