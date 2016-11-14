a50eb8687e7eb51cb294eebf3c12f4eff070bb41
jgedeon120 <joe.gedeon@gmail.com>
2016-11-14 11:40:10 -0500

    Updated Bambenek parser_c2dommasterlist.py

--- intelmq/bots/parsers/bambenek/parser_c2dommasterlist.py
+++ intelmq/bots/parsers/bambenek/parser_c2dommasterlist.py
@@ -11,17 +11,20 @@ from intelmq.lib.message import Event
 
 class Bambenekc2dommasterlistParserBot(ParserBot):
     def parse_line(self, line, report):
-        lvalue = line.split(',')
-        event = Event(report)
+        if line.startswith('#'):
+            self.tempdata(line)
+        else:
+            lvalue = line.split(',')
+            event = Event(report)
 
-        event.add('source.fqdn', lvalue[0])
-        event.add('event_description.text', lvalue[1])
-        event.add('time.source', lvalue[2] + " UTC")
-        event.add('event_description.url', lvalue[3])
-        event.add('classification.type', 'c&c')
-        event.add('status', 'online')
-        event.add('raw', line)
-        yield event
+            event.add('source.fqdn', lvalue[0])
+            event.add('event_description.text', lvalue[1])
+            event.add('time.source', lvalue[2] + " UTC")
+            event.add('event_description.url', lvalue[3])
+            event.add('classification.type', 'c&c')
+            event.add('status', 'online')
+            event.add('raw', line)
+            yield event
 
 if __name__ == "__main__":
     bot = Bambenekc2dommasterlistParserBot(sys.argv[1])
