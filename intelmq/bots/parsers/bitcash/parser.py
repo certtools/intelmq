bfa17c95cf8f43d148ae99ed332d6fe8f2571567
jgedeon120 <joe.gedeon@gmail.com>
2016-11-21 21:03:37 -0500

    Correct pep8 issue.

--- intelmq/bots/parsers/bitcash/parser.py
+++ intelmq/bots/parsers/bitcash/parser.py
@@ -23,7 +23,8 @@ class BitcashBlocklistParserBot(ParserBot):
             if FQDN.is_valid(line[2]):
                 event.add('source.reverse_dns', line[2])
             event.add('classification.type', 'scanner')
-            event.add('event_description.text', 'IPs banned for serious abusing of Bitcash services (scanning, sniffing, harvesting, dos attacks)')
+            event.add('event_description.text', 'IPs banned for serious abusing of Bitcash services '
+                                                '(scanning, sniffing, harvesting, dos attacks)')
             event.add('raw', ','.join(line))
 
             yield event
