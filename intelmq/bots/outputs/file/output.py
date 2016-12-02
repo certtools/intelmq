8d1ec08ac5156a488833b114695721dfe2704c48
Franz Nemeth <franz.nemeth@fnemeth.net>
2016-12-02 21:02:09 +0100

    File output bot now checks if the specified file is actually stil present on the file system

--- intelmq/bots/outputs/file/output.py
+++ intelmq/bots/outputs/file/output.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import io
 import sys
+import os
 
 from intelmq.lib.bot import Bot
 
@@ -14,12 +15,21 @@ class FileBot(Bot):
 
     def process(self):
         event = self.receive_message()
+        
 
         event_data = event.to_json(hierarchical=self.parameters.hierarchical_output)
-        self.file.write(event_data)
-        self.file.write("\n")
-        self.file.flush()
-        self.acknowledge_message()
+
+        try:
+            os.stat(self.parameters.file)
+        except FileNotFoundError:
+            self.logger.debug("Reopening %r file." % self.parameters.file)
+            self.file = io.open(self.parameters.file, mode='at', encoding="utf-8")
+            self.logger.info("File %r is open." % self.parameters.file)
+        finally:
+            self.file.write(event_data)
+            self.file.write("\n")
+            self.file.flush()
+            self.acknowledge_message()
 
 
 if __name__ == "__main__":
