078c568d524effb715c6245b3fa614663bd261d5
Sebastian Wagner <sebix@sebix.at>
2016-12-09 11:27:06 +0100

    ENH: json parser: allow splitting at newlines

--- intelmq/tests/bots/parsers/json/test_parser.py
+++ intelmq/tests/bots/parsers/json/test_parser.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-
+import base64
+import os
 import unittest
 
 import intelmq.lib.test as test
 from intelmq.bots.parsers.json.parser import JSONParserBot
 
-EXAMPLE_REPORT = {"feed.name": "Test",
-                  "raw": "eyJmZWVkLm5hbWUiOiAiVGVzdCBmZWVkIiwgInJhdyI6ICJabTl2WW1GeUNnPT0iLCAiX190eXBl"
-                  "IjogIkV2ZW50IiwgInRpbWUub2JzZXJ2YXRpb24iOiAiMjAxNS0wMS0wMVQwMDowMDowMCswMDow"
-                  "MCIsICJjbGFzc2lmaWNhdGlvbi50eXBlIjogInVua25vd24ifQ==",
+ONELINE_REPORT = {"feed.name": "Test",
+                  "raw": "eyJmZWVkLm5hbWUiOiAiVGVzdCBmZWVkIiwgInJhdyI6ICJabTl2WW1GeUNnPT0iLCAiX190eXBlIjogIkV2ZW50IiwgInRpbWUub2JzZXJ2YXRpb24iOiAiMjAxNS0wMS0wMVQwMDowMDowMCswMDowMCIsICJjbGFzc2lmaWNhdGlvbi50eXBlIjogInVua25vd24ifQ==",
                   "__type": "Report",
                   "time.observation": "2016-10-10T00:00:00+00:00",
                   }
@@ -18,6 +17,26 @@ EXAMPLE_EVENT = {"feed.name": "Test feed",
                  "time.observation": "2015-01-01T00:00:00+00:00",
                  "classification.type": "unknown"
                  }
+with open(os.path.join(os.path.dirname(__file__), 'data.json'), 'rb') as fh:
+    RAW = base64.b64encode(fh.read()).decode()
+
+MULTILINE_REPORT = {"feed.name": "Test feed",
+                    "raw": RAW,
+                    "__type": "Report",
+                    }
+MULTILINE_EVENTS = [{"feed.name": "Test feed",
+                     "raw": 'eyJfX3R5cGUiOiAiRXZlbnQiLCAic291cmNlLmlwIjogIjEyNy4wLjAuMSIsICJjbGFzc2lmaWNhdGlvbi50eXBlIjogImMmYyJ9',
+                     "__type": "Event",
+                     "classification.type": "c&c",
+                     "source.ip": "127.0.0.1"
+                     },
+                    {"feed.name": "Test feed",
+                     "raw": 'eyJfX3R5cGUiOiAiRXZlbnQiLCAic291cmNlLmlwIjogIjEyNy4wLjAuMiIsICJjbGFzc2lmaWNhdGlvbi50eXBlIjogImMmYyJ9',
+                     "__type": "Event",
+                     "classification.type": "c&c",
+                     "source.ip": "127.0.0.2"
+                     },
+                    ]
 
 
 class TestJSONParserBot(test.BotTestCase, unittest.TestCase):
@@ -28,12 +47,20 @@ class TestJSONParserBot(test.BotTestCase, unittest.TestCase):
     @classmethod
     def set_bot(cls):
         cls.bot_reference = JSONParserBot
-        cls.default_input_message = EXAMPLE_REPORT
+        cls.default_input_message = ONELINE_REPORT
 
-    def test_report(self):
+    def test_oneline_report(self):
         """ Test if correct Event has been produced. """
         self.run_bot()
         self.assertMessageEqual(0, EXAMPLE_EVENT)
+
+    def test_multiline_report(self):
+        """ Test if correct Event has been produced. """
+        self.input_message = MULTILINE_REPORT
+        self.sysconfig = {"splitlines": True}
+        self.run_bot()
+        self.assertMessageEqual(0, MULTILINE_EVENTS[0])
+        self.assertMessageEqual(1, MULTILINE_EVENTS[1])
 
 
 if __name__ == '__main__':  # pragma: no cover
