cb59b77e9071d50b6323a9a0bd14a33a4fa84546
Sascha Wilde <wilde@intevation.de>
2017-01-27 11:51:29 +0100

    Fixed path to modify bot example configurations.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     package_data={'intelmq': [
         'etc/*.conf',
         'bots/BOTS',
-        'bots/experts/modify/*.conf',
+        'bots/experts/modify/examples/*.conf',
     ]
     },
     include_package_data=True,
