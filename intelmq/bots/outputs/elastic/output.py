b20dadda16f54a8e2a9a6dbff89250d75874b78d
navtej <nsbuttar@hotmail.com>
2017-03-09 16:46:49 +0530

    fix pep8 errors

--- intelmq/bots/outputs/elastic/output.py
+++ intelmq/bots/outputs/elastic/output.py
@@ -7,8 +7,11 @@ from elasticsearch import Elasticsearch
 
 from intelmq.lib.bot import Bot
 
-#TODO: recursion depth check
-def replace_keys(obj, key_char = '.', replacement = '_'):
+#
+# TODO: recursion depth check
+
+
+def replace_keys(obj, key_char='.', replacement='_'):
     if isinstance(obj, Mapping):
         replacement_obj = {}
         for key, val in obj.items():
@@ -17,36 +20,36 @@ def replace_keys(obj, key_char = '.', replacement = '_'):
         return replacement_obj
     return obj
 
+
 class ElasticsearchOutputBot(Bot):
 
     def init(self):
         self.elastic_host = getattr(self.parameters,
-                                    'elastic_host','127.0.0.1')
+                                    'elastic_host', '127.0.0.1')
         self.elastic_port = getattr(self.parameters,
-                                    'elastic_port','9200')
+                                    'elastic_port', '9200')
         self.elastic_index = getattr(self.parameters,
                                      'elastic_index', 'intelmq')
         self.elastic_doctype = getattr(self.parameters,
-                                     'elastic_doctype', 'events')
+                                       'elastic_doctype', 'events')
         self.sanitize_keys = getattr(self.parameters,
                                      'sanitize_keys', True)
         self.replacement_char = getattr(self.parameters,
                                         'replacement_char', '_')
-        self.es = Elasticsearch([
-                    {'host': self.elastic_host, 'port': self.elastic_port}
-                ])
+        self.es = Elasticsearch([{'host': self.elastic_host,
+                                  'port': self.elastic_port}])
         if not self.es.indices.exists(self.elastic_index):
-            self.es.indices.create(index = self.elastic_index, ignore=400)
+            self.es.indices.create(index=self.elastic_index, ignore=400)
 
     def process(self):
         event = self.receive_message()
         event_dict = event.to_dict(hierarchical=False)
         if self.sanitize_keys:
             event_dict = replace_keys(event_dict,
-                                      replacement = self.replacement_char)
-        self.es.index(index = self.elastic_index,
-                      doc_type = self.elastic_doctype,
-                      body = event_dict)
+                                      replacement=self.replacement_char)
+        self.es.index(index=self.elastic_index,
+                      doc_type=self.elastic_doctype,
+                      body=event_dict)
         self.acknowledge_message()
 
 
