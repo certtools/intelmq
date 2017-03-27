3ad6815aff79db9da20a03a8821d1077b2fdd6f8
Dustin Demuth <dustin@intevation.de>
2017-03-27 11:16:59 +0200

    Added Ripe-Download-Script to Setup

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setup(
         'intelmq/bots/experts/tor_nodes/update-tor-nodes',
         'intelmq/bots/experts/maxmind_geoip/update-geoip-data',
         'intelmq/bots/experts/asn_lookup/update-asn-data',
+        'intelmq/bots/experts/certbund_contact/ripe_download',
         'intelmq/bots/experts/certbund_contact/ripe_import.py',
         'intelmq/bots/experts/certbund_contact/ripe_diff.py',
         'intelmq/bots/experts/certbund_contact/add_inhibition.py',
