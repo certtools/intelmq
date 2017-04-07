7a5e81404d6a78dbc0839eb4dd55aedaa7742f7b
Dustin Demuth <dustin@intevation.de>
2017-04-07 16:29:22 +0200

    Added an notification-rule for Avalanche Feed Data.
     - The Rule makes use of organisation-tags
     - The Rule uses different Kinds of aggregation:
     -- Per CC
     -- Per ASN
     -- Per CIDR

--- intelmq/bots/experts/certbund_contact/rulesupport.py
+++ intelmq/bots/experts/certbund_contact/rulesupport.py
@@ -409,7 +409,7 @@ class Context:
 def most_specific_matches(context):
     """Return the most specific matches from the context.
 
-    Which matches are considered more specifc depends on which
+    Which matches are considered more specific depends on which
     attributes of the event matched and whether the matching entries in
     the contact database are manually managed or automatically.
 
@@ -430,7 +430,7 @@ def most_specific_matches(context):
             return by_managed["manual"] or by_managed["automatic"]
 
     return (get_preferred_by_field("fqdn") |
-            (get_preferred_by_field("ip") or get_preferred_by_field("asn")))
+           (get_preferred_by_field("ip") or get_preferred_by_field("asn") or get_preferred_by_field("geolocation.cc")))
 
 
 def keep_most_specific_contacts(context):
