8be4c0ea19144ecffc5fb3ce8efc1624623d982f
Sebastian Wagner <wagner@cert.at>
2017-04-10 19:12:13 +0200

    ENH: msdcu parser: proxy support

--- intelmq/bots/collectors/microsoft/collector_azure.py
+++ intelmq/bots/collectors/microsoft/collector_azure.py
@@ -6,6 +6,7 @@ Tested with 0.33, probably works with >0.30 too.
 import datetime
 import gzip
 import io
+import urllib.parse
 
 import pytz
 
@@ -22,16 +23,25 @@ class MicrosoftAzureCollectorBot(CollectorBot):
         if azure is None:
             raise ValueError('Could not import azure.storage. Please install it.')
 
+        if hasattr(self.parameters, 'https_proxy'):
+            parsed = urllib.parse.urlparse(self.parameters.https_proxy)
+            self.proxy = {'host': parsed.hostname, 'port': parsed.port,
+                          'user': parsed.username, 'password': parsed.password}
+        else:
+            self.proxy = None
+
     def process(self):
         storage_client = azure.storage.CloudStorageAccount(self.parameters.account_name,
                                                            self.parameters.account_key)
         blob_service = storage_client.create_block_blob_service()
+        if self.proxy:
+            blob_service.set_proxy(**self.proxy)
         containers = blob_service.list_containers()
         for container in containers:
             self.logger.info('Processing Container %r.' % container.name)
             if container.name == 'heartbeat':
                 if self.parameters.delete:
-                        blob_service.delete_container(container.name)
+                    blob_service.delete_container(container.name)
                 continue
             time_container_fetch = datetime.datetime.now(pytz.timezone('UTC'))
             for blob in blob_service.list_blobs(container.name):
