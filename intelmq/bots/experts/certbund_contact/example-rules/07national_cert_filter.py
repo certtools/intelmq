e3f8601d287ba161734c513b6990f50690e15530
Bernhard Herzog <bernhard.herzog@intevation.de>
2017-04-13 18:54:53 +0200

    Refactor example-rules/07national_cert_filter.py
    The new version is more compact and makes the structure of the code more
    obvious with fewer data structures that are modified in-place. It is a
    bit more advanced with respect to the python features it uses, though.

--- intelmq/bots/experts/certbund_contact/example-rules/07national_cert_filter.py
+++ intelmq/bots/experts/certbund_contact/example-rules/07national_cert_filter.py
@@ -13,51 +13,52 @@ def determine_directives(context):
 
     cc_field = context.section + ".geolocation.cc"
 
-    # Only run this script, if source or destination .geolocation.cc are 
-    # FILTER_CERT
+    # If the event is not related to the the country FILTER_CERT as
+    # indicated by the cc_field, we don't need to do anything and exit
+    # early.
     if context.get(cc_field) != FILTER_CERT:
-       return 
+        return
 
-    
+    # Now look at all matches for the geolocation.cc field and remove
+    # all organisations referenced by such matches that do not have a
+    # FILTER_ANNOTATION annotation. Keep the matches for other fields.
+    #
+    # Strategy: Build a new list of match objects by iterating over all
+    # match objects and adding the match unchanged if it's not a match
+    # for geolocation.cc or otherwise removing the organisations without
+    # the relevant annotation from match before adding it (and only
+    # adding it if any organsition remains)
+
     new_matches = []
 
-    # iterate the matches.
     for match in context.matches:
-        # Only take the datasets, which match on the field geolocation.cc
         if match.field == 'geolocation.cc':
-            # This indicates a candidate where REMOVAL of the Organisation
-            # from the match migth be required...
-            orgs = context.organisations_for_match(match)
-            kept_org_ids = set()
+            # It's a geolocation match, so replace its list of
+            # organisation IDs with a list of the IDs of all
+            # organisations referenced by the match that have at least
+            # one annotation tag equal to FILTER_ANNOTATION ...
+            match.organisations = \
+                [org.orgid
+                 for org in context.organisations_for_match(match)
+                 if any(a.tag == FILTER_ANNOTATION for a in org.annotations)]
 
-            # Iterate the orgs.
-            for org in orgs:
-                annos = org.annotations
-                for a in annos:
-                   if a.tag == FILTER_ANNOTATION: 
-                       # only keep those organisations in this match, if the 
-                       # organisations has the FILTER_ANNOTATION annotation.
-                       kept_org_ids.add(org.orgid)
-                       break
-
-            # Overwrite the organisations of this match with those we decided to keep
-            match.organisations = kept_org_ids
-
-            # Decide if this Match shall be kept at all: If no Orgs exist, we don't need to.
+            # .. and append it to the new list of matches if any
+            # organisations remain
             if match.organisations:
                 new_matches.append(match)
 
         else:
-          # this match is not relevant....
-          new_matches.append(match)
+            # It's not a geolocation match, so append it to the new list
+            # of matches unchanged.
+            new_matches.append(match)
 
 
-    # Overwrite the contexts matches
+    # Overwrite the context's matches with the new list
     context.matches = new_matches
-    
+
     context.logger.debug("Content of the Context AFTER this script:")
     context.logger.debug("Organisations %r" % context.organisations)
     context.logger.debug("Matches %r" % context.matches)
-    
-    # We'll return None, as other scripts shall still run....
-    return None 
+
+    # Return None, because other scripts still have to run
+    return None
