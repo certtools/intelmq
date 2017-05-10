e30b9ca3b91083e0515624226359b488f11f5719
Sebastian Wagner <wagner@cert.at>
2017-05-10 15:05:28 +0200

    BUG: shadowserver parser: remove null byte
    temporary fix for certtools/intelmq#967

--- intelmq/bots/parsers/shadowserver/parser.py
+++ intelmq/bots/parsers/shadowserver/parser.py
@@ -49,6 +49,8 @@ class ShadowserverParserBot(ParserBot):
 
     def parse(self, report):
         raw_report = utils.base64_decode(report["raw"])
+        # Temporary fix for https://github.com/certtools/intelmq/issues/967
+        raw_report = raw_report.translate({0: None})
         csvr = csv.DictReader(io.StringIO(raw_report))
 
         # create an array of fieldnames,
