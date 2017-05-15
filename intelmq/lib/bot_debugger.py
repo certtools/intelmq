0c217f5ed69ffd8ff43f484cf43d9d41a2d2608e
Edvard Rejthar <edvard.rejthar@nic.cz>
2017-05-15 20:59:56 +0200

    working on feedback, pull 973

--- intelmq/lib/bot_debugger.py
+++ intelmq/lib/bot_debugger.py
@@ -14,21 +14,21 @@ Depending on the subcommand received, the class either
 """
 import logging
 import json
+from pprint import pprint
 
-from intelmq.lib.message import Event
 from importlib import import_module
-from intelmq.lib.utils import StreamHandler
-from intelmq.lib.message import Event
+from intelmq.lib.utils import StreamHandler, error_message_from_exc
+from intelmq.lib.message import Event, MessageFactory
 
 
 class BotDebugger:
-        def leverageLogger(self, level = logging.DEBUG):
+        def leverageLogger(self, level=logging.DEBUG):
             self.instance.logger.setLevel(level)
             for h in self.instance.logger.handlers:
                 if isinstance(h, StreamHandler):
                     h.setLevel(level)
 
-        def __init__(self, module_path, bot_id, run_subcommand = None, message_kind = None, dryrun = None, msg = None):
+        def __init__(self, module_path, bot_id, run_subcommand=None, message_kind=None, dryrun=None, msg=None):        
             module = import_module(module_path)
             bot = getattr(module, 'BOT')
             self.instance = bot(bot_id)
@@ -41,7 +41,7 @@ class BotDebugger:
                 if run_subcommand == "process":
                     self._process(dryrun, msg)
                 elif run_subcommand == "message":
-                    self.leverageLogger(level = logging.INFO)
+                    self.leverageLogger(level=logging.INFO)
                     self._message(message_kind, msg)
                 else:
                     print("Subcommand {} not known.".format(run_subcommand))
@@ -57,25 +57,27 @@ class BotDebugger:
                 print("Dryrun only, no message will be really sent through.")
 
             self.instance.logger.info("Processing...")
-            self.instance.process() # normal run - bot loads and parses its next msg from pipeline
+            self.instance.process()
+
+        class PoorBot:
+            pass
 
         def _message(self, message_action_kind, msg):
             if message_action_kind == "get":
                 self.instance.logger.info("Trying to get the message...")
-                msg = self.instance.receive_message()
-                print(msg)
+                pprint(self.instance.receive_message())
             elif message_action_kind == "pop":
                 self.instance.logger.info("Trying to pop the message...")
-                print(self.instance.receive_message())
+                pprint(self.instance.receive_message())
                 self.instance.acknowledge_message()
-            elif message_action_kind == "send":
+            elif message_action_kind == "send":                
                 if msg:
                     try:
-                        msg = Event(json.loads(msg))
-                    except:
-                        print("Wrong formatted msg.")
+                        msg = MessageFactory.unserialize(msg)
+                    except (Exception, KeyError, TypeError, json.JSONDecodeError) as exc:                        
+                        print("Message can not be parsed from JSON: " + error_message_from_exc(exc))
                         return
                     self.instance.send_message(msg)
-                    self.instance.logger.info("Message send to output pipelines.")
+                    self.instance.logger.info("Message sent to output pipelines.")
                 else:
                     self.instance.logger.info("Message missing!")
