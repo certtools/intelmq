db85e2b09837ac87e46da43d11c25eef434641c6
Edvard Rejthar <edvard.rejthar@nic.cz>
2017-05-16 16:55:49 +0200

    console argument, get message without popping to source q

--- intelmq/bin/intelmqctl.py
+++ intelmq/bin/intelmqctl.py
@@ -118,7 +118,7 @@ class IntelMQProcessManager:
                 self.logger.error('Directory %s does not exist and cannot be '
                                   'created: %s.', self.PIDDIR, exc)
 
-    def bot_run(self, bot_id, run_subcommand=None, message_action_kind=None, dryrun=None, msg=None):
+    def bot_run(self, bot_id, run_subcommand=None, console_type=None, message_action_kind=None, dryrun=None, msg=None):
         pid = self.__read_pidfile(bot_id)
         if pid and self.__status_process(pid):
             paused = True
@@ -132,7 +132,7 @@ class IntelMQProcessManager:
             fp.write(str(os.getpid()))
 
         try:
-            BotDebugger(self.__runtime_configuration[bot_id]['module'], bot_id, run_subcommand, message_action_kind, dryrun, msg)
+            BotDebugger(self.__runtime_configuration[bot_id]['module'], bot_id, run_subcommand, console_type, dryrun, message_action_kind, msg)
             retval = 0
         except KeyboardInterrupt:
             print('Keyboard interrupt.')
@@ -309,8 +309,9 @@ class IntelMQController():
         intelmqctl [start|stop|restart|status|reload]
         intelmqctl list [bots|queues]
         intelmqctl log bot-id [number-of-lines [log-level]]
-        intelmqctl run bot-id message [get|pop|send --msg]
+        intelmqctl run bot-id message [get|pop|send]
         intelmqctl run bot-id process [--msg|--dryrun]
+        intelmqctl run bot-id console
         intelmqctl clear queue-id
         intelmqctl check
 
@@ -325,6 +326,8 @@ Get status of a bot:
 
 Run a bot directly for debugging purpose and temporarily leverage the logging level to DEBUG:
     intelmqctl run bot-id
+Get a pdb (or ipdb if installed) live console.
+    intelmqctl run bot-id console
 See the message that waits in the input queue.
     intelmqctl run bot-id message get
 See additional help for further explanation.
@@ -441,8 +444,11 @@ Outputs are additionally logged to /opt/intelmq/var/log/intelmqctl'''
             parser_run.add_argument('bot_id',
                                     choices=self.runtime_configuration.keys())
             parser_run_subparsers = parser_run.add_subparsers(title='run-subcommands')
+            parser_run_console = parser_run_subparsers.add_parser('console', help='Get a ipdb live console.')
+            parser_run_console.add_argument('console_type', nargs='?', help='You may specify which console should be run. Default is ipdb (if installed) or pudb (if installed) or pdb but you may want to use another one.')
+            parser_run_console.set_defaults(run_subcommand="console")
             parser_run_message = parser_run_subparsers.add_parser('message', help='Debug bot\'s pipelines. Get the message in the input pipeline, pop it (cut it) and display it, or send the message directly to bot\'s output pipeline.')
-            parser_run_message.add_argument('message_action_kind', choices=["get", "pop", "send"])
+            parser_run_message.add_argument('message_action_kind', choices = ["get","pop","send"])
             parser_run_message.add_argument('msg', nargs='?', help='If send was chosen, put here the message in JSON.')
             parser_run_message.set_defaults(run_subcommand="message")
             parser_run_process = parser_run_subparsers.add_parser('process', help='Single run of bot\'s process() method.')
@@ -537,8 +543,8 @@ Outputs are additionally logged to /opt/intelmq/var/log/intelmqctl'''
         elif results == 'error':
             return 1
 
-    def bot_run(self, bot_id, run_subcommand=None, message_action_kind=None, dryrun=None, msg=None):
-        return self.bot_process_manager.bot_run(bot_id, run_subcommand, message_action_kind, dryrun, msg)
+    def bot_run(self, **kwargs): #bot_id, run_subcommand=None, console_type=None, dryrun=None, message_action_kind=None, msg=None
+        return self.bot_process_manager.bot_run(**kwargs) #bot_id, run_subcommand, console_type, message_action_kind, dryrun, msg
 
     def bot_start(self, bot_id, getstatus=True):
         if bot_id is None:
--- intelmq/lib/bot_debugger.py
+++ intelmq/lib/bot_debugger.py
@@ -28,7 +28,7 @@ class BotDebugger:
                 if isinstance(h, StreamHandler):
                     h.setLevel(level)
 
-        def __init__(self, module_path, bot_id, run_subcommand=None, message_kind=None, dryrun=None, msg=None):
+        def __init__(self, module_path, bot_id, run_subcommand=None, console_type=None, dryrun=None, message_kind=None, msg=None):
             module = import_module(module_path)
             bot = getattr(module, 'BOT')
             self.instance = bot(bot_id)
@@ -41,10 +41,57 @@ class BotDebugger:
                 if run_subcommand == "process":
                     self._process(dryrun, msg)
                 elif run_subcommand == "message":
-                    self.leverageLogger(level=logging.INFO)
+                    # XXX self.leverageLogger(level=logging.INFO)
                     self._message(message_kind, msg)
+                elif run_subcommand == "console":
+                    self._console(console_type)
                 else:
                     print("Subcommand {} not known.".format(run_subcommand))
+
+        class PoorBot:
+            pass
+
+        def _console(self,console_type):
+            consoles = [console_type, "ipdb", "pudb", "pdb"]
+            for console in consoles:                
+                try:
+                    module = import_module(console)
+                except Exception as exc:                    
+                    pass
+                else:
+                    if console_type and console != console_type:
+                        print("Console {} not available.".format(console_type))
+                    print("*** Using console {}. Please use 'self' to access to the bot instance properties. ***".format(module.__name__))
+                    break
+            else:
+                print("Can't run console.")
+                return
+
+            self = self.instance
+            module.set_trace()
+
+        def _message(self, message_action_kind, msg):
+            if message_action_kind == "get":
+                self.instance.logger.info("Trying to get the message...")
+                # never pops from source to internal queue, thx to disabling brpoplpush operation
+                pl = self.instance._Bot__source_pipeline                
+                pl.pipe.brpoplpush = lambda source_q, inter_q, i: pl.pipe.lindex(source_q, -1)                
+                pprint(self.instance.receive_message())
+            elif message_action_kind == "pop":
+                self.instance.logger.info("Trying to pop the message...")
+                pprint(self.instance.receive_message())
+                self.instance.acknowledge_message()
+            elif message_action_kind == "send":                
+                if msg:
+                    try:
+                        msg = MessageFactory.unserialize(msg)
+                    except (Exception, KeyError, TypeError, json.JSONDecodeError) as exc:                        
+                        print("Message can not be parsed from JSON: " + error_message_from_exc(exc))
+                        return
+                    self.instance.send_message(msg)
+                    self.instance.logger.info("Message sent to output pipelines.")
+                else:
+                    self.instance.logger.info("Message missing!")
 
         def _process(self, dryrun, msg):
             if msg:
@@ -58,27 +105,3 @@ class BotDebugger:
 
             self.instance.logger.info("Processing...")
             self.instance.process()
-
-        class PoorBot:
-            pass
-
-        def _message(self, message_action_kind, msg):
-            if message_action_kind == "get":
-                self.instance.logger.info("Trying to get the message...")
-                pprint(self.instance.receive_message())
-            elif message_action_kind == "pop":
-                self.instance.logger.info("Trying to pop the message...")
-                pprint(self.instance.receive_message())
-                self.instance.acknowledge_message()
-            elif message_action_kind == "send":
-                if msg:
-                    try:
-                        msg = MessageFactory.unserialize(msg)
-                    except (Exception, KeyError, TypeError, json.JSONDecodeError) as exc:
-                        print("Message can not be parsed from JSON: " + error_message_from_exc(exc))
-                        return
-                    self.instance.send_message(msg)
-                    self.instance.logger.info("Message sent to output pipelines.")
-                else:
-                    self.instance.logger.info("Message missing!")
-                    
