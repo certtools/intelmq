0eb770cc1ca7f146974afd45a7fa8653c15171bd
navtej <nsbuttar@hotmail.com>
2017-05-30 13:25:16 +0530

    extract malware name from fraunhofer dga feed
    fixes certtools/intelmq#984

--- intelmq/bots/parsers/fraunhofer/parser_dga.py
+++ intelmq/bots/parsers/fraunhofer/parser_dga.py
@@ -17,17 +17,17 @@ class FraunhoferDGAParserBot(Bot):
         report = self.receive_message()
         dict_report = json.loads(utils.base64_decode(report.get("raw")))
 
-        # add all lists together, only one loop needed
-        for row in sum(dict_report.values(), []):
+        for key in dict_report:
+            malware_name = key.split('_')[0]
+            for row in dict_report[key]:
+                event = self.new_event(report)
+                event.add('classification.type', 'c&c')
+                event.add('malware.name', malware_name)
+                if not event.add('source.ip', row, raise_failure=False):
+                    event.add('source.fqdn', row)
+                event.add("raw", row)
+                self.send_message(event)
 
-            event = self.new_event(report)
-
-            event.add('classification.type', 'c&c')
-            if not event.add('source.ip', row, raise_failure=False):
-                event.add('source.fqdn', row)
-            event.add("raw", row)
-
-            self.send_message(event)
         self.acknowledge_message()
 
 
