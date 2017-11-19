124e64ba9fcc6f37cc71d3949910ec9b02d0a5ec
cgi1 <gies0r@gmail.com>
2017-11-19 19:47:51 +0100

    Adding certstrean (https://github.com/CaliDog/certstream-python)

---
+++ intelmq/bots/collectors/certstream/collector_certstream.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+    A Bot to collect data from the Certificate Transparency Log (CTL)
+    This bot works based on certstream libary (https://github.com/CaliDog/certstream-python)
+    @author: Florian Krenn, Christoph Giese (Telekom Security)
+    No parameters necessary.
+
+"""
+import json
+import sys
+import certstream
+
+from intelmq.lib.bot import Bot
+from intelmq.lib.message import Report
+
+
+class CertstreamCollectorBot(Bot):
+
+    def init(self):
+
+        self.logger.debug("Nothing to init.")
+
+    def callback(self, message, context=None):  # callback handler for certstream events.
+        CertstreamCollectorBot.send_update(message=message, self=self)
+
+    def process(self):
+        certstream.listen_for_events(self.callback)
+
+    def send_update(self, message):
+
+        # self.logger.debug("Received new certificate update.")
+        report = Report()
+
+        if message['message_type'] == 'heartbeat':
+            return
+
+        if message['message_type'] == 'certificate_update':
+            new_report = report.copy()
+            new_report.add("feed.name", "Certstream")
+            new_report.add("raw", json.dumps(message))
+
+            self.send_message(new_report)
+            self.logger.debug("Send certificate_update.")
+
+
+if __name__ == "__main__":
+    bot = CertstreamCollectorBot(sys.argv[1])
+    bot.start()
---
+++ intelmq/bots/parsers/certstream/parser_certstream.py
@@ -0,0 +1,63 @@
+"""
+A bot to parse certstream data.
+@author: Christoph Giese (Telekom Security, CDR)
+"""
+
+import json
+
+import sys
+import datetime
+
+from intelmq.lib import utils, exceptions
+from intelmq.lib.bot import Bot
+
+class CertStreamParserBot(Bot):
+
+    def process(self):
+        report = self.receive_message()
+
+        raw_report = utils.base64_decode(report.get('raw'))
+        try:
+
+            json_report = json.loads(raw_report)
+        except:
+            self.logger.exception("Failed to convert raw_report to json!")
+            return
+
+        if 'data' not in json_report:
+            self.logger.debug("No data to check..")
+            return
+        data = json_report['data']
+
+        event = self.new_event(report)
+        event.add('raw', raw_report)
+
+        if 'seen' in data:
+            event.add('time.source', datetime.datetime.fromtimestamp(int(data['seen'])).strftime('%Y-%m-%d %H:%M:%S UTC'))
+
+        # ToDo: Check if leaf_cert --> extensions --> subjectAltName is identical to all_domains
+        # ToDo: Check if leaf_cert --> extensions --> extendedKeyUsage is always for Web Server Authentication (if not filter)
+
+        if 'leaf_cert' in data:
+            if 'all_domains' in data['leaf_cert']:
+                for domain in data['leaf_cert']['all_domains']:
+
+                    try:
+                        event.add('source.fqdn', domain, force=True)
+                    except exceptions.InvalidValue:
+                        self.logger.debug("Invalid value (%s) in all_domains field." % domain)
+                    except:
+                        self.logger.exception('Other error adding (%s)!' % domain)
+                    self.logger.debug("Send domain (%s) from certificate chain." % domain)
+                    self.send_message(event)
+
+        if 'chain' in data:
+            for chain_element in data['chain']:
+                pass # ToDo: Add chains
+
+        self.acknowledge_message()
+
+
+if __name__ == "__main__":
+    bot = CertStreamParserBot(sys.argv[1])
+    bot.start()
