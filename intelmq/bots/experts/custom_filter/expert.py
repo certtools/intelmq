f638748daafd5510516a35327d6f4f2f4a7bf58b
Edvard Rejthar <edvard.rejthar@nic.cz>
2017-12-08 14:41:58 +0100

    custom-filter: network mask, thorough testing

--- intelmq/bots/experts/custom_filter/expert.py
+++ intelmq/bots/experts/custom_filter/expert.py
@@ -3,6 +3,7 @@ import sys
 from glob import glob
 from intelmq.lib.bot import Bot
 import json
+from netaddr import IPNetwork, IPAddress
 
 class CustomFilterExpertBot(Bot):    
     filters = {}
@@ -11,10 +12,20 @@ class CustomFilterExpertBot(Bot):
         includable = None
         for filterO in self.filters.values():
             conforming = False
-            for key, value in filterO["conditions"].items():                
+            for key, values in filterO["conditions"].items():                
                 try:                    
-                    if (isinstance(value, list) and message[key] in value) or value == message[key]: # filter passed                                                
-                        conforming = True
+                    if not isinstance(values, list):
+                        values = [values] # the values can be list or string
+                    if key in ["source.ip"]: # expand network masks
+                        for val in values:
+                            if ("/" in val and IPAddress(message[key]) in IPNetwork(val)) or message[key] == val:
+                                conforming = True
+                                break
+                    elif message[key] in values:
+                        conforming = True                                            
+                                          
+                    #if (isinstance(values, list) and message[key] in values) or values == message[key]: # filter passed                                                
+                    #    conforming = True
                     else:                        
                         conforming = False
                         break
@@ -34,7 +45,7 @@ class CustomFilterExpertBot(Bot):
             return True # none of the filters matched
         return includable 
 
-    def init(self):
+    def init(self):        
         """ Loads rules from .json """        
         for i, rule_file in enumerate(glob(self.parameters.rules_dir + "*.json")):
             with open(rule_file, 'r') as f:
@@ -45,9 +56,9 @@ class CustomFilterExpertBot(Bot):
         message = self.receive_message()        
         if message and self.meet_condition(message):
             self.send_message(message)
-            self.logger.error("PASSING message: " + repr(message) + ".") # .debug not displayed until an .error is called
+            self.logger.info("PASSING message: " + repr(message) + ".") # .debug not displayed until an .error is called
         else:
-            self.logger.error("Dropping message: " + repr(message) + ".")
+            self.logger.info("Dropping message: " + repr(message) + ".")
         self.acknowledge_message()
      
 
--- intelmq/tests/bots/experts/custom_filter/test_expert.py
+++ intelmq/tests/bots/experts/custom_filter/test_expert.py
@@ -6,7 +6,10 @@ import unittest
 import os
 import inspect
 
-EXAMPLE_FILTER_PASS = {"source.ip": "10.0.0.2",
+EXAMPLE_MESSAGE = {
+                  "__type": "Event",
+                  "source.ip": "10.0.0.1",
+                  "feed.name": "good-feed",
                   "time.source": "2015-06-04T13:37:00+00:00",
                   "feed.url": "http://www.example.com/",
                   "source.reverse_dns": "reverse.example.net",
@@ -14,29 +17,11 @@ EXAMPLE_FILTER_PASS = {"source.ip": "10.0.0.2",
                   "time.observation": "2015-08-11T13:03:40+00:00",
                   "raw": "MjAxNS8wNi8wNF8xMzozNyxleGFtcGxlLm9yZywxOTIuMC4yLjMs"
                          "cmV2ZXJzZS5leGFtcGxlLm5ldCxleGFtcGxlIGRlc2NyaXB0aW9u"
-                         "LHJlcG9ydEBleGFtcGxlLm9yZywwMDAwMAo=",
-                  "__type": "Report",
+                         "LHJlcG9ydEBleGFtcGxlLm9yZywwMDAwMAo=",                  
                   "classification.type": "malware",
                   "event_description.text": "example description",
-                  "source.asn": 00000,
-                  "feed.name": "Example"}
-EXAMPLE_FILTER_OUT = {"source.ip": "10.0.0.1",
-                  "source.abuse_contact": "one@example.com",
-                  "time.source": "2015-06-04T13:37:00+00:00",
-                  "feed.url": "http://www.example.com/",
-                  "source.reverse_dns": "reverse.example.net",
-                  "source.url": "http://example.org",
-                  "time.observation": "2015-08-11T13:03:40+00:00",
-                  "raw": "MjAxNS8wNi8wNF8xMzozNyxleGFtcGxlLm9yZywxOTIuMC4yLjMs"
-                         "cmV2ZXJzZS5leGFtcGxlLm5ldCxleGFtcGxlIGRlc2NyaXB0aW9u"
-                         "LHJlcG9ydEBleGFtcGxlLm9yZywwMDAwMAo=",
-                  "__type": "Report",
-                  "classification.type": "malware",
-                  "event_description.text": "example description",
-                  "source.asn": 00000,
-                  "feed.name": "Example"}
-
-
+                  "source.asn": 00000
+                  }
 
 class TestCustomFilterExpertBot(test.BotTestCase, unittest.TestCase):
     """
@@ -46,24 +31,42 @@ class TestCustomFilterExpertBot(test.BotTestCase, unittest.TestCase):
     @classmethod
     def set_bot(self):
         self.bot_reference = CustomFilterExpertBot
-        self.default_input_message = {'__type': 'Report'}
-        
+        self.default_input_message = {'__type': 'Report'}        
         self.sysconfig = {'rules_dir': os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"}
-        
+            
+    def test_filter_pass(self):
+        """ Test if certain messages pass filter. """
+        msg = self.input_message = EXAMPLE_MESSAGE.copy()
+        self.run_bot()
+        self.assertMessageEqual(0, msg)
 
     def test_filter_out(self):
         """ Test if certain messages do not pass filter. """
-        self.input_message = EXAMPLE_FILTER_OUT
+        self.input_message = EXAMPLE_MESSAGE.copy()
+        self.input_message["feed.name"] = "bad-feed"        
         self.run_bot()
         self.assertOutputQueueLen(0)
-        #self.assertMessageEqual(0, EXAMPLE_ROBERT)
-
-    def test_filter_pass(self):
-        """ Test if certain messages pass filter. """
-        self.input_message = EXAMPLE_FILTER_PASS
+        
+    def test_filter_masked_ip_excluded(self):
+        """ Test if certain messages are excluded by a filter defining network mask. """
+        self.input_message = EXAMPLE_MESSAGE.copy()
+        self.input_message["source.ip"] = "10.0.1.3"
         self.run_bot()
-        self.assertMessageEqual(0, EXAMPLE_FILTER_PASS)
-
+        self.assertOutputQueueLen(0)
+        
+    def test_filter_static_ip_exluded(self):
+        """ Test if certain messages are excluded by a filter defining network mask. """
+        self.input_message = EXAMPLE_MESSAGE.copy()
+        self.input_message["source.ip"] = "10.0.2.2"
+        self.run_bot()
+        self.assertOutputQueueLen(0)
+        
+    def test_filter_static_ip_passed(self):
+        """ Test if certain messages are excluded by a filter defining network mask. """
+        msg = self.input_message = EXAMPLE_MESSAGE.copy()
+        self.input_message["source.ip"] = "10.0.2.3"
+        self.run_bot()
+        self.assertMessageEqual(0, msg)
 
 if __name__ == '__main__':
     unittest.main()
