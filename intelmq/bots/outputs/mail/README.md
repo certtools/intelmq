c6f09b79ef26195bd97ba02973360cb5de604f19
Edvard Rejthar <edvard.rejthar@nic.cz>
2018-01-04 19:30:09 +0100

    FIXED gitlab#75 gitlab#81
    REMOVE: generic parser used for Avalanche and Mumblehard
    
    Therefore no need to have specific new parsers for them. Removing.
    
    gpg checks at cli startup

--- intelmq/bots/outputs/mail/README.md
+++ intelmq/bots/outputs/mail/README.md
@@ -11,13 +11,27 @@ Disabled intelmq bot. Its functionality gets launched by cli.
 It loads the events gathered by output_gather.py and sends them to abuse contact e-mails.
 
 Launch it like that:
-</usr/local/bin executable> <bot-id> cli [--tester tester emails]
+`</usr/local/bin executable> <bot-id> cli [--tester tester's email]`
 Ex:
-intelmq.bots.outputs.mail.output_send  mailsend-output-cz cli --tester edvard.rejthar+test@nic.cz
+`intelmq.bots.outputs.mail.output_send  mailsend-output-cz cli --tester edvard.rejthar+test@nic.cz`
+
+Other flags:
+```
+  -h, --help            show this help message and exit
+  --tester TESTING_TO   tester's e-mail
+  --ignore-older-than-days IGNORE_OLDER_THAN_DAYS
+                        1..n skip all events with time.observation older than
+                        1..n day; 0 disabled (allow all)
+  --gpgkey GPGKEY       fingerprint of gpg key to be used
+  --limit-results LIMIT_RESULTS
+                        Just send first N mails.
+```
 
 It shows ready e-mails and let you send them to tester's e-mail OR to the recipients.
 When done, e-mails are deleted.
 E-mails are send in zipped csv file, delimited by comma, strings in "".
+
+The field "raw" gets base64 decoded if possible. Bytes \n and \r are replaced with "\n" and "\r" strings in order to guarantee best CSV files readability both in Office and LibreOffice. A multiline string may be stored in "raw" which completely confused Microsoft Excel.
 
 
 Configuration:
@@ -25,13 +39,16 @@ Configuration:
 "alternative_mails": "", # empty string or or path to csv in the form original@email.com,alternative@email.com
 "bcc": [], # the list of e-mails to be put in the bcc field for every mail
 "emailFrom": "", # sender's e-mail
+"gpgkey": "key fingerprint", # (OPTIONAL) fingerprint of a GPG key stored in ~/.gnupg keyring folder
+"gpgpass": "password", # (OPTIONAL) password for the GPG key
 "mail_template": "", # file containing the body of the mail
-"ignore_older_than_days": 0, # 1..n skip all events with time.observation older than 1..n day; 0 disabled (allow all)
+"ignore_older_than_days": 0, # (OPTIONAL) 1..n skip all events with time.observation older than 1..n day; 0 disabled (allow all)
+"limit_results": 10, # (OPTIONAL) intended as a debugging option, allows loading just first N e-mails from the queue
 "redis_cache_db": "",
 "redis_cache_host": "",
 "redis_cache_port": "",
 "redis_cache_ttl": "",
 "smtp_server": "mailer",
 "subject": "Subject may contain date formatting like this %Y-%m-%d",
-"testing_to": "" # default tester's e-mail
+"testing_to": "" # (OPTIONAL) default tester's e-mail
 ```
--- intelmq/bots/outputs/mail/output_send.py
+++ intelmq/bots/outputs/mail/output_send.py
@@ -3,38 +3,38 @@
 #
 #
 # Initiate dialog ex like this:
-# ssh -t $USER@proki.csirt.cz 'sudo docker exec -i -t intelmq intelmq.bots.outputs.mail.output_send  mailsend-output-cz dialog'
+# ssh -t $USER@proki.csirt.cz 'sudo docker exec -i -t intelmq intelmq.bots.outputs.mail.output_send  mailsend-output-cz cli'
 #
 from __future__ import unicode_literals
-from base64 import b64decode
+
+import argparse
 import csv
-from collections import namedtuple, OrderedDict
 import datetime
 import json
-import argparse
-from email import encoders
-from email.message import Message
-from email.mime.audio import MIMEAudio
-from email.mime.base import MIMEBase
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-import logging
 import os
 import smtplib
 import sys
 import time
 import zipfile
+from base64 import b64decode
+from collections import namedtuple, OrderedDict
+from email.message import Message
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from intelmq.lib.bot import Bot
 from intelmq.lib.cache import Cache
-from intelmq.lib.message import Event
+
+from .gpgsafe import GPGSafe
+
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
 
 Mail = namedtuple('Mail', ["key", "to", "path", "count"])
+
 
 class MailSendOutputBot(Bot):
     TMP_DIR = "/tmp/intelmq-mails/"
@@ -44,29 +44,40 @@ class MailSendOutputBot(Bot):
 
     def set_cache(self):
         self.cache = Cache(
-                           self.parameters.redis_cache_host,
-                           self.parameters.redis_cache_port,
-                           self.parameters.redis_cache_db,
-                           self.parameters.redis_cache_ttl
-                           )
+            self.parameters.redis_cache_host,
+            self.parameters.redis_cache_port,
+            self.parameters.redis_cache_db,
+            self.parameters.redis_cache_ttl
+        )
 
     def init(self):
         self.set_cache()
         parser = argparse.ArgumentParser(prog=" ".join(sys.argv[0:1]))
         parser.add_argument('cli', help='initiate cli dialog')
-        parser.add_argument('--tester', help='tester\'s e-mail')
-        args = parser.parse_args(sys.argv[2:])
+        parser.add_argument('--tester', dest="testing_to", help='tester\'s e-mail')
+        parser.add_argument('--ignore-older-than-days', help='1..n skip all events with time.observation older than 1..n day; 0 disabled (allow all)', type=int)
+        parser.add_argument("--gpgkey", help="fingerprint of gpg key to be used")
+        parser.add_argument("--limit-results", type=int, help="Just send first N mails.")
+        parser.parse_args(sys.argv[2:], namespace=self.parameters)
 
-        if args.tester:
-            self.parameters.testing_to = args.tester
+        if self.parameters.cli == "cli":
 
-        if args.cli == "cli":
+            self.parameters.gpg = None
+            if self.parameters.gpgkey:
+                GPGHOME = "~/.gnupg"
+                self.parameters.gpg = GPGSafe(use_agent=False, homedir=GPGHOME)
+                if bool(self._sign("test text")):
+                    print("Successfully loaded GPG key {}".format(self.parameters.gpgkey))
+                else:
+                    print("Error loading GPG key {} from {}".format(self.parameters.gpgkey, os.path.expanduser(GPGHOME)))
+                    sys.exit(1)
+
             os.makedirs(self.TMP_DIR, exist_ok=True)
             with open(self.parameters.mail_template) as f:
                 self.mailContents = f.read()
             self.alternativeMail = {}
             if self.parameters.alternative_mails:
-                with open(self.parameters.alternative_mails,"r") as f:
+                with open(self.parameters.alternative_mails, "r") as f:
                     reader = csv.reader(f, delimiter=",")
                     for row in reader:
                         self.alternativeMail[row[0]] = row[1]
@@ -74,53 +85,82 @@ class MailSendOutputBot(Bot):
             print("Preparing mail queue...")
             mails = [m for m in self.prepare_mails() if m]
 
+            print()
+            if self.parameters.limit_results:
+                print("Results limited to {} by flag. ".format(self.parameters.limit_results), end="")
+
             if not len(mails):
                 print(" *** No mails in queue ***")
-                sys.exit(0);
-            
+                sys.exit(0)
+            else:
+                print("Number of mails in the queue:", len(mails))
+
             with smtplib.SMTP(self.parameters.smtp_server) as self.smtp:
                 while True:
-                    print("What you would like to do?\n"
-                        "* enter to send first mail to tester's address {}.\n"
-                        "* 'debug' to send all the e-mails to tester's address"
-                        .format(self.parameters.testing_to))
+                    print("GPG active" if self.parameters.gpgkey else "No GPG")
+                    print("\nWhat you would like to do?\n"
+                          "* enter to send first mail to tester's address {}.\n"
+                          "* any mail from above to be delivered to tester's address\n"
+                          "* 'debug' to send all the e-mails to tester's address"
+                          .format(self.parameters.testing_to))
                     if self.parameters.testing_to:
                         print("* 's' for setting other tester's address")
                     print("* 'all' for sending all the e-mails\n"
-                        "* 'x' to cancel\n"
-                        "? ", end="")
+                          "* 'clear' for clearing the queue\n"
+                          "* 'x' to cancel\n"
+                          "? ", end="")
                     i = input()
-                    if i == "x":
+                    if i in ["x", "q"]:
                         sys.exit(0)
                     elif i == "all":
                         count = 0
                         for mail in mails:
-                            count += 1 if self.send_mail(mail, send=True) else 0
-                            self.cache.redis.delete(mail.key)
-                            if mail.path:
-                                os.unlink(mail.path)
+                            if self.build_mail(mail, send=True):
+                                count += 1
+                                self.cache.redis.delete(mail.key)
+                                if mail.path:
+                                    os.unlink(mail.path)
                         print("{}× mail sent.\n".format(count))
                         sys.exit(0)
+                    elif i == "clear":
+                        for mail in mails:
+                            self.cache.redis.delete(mail.key)
+                        print("Queue cleared.")
+                        sys.exit(0)
                     elif i == "s":
-                        print("\nWhat e-mail should I use?")
-                        self.parameters.testing_to = input()
-                    elif i in ["","y","debug"]:
-                        if not self.parameters.testing_to:
-                            print("\nWhat e-mail should I use?")
-                            self.parameters.testing_to = input()
-                        if i in ["","y"]:
-                            mail = mails[0]
-                            self.send_mail(mail, send=True, override_to=self.parameters.testing_to)
-                            count = 1
-                        elif i == "debug":
-                            count = sum([1 for mail in mails if self.send_mail(mail, send=True, override_to=self.parameters.testing_to)])                            
-                        print("{}× mail sent to: {}\n".format(count, self.parameters.testing_to))            
+                        self.set_tester()
+                    elif i in ["", "y"]:
+                        self.send_mails_to_tester([mails[0]])
+                    elif i == "debug":
+                        self.send_mails_to_tester(mails)
+                    else:
+                        for mail in mails:
+                            if mail.to == i:
+                                self.send_mails_to_tester([mail])
+                                break
+                        else:
+                            print("Unknown option.")
 
         else:
             print("Running forever with no job. Run with 'cli' parameter.")
 
-    # Sends out all emails
+    def set_tester(self, force=True):
+        if not force and self.parameters.testing_to:
+            return
+        print("\nWhat e-mail should I use?")
+        self.parameters.testing_to = input()
+
+    def send_mails_to_tester(self, mails):
+        """
+            These mails are going to tester's address. Then prints out their count.
+        :param mails: list
+        """
+        self.set_tester(False)
+        count = sum([1 for mail in mails if self.build_mail(mail, send=True, override_to=self.parameters.testing_to)])
+        print("{}× mail sent to: {}\n".format(count, self.parameters.testing_to))
+
     def prepare_mails(self):
+        """ Generates Mail objects """
         allowed_fieldnames = ['time.source', 'source.ip', 'classification.taxonomy', 'classification.type',
                               'time.observation', 'source.geolocation.cc', 'source.asn', 'event_description.text',
                               'malware.name', 'feed.name', 'feed.url', 'raw']
@@ -130,31 +170,39 @@ class MailSendOutputBot(Bot):
                                   'event_description.text': 'description', 'malware.name': 'malware',
                                   'feed.name': 'feed_name', 'feed.url': 'feed_url', 'raw': 'raw'}
 
-        for mail_record in self.cache.redis.keys("mail:*"):
+        for mail_record in self.cache.redis.keys("mail:*")[slice(self.parameters.limit_results)]:
             lines = []
             self.logger.debug(mail_record)
-            for message in self.cache.redis.lrange(mail_record, 0, -1):
-                lines.append(json.loads(str(message,encoding="utf-8")))
+            try:
+                messages = self.cache.redis.lrange(mail_record, 0, -1)
+            except TimeoutError:
+                print("!! {} timeouted, too big to read from redis".format(mail_record))
+                continue
+            for message in messages:
+                lines.append(json.loads(str(message, encoding="utf-8")))
 
             # prepare rows for csv attachment
-            threshold = datetime.datetime.now()-datetime.timedelta(days=self.parameters.ignore_older_than_days) if getattr(self.parameters, 'ignore_older_than_days', False) else False            
+            threshold = datetime.datetime.now() - datetime.timedelta(
+                days=self.parameters.ignore_older_than_days) if getattr(self.parameters, 'ignore_older_than_days',
+                                                                        False) else False
             fieldnames = set()
-            rows_output = []                        
+            rows_output = []
             for row in lines:
-                #if "feed.name" in row and "NTP-Monitor" in row["feed.name"]: continue                
+                # if "feed.name" in row and "NTP-Monitor" in row["feed.name"]: continue
                 if threshold and row["time.observation"][:19] < threshold.isoformat()[:19]:
-                        continue
+                    # import ipdb; ipdb.set_trace()
+                    continue
                 fieldnames = fieldnames | set(row.keys())
-                keys = set(allowed_fieldnames).intersection(row)                
-                ordered_keys = []                 
-                for field in allowed_fieldnames:                    
+                keys = set(allowed_fieldnames).intersection(row)
+                ordered_keys = []
+                for field in allowed_fieldnames:
                     if field in keys:
                         ordered_keys.append(field)
                 try:
                     row["raw"] = b64decode(row["raw"]).decode("utf-8").strip().replace("\n", "\\n").replace("\r", "\\r")
-                except: # let the row field as is
+                except:  # let the row field as is
                     pass
-                rows_output.append(OrderedDict({fieldnames_translation[k]:row[k] for k in ordered_keys}))
+                rows_output.append(OrderedDict({fieldnames_translation[k]: row[k] for k in ordered_keys}))
 
             # prepare headers for csv attachment
             ordered_fieldnames = []
@@ -169,9 +217,9 @@ class MailSendOutputBot(Bot):
 
             email_to = str(mail_record[len("mail:"):], encoding="utf-8")
             count = len(rows_output)
-            if not count:                
+            if not count:
                 path = None
-            else:                
+            else:
                 filename = '{}_{}_events'.format(time.strftime("%y%m%d"), count)
                 path = self.TMP_DIR + filename + '_' + email_to + '.zip'
 
@@ -183,29 +231,34 @@ class MailSendOutputBot(Bot):
                     continue
                 finally:
                     zf.close()
-                
+
                 if email_to in self.alternativeMail:
                     print("Alternative: instead of {} we use {}".format(email_to, self.alternativeMail[email_to]))
                     email_to = self.alternativeMail[email_to]
 
-            # send the whole message
             mail = Mail(mail_record, email_to, path, count)
-            self.send_mail(mail, send=False)
-            yield mail
+            self.build_mail(mail, send=False)
+            if count:
+                yield mail
 
-    #def _hasTestingTo(self):
+    # def _hasTestingTo(self):
     #    return hasattr(self.parameters, 'testing_to') and self.parameters.testing_to != ""
 
+    def build_mail(self, mail, send=False, override_to=None):
+        """ creates a MIME message
+        :param mail: Mail object
+        :param send: True to send through SMTP, False for just printing the information
+        :param override_to: Use this e-mail instead of the one specified in the Mail object
+        :return: True if successfully sent.
 
-    # actual funtion to send email through smtp
-    def send_mail(self, mail, send=False,  override_to=None):
+        """
         if override_to:
             intended_to = mail.to
             email_to = override_to
         else:
             intended_to = None
             email_to = mail.to
-        email_from =  self.parameters.emailFrom        
+        email_from = self.parameters.emailFrom
         text = self.mailContents
         try:
             subject = time.strftime(self.parameters.subject)
@@ -215,32 +268,57 @@ class MailSendOutputBot(Bot):
             subject += " (intended for {})".format(intended_to)
         else:
             subject += " ({})".format(email_to)
-        msg = MIMEMultipart()
-        msg["From"] = email_from
-        msg["Subject"] = subject
-        msg["To"] = email_to
-        rcpts = [email_to]
+        recipients = [email_to]
         if hasattr(self.parameters, 'bcc') and not intended_to:
-            rcpts += self.parameters.bcc # bcc is in fact an independent mail
+            recipients += self.parameters.bcc  # bcc is in fact an independent mail
 
         if send is True:
             if not mail.count:
                 return False
-            msg.attach(MIMEText(text, "html", "utf-8"))
-            with open(mail.path,"rb") as f: # plain/text - with open(mail.path,"r") as f: attachment = MIMEText(f.read(), subtype, "utf-8")
+
+            base_msg = MIMEMultipart()
+            base_msg.attach(MIMEText(text, "html", "utf-8"))
+
+            with open(mail.path, "rb") as f:  # plain/text - with open(mail.path,"r") as f: attachment = MIMEText(f.read(), subtype, "utf-8")
                 attachment = MIMEApplication(f.read(), "zip")
             attachment.add_header("Content-Disposition", "attachment",
                                   filename='proki_{}.zip'.format(time.strftime("%Y%m%d")))
-            msg.attach(attachment)            
-            self.smtp.sendmail(email_from, rcpts, msg.as_string().encode('ascii'))            
+            base_msg.attach(attachment)
+            if self.parameters.gpg:
+                msg = MIMEMultipart(_subtype="signed", micalg="pgp-sha1", protocol="application/pgp-signature")
+                s = base_msg.as_string().replace('\n', '\r\n')
+                signature = self._sign(s)
+
+                if not signature:
+                    print("Failed to sign the message for {}".format(email_to))
+                    return False
+                signature_msg = Message()
+                signature_msg['Content-Type'] = 'application/pgp-signature; name="signature.asc"'
+                signature_msg['Content-Description'] = 'OpenPGP digital signature'
+                signature_msg.set_payload(signature)
+                msg.attach(base_msg)
+                msg.attach(signature_msg)
+            else:
+                msg = base_msg
+
+            msg["From"] = email_from
+            msg["Subject"] = subject
+            msg["To"] = email_to
+            self.smtp.sendmail(email_from, recipients, msg.as_string().encode('ascii'))  # .encode('ascii')
             return True
         else:
-            print('To: ' + email_to + '; Subject: ' + subject)
+            print('To: {}; Subject: {} '.format(email_to, subject), end="")
             if not mail.count:
                 print("Won't be send, all events skipped")
             else:
-                print('Events: {}'.format(mail.count))            
-            print('-------------------------------------------------')
+                print('Events: {}, Size: {}'.format(mail.count, os.path.getsize(mail.path)))
             return None
+
+    def _sign(self, s):
+        try:
+            return str(self.parameters.gpg.sign(s, default_key=self.parameters.gpgkey, detach=True, clearsign=False, passphrase=self.parameters.gpgpass))
+        except:
+            return ""
+
 
 BOT = MailSendOutputBot
