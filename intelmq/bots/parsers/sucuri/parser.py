db084c872de6f775d355581b1472ae23675e2f1b
dargen3 <dargen3@centrum.cz>
2018-01-25 09:20:30 +0000

    Right codestyle and change in CHANGELOG

--- intelmq/bots/parsers/sucuri/parser.py
+++ intelmq/bots/parsers/sucuri/parser.py
@@ -9,7 +9,7 @@ class MyHTMLParser(HTMLParser):
 
     lsData = ""
 
-    def handle_data(self,data):
+    def handle_data(self, data):
         self.lsData = data
 
 
@@ -19,20 +19,20 @@ parser = MyHTMLParser()
 class SucuriParserBot(ParserBot):
     def process(self):
         report = self.receive_message()
-        raw_report = utils.base64_decode(report["raw"]) # decoding
-        raw_report = re.sub(r"<!--(.|\s|\n)*?-->", "", raw_report) # removing comments
+        raw_report = utils.base64_decode(report["raw"])  # decoding
+        raw_report = re.sub(r"<!--(.|\s|\n)*?-->", "", raw_report)  # removing comments
         report_list = [row.strip() for row in raw_report.splitlines()]
         index = 0
         actual_line = report_list[index]
-        while parser.lsData != "Hidden iframes": # displacement to target table
+        while parser.lsData != "Hidden iframes":  # displacement to target table
             index += 1
             actual_line = report_list[index]
             parser.feed(actual_line)
-        while actual_line[:8] != "</tbody>": # scrabing table data
+        while actual_line[:8] != "</tbody>":  # scrabing table data
             index += 1
             actual_line = report_list[index]
             if actual_line[:2] == "<t":
-                event = self.new_event(report)
+                event = self.new_event(report)  # making new event
                 parser.feed(report_list[index])
                 event.add("source.url", parser.lsData)
                 event.add("classification.type", "blacklist")
@@ -43,5 +43,4 @@ class SucuriParserBot(ParserBot):
 
 
 BOT = SucuriParserBot
-
 
