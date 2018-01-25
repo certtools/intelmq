4edebf20ce4785649768a5d8b3b4e6e7c2759b7e
Bernhard Herzog <bernhard.herzog@intevation.de>
2018-01-25 21:33:31 +0100

    Use overwrite instead of force with the Event.add method
    The force parameter has been deprecated and one has to use overwrite
    instead. The intelmq version we currently use still supports the force
    parameter but prints a deprecation warning when it's used. Also, in
    intelmq 1.0 it will be removed entirely.

--- intelmq/bots/experts/certbund_contact/eventjson.py
+++ intelmq/bots/experts/certbund_contact/eventjson.py
@@ -45,7 +45,7 @@ def set_certbund_field(event, key, value):
     extra = get_parsed_extra_field(event)
     certbund = extra.setdefault("certbund", {})
     certbund[key] = value
-    event.add("extra", extra, force=True)
+    event.add("extra", extra, overwrite=True)
 
 
 def del_certbund_field(event, key):
