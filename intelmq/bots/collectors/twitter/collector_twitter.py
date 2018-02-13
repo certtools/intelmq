0d9bb834b127ed9c66035147f015004182c9a871
Karel <kkjkolar@live.com>
2018-02-13 23:16:32 +0000

    Added twitter tests, requirements, documentation
    added parser etc.
    
    pep8 compliance for code
    
    removed comment on if pid
    
    Added new tests, changed separator for substitutions from comma to semicolon
    
    Polishing
    
    Fixing BOTS
    
    Style

--- intelmq/bots/collectors/twitter/collector_twitter.py
+++ intelmq/bots/collectors/twitter/collector_twitter.py
@@ -1,99 +1,119 @@
-import twitter
-import requests
-from url_normalize import url_normalize
-from tld import get_tld
-from tld.utils import update_tld_names
+"""
+Twitter Collector Bot
+
+Extracts tweet_text from tweets and follows urls in supported_sources.
+Url text and tweet text are sent separately.
+
+Parameters:
+
+    feed - feed name (Twitter)
+
+    tweet_count : int default 20
+
+    target_timelines : comma separated screen_names of users whose timelines are followed
+
+    follow_urls : comma separated screen_names of tweeters for which it is
+            allowed to follow urls, currently extraction only from pastebin is supported
+
+
+    include_rts : bool default False (include retweets)
+
+    exclude_replies: bool default True
+
+    timelimit: int default 84400s, how far to the past do we accept tweets?
+
+    Api login data:
+        consumer_key
+        consumer_secret
+        access_token_key
+        access_token_secret
+
+To get api login data see: https://python-twitter.readthedocs.io/en/latest/getting_started.html
+
+"""
+
+import time
 from intelmq.lib.bot import CollectorBot
+
+try:
+    import requests
+except ImportError:
+    requests = None
+
+try:
+    import twitter
+except ImportError:
+    twitter = None
 
 
 class TwitterCollectorBot(CollectorBot):
+
     def init(self):
-        update_tld_names()
-        self.id_map = {}
-        self.target_timelines = self.parameters.target_timelines.split(",")
-        self.users = self.parameters.users.split(",")
-        self.api = twitter.Api(consumer_key=self.parameters.consumer_key,
-                               consumer_secret=self.parameters.consumer_secret,
-                               access_token_key=self.parameters.access_token_key,
-                               access_token_secret=self.parameters.access_token_secret, tweet_mode="extended")
+        self.current_time_in_seconds = int(time.time())
+        self.target_timelines = []
+        if getattr(self.parameters, "target_timelines", '') is not '':
+            self.target_timelines.extend(
+                self.parameters.target_timelines.split(','))
+        self.tweet_count = int(getattr(self.parameters, "tweet_count", 20))
+        self.follow_urls = []
+        if getattr(self.parameters, "follow_urls", '') is not '':
+            self.follow_urls.extend(
+                self.parameters.follow_urls.split(','))
+        self.include_rts = getattr(self.parameters, "include_rts", False)
+        self.exclude_replies = getattr(self.parameters, "exclude_replies", False)
+        self.timelimit = int(getattr(self.parameters, "timelimit", 24 * 60 * 60))
+        self.api = twitter.Api(
+            consumer_key=self.parameters.consumer_key,
+            consumer_secret=self.parameters.consumer_secret,
+            access_token_key=self.parameters.access_token_key,
+            access_token_secret=self.parameters.access_token_secret,
+            tweet_mode="extended")
 
-    def max_id_finder(self, tweets: list, name: str):
-      # Finds max twee_id in tweets from specific user, so next time the bot gets tweets with ids from this maximum
-        for tweet in tweets:
-            if tweet.id > self.id_map[name]:
-                self.id_map[name] = tweet.id
-
-    def get_data_from_url(self, url: str) -> list:
-        data = []
-        supported_sources = ["pastebin.com"]
-        for source in supported_sources:
-            if source in url:
-                if "raw" not in url:
-                    request = requests.get(url.replace("pastebin.com", "pastebin.com/raw"))
-                else:
-                    request = requests.get(url)
-                for source in request.content.decode().split("\r\n"):
-                    if "." or "/" in source:
-                        data.append(source)
-        return data
-
-    def get_data_from_text(self, tweet: list) -> list:
-        data = []
-        for part in tweet:
-            if "t.co" not in part:
-                result = get_tld(part, fail_silently=True, as_object=True)
-                if result is not None:
-                        data.append(url_normalize(part))
-        return data
-
-    def analyze_tweet(self, tweet) -> list:
-        data = []
-        tweet_text = []
-        tweet_text.extend(tweet.full_text.split(" "))
-        if tweet.quoted_status is not None:
-            tweet_text.extend(tweet.quoted_status.full_text.split(" "))
-        if len(tweet.urls) > 0:
-            for source in [tweet.urls, tweet.quoted_status.urls]:
-                for url in source:
-                    data.extend(self.get_data_from_url(url.expanded_url))
-        data.extend(self.get_data_from_text(tweet_text))
-        return data
+    def get_text_from_url(self, url: str) -> str:
+        if "pastebin.com" in url:
+            self.logger.debug('Processing url %r.', url)
+            if "raw" not in url:
+                request = requests.get(
+                    url.replace("pastebin.com", "pastebin.com/raw"))
+            else:
+                request = requests.get(url)
+            return request.text
+        return ''
 
     def process(self):
         tweets = []
         for target in self.target_timelines:
-            try:
-                statuses = self.api.GetUserTimeline(screen_name=target, count=self.parameters.tweet_count,
-                                                    since_id=self.id_map[target])
-            except KeyError:
-                self.id_map[target] = 0
-                statuses = self.api.GetUserTimeline(screen_name=target, count=self.parameters.tweet_count)
-            except Exception as e:
-                self.logger.info("Getting tweets failed {}".format(e))
-            self.max_id_finder(statuses, target)
+            statuses = self.api.GetUserTimeline(
+                screen_name=target,
+                count=self.tweet_count,
+                include_rts=self.include_rts,
+                exclude_replies=self.exclude_replies)
             tweets.extend(statuses)
+        tweets_new = []
+        for tweet in tweets:  # filter out new tweets
+            if tweet.created_at_in_seconds > (self.current_time_in_seconds - self.timelimit):
+                tweets_new.append(tweet)
+        for tweet in tweets_new:
+            self.logger.debug('Processing Tweet ID %r.', tweet.id)
+            report = self.new_report()
+            report.add('raw', tweet.full_text)
+            report.add(
+                'feed.url',
+                'https://twitter.com/{0}/status/{1}'.format(tweet.user.screen_name, tweet.id))
+            self.send_message(report)
+            if tweet.user.screen_name in self.follow_urls:
+                if len(tweet.urls) > 0:
+                    text = ''
+                    for source in tweet.urls:
+                        text = text + self.get_text_from_url(source.expanded_url)
+                    if len(text) > 0:
+                        report = self.new_report()
+                        report.add('raw', text)
+                        report.add('feed.code', 'url_text')
+                        report.add(
+                            'feed.url',
+                            'https://twitter.com/{0}/status/{1}'.format(tweet.user.screen_name, tweet.id))
+                        self.send_message(report)
 
-        for user in self.users:
-            try:
-                statuses = self.api.GetFavorites(screen_name=user, count=self.parameters.tweet_count,
-                                                 since_id=self.id_map[user])
-            except KeyError:
-                self.id_map[user] = 0
-                statuses = self.api.GetFavorites(screen_name=user, count=self.parameters.tweet_count)
-            except Exception as e:
-                self.logger.info("Getting tweets failed {}".format(e))
-            self.max_id_finder(statuses, user)
-            tweets.extend(statuses)
 
-        data = []
-        for tweet in tweets:
-            data.extend(self.analyze_tweet(tweet))
-        data = "\n".join(data)
-
-        report = self.new_report()
-        report.add("raw", data)
-        report.add("feed.url", "<data source>")
-        self.send_message(report)
-
-
-BOT=TwitterCollectorBot
+BOT = TwitterCollectorBot
