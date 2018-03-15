d9b5042f539fe833610735c0e2814956ca2046d3
Sebastian Wagner <wagner@cert.at>
2018-03-15 14:12:30 +0100

    MAINT: modify expert rules: merge examples into 1
    and remove tcp-default rule, too generic

--- intelmq/tests/bots/experts/modify/test_expert.py
+++ intelmq/tests/bots/experts/modify/test_expert.py
@@ -15,6 +15,7 @@ EVENT_TEMPL = {"__type": "Event",
                "feed.name": "Spamhaus Cert",
                "feed.url": "https://portal.spamhaus.org/cert/api.php?cert="
                            "<CERTNAME>&key=<APIKEY>",
+               "classification.taxonomy": "malicious code",
                "classification.type": "botnet drone",
                "time.observation": "2015-01-01T00:00:00+00:00",
                }
@@ -22,10 +23,17 @@ INPUT = [{'feed.name': 'Abuse.ch',
           'feed.url': 'https://feodotracker.abuse.ch/blocklist/?download=domainblocklist'},
          {'malware.name': 'foobar', 'feed.name': 'Other Feed'},
          {'source.port': 80, 'malware.name': 'zeus'},
+         {'malware.name': 'xcodeghost'},
+         {'malware.name': 'securityscorecard-someexample-value'},
+         {'malware.name': 'anyvalue'},
          ]
 OUTPUT = [{'classification.identifier': 'feodo'},
-          {},
-          {'protocol.application': 'http', 'classification.identifier': 'zeus'},
+          {'classification.identifier': 'foobar'},
+          {'protocol.transport': 'tcp', 'protocol.application': 'http',
+           'classification.identifier': 'zeus'},
+          {'classification.identifier': 'xcodeghost'},
+          {'classification.identifier': 'someexample-value'},
+          {'classification.identifier': 'anyvalue'},
           ]
 for index in range(len(INPUT)):
     copy1 = EVENT_TEMPL.copy()
@@ -69,55 +77,6 @@ class TestModifyExpertBot(test.BotTestCase, unittest.TestCase):
         self.assertDictEqual(convert_config(old_config)[0],
                              new_config[0])
 
-EVENT_TEMPL2 = {"__type": "Event",
-               "feed.name": "Testing IntelMQ Mock Feed",
-               "feed.url": "https://example.org/does-not-exist",
-               "classification.type": "botnet drone",
-               "time.observation": "2015-01-02T01:20:00+00:00",
-               }
-
-INPUT2 = [
-    {'malware.name': 'xcodeghost'},
-    {'malware.name': 'securityscorecard-someexample-value'},
-    {'malware.name': 'anyvalue'},
-         ]
-OUTPUT2 = [
-    {'protocol.transport': 'tcp', 'classification.identifier': 'xcodeghost'},
-    {'protocol.transport': 'tcp', 'classification.identifier': 'someexample-value'},
-    {'protocol.transport': 'tcp', 'classification.identifier': 'anyvalue'},
-          ]
-for index in range(len(INPUT2)):
-    copy1 = EVENT_TEMPL2.copy()
-    copy2 = EVENT_TEMPL2.copy()
-    copy1.update(INPUT2[index])
-    copy2.update(INPUT2[index])
-    copy2.update(OUTPUT2[index])
-    INPUT2[index] = copy1
-    OUTPUT2[index] = copy2
-
-
-class TestMoreFeedsModifyExpertBot(test.BotTestCase, unittest.TestCase):
-    """Testing ModifyExpertBot for 'morefeeds' configuration.
-    """
-
-    @classmethod
-    def set_bot(cls):
-        cls.bot_reference = ModifyExpertBot
-        config_path = resource_filename('intelmq',
-                                        'bots/experts/modify/examples/morefeeds.conf')
-        cls.sysconfig = {'configuration_path': config_path
-                         }
-
-    def test_bot_name(self):
-        "Do **not** test that our second test has the same name as the bot."
-
-    def test_events(self):
-        """ Test if correct Events have been produced. """
-        self.input_message = INPUT2
-        self.run_bot(iterations=len(INPUT2))
-
-        for position, event_out in enumerate(OUTPUT2):
-            self.assertMessageEqual(position, event_out)
 
 
 if __name__ == '__main__':  # pragma: no cover
