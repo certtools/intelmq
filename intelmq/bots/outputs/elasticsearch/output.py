accddff9a68b2628e9bae67738ee87690048df7a
Zach Stone <stone-z@users.noreply.github.com>
2018-06-19 21:01:35 +0200

    Initial Elasticsearch index rotation implementation. WIP. - Add parameter to Elasticsearch Output bot for rotating indexes - Select index name to use at runtime - Add template functionality to elasticmapper to create templates

--- intelmq/bots/outputs/elasticsearch/output.py
+++ intelmq/bots/outputs/elasticsearch/output.py
@@ -4,6 +4,7 @@ The ES-connection can't be closed explicitly.
 """
 
 from json import loads
+from datetime import datetime
 from collections.abc import Mapping
 
 try:
@@ -36,6 +37,8 @@ class ElasticsearchOutputBot(Bot):
                                     'elastic_port', '9200')
         self.elastic_index = getattr(self.parameters,
                                      'elastic_index', 'intelmq')
+        self.rotate_index = getattr(self.parameters,
+                                    'rotate_index', False)
         self.http_username = getattr(self.parameters,
                                      'http_username', None)
         self.http_password = getattr(self.parameters,
@@ -54,8 +57,50 @@ class ElasticsearchOutputBot(Bot):
             kwargs = {'http_auth': (self.http_username, self.http_password)}
         self.es = Elasticsearch([{'host': self.elastic_host, 'port': self.elastic_port}], **kwargs)
 
-        if not self.es.indices.exists(self.elastic_index):
-            self.es.indices.create(index=self.elastic_index, ignore=400)
+        if self.rotate_index:
+            # Use time-based index names - check that the template exists
+            if not self.es.indices.exists_template(name=self.elastic_index):
+                print()  # error TODO
+
+        else:
+            # Using a single named index. Check that it exists
+            if not self.es.indices.exists(self.elastic_index):
+                self.es.indices.create(index=self.elastic_index, ignore=400)
+
+    def get_index(self, event_dict):
+        """
+        Returns the correct index name to use for the given event,
+         based on the current bot's settings and the event's date.
+        :param event_dict: The event (as a dict) to examine.
+        :return: A string containing the name of the index which should store the event.
+        """
+        # This function supports rotating indices based on timestamps.
+        # If the bot should rotate indices, the index name will include a date stamp based on:
+        #   - the time_source field - if one is available, else
+        #   - the time_observation field - if one is available, else
+        #   - the current time, if neither of the above is available.
+
+        if self.rotate_index:
+            event_date = None
+            # Try to use the the time information from the event.
+            for t in [event_dict.get('time_source', None), event_dict.get('time_observation', None)]:
+                try:
+                    event_date = datetime.strptime(t, '%Y-%m-%dT%H:%M:%S+00:00').date()
+                    # event_date = date.fromtimestamp(t)
+                    break
+                except (TypeError, ValueError) as e:
+                    print("Error: {}".format(e))
+                    event_date = None
+                    continue
+
+            # If no time available in the event, use today's date.
+            event_date = event_date or datetime.today().date()
+
+            print("Date: {}".format(event_date.isoformat()))
+            return "{}-{}".format(self.elastic_index, event_date.isoformat())
+        else:
+            # If the bot should NOT rotate indices, just use the index name
+            return self.elastic_index
 
     def process(self):
         event = self.receive_message()
@@ -78,7 +123,11 @@ class ElasticsearchOutputBot(Bot):
         event_dict = replace_keys(event_dict,
                                   replacement=self.replacement_char)
 
-        self.es.index(index=self.elastic_index,
+        # TODO: Remove me
+        # 2018-06-13T00:00:00+00:00
+        # event_dict.update({"time_source": "2018-05-13T00:00:00+00:00"})
+
+        self.es.index(index=self.get_index(event_dict),
                       doc_type=self.elastic_doctype,
                       body=event_dict)
         self.acknowledge_message()
