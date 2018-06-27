508b8466534153c1092fc301e45f6e9e02c15b4d
tux78 <vdberge.sebastian@googlemail.com>
2018-06-27 23:13:10 +0200

    Parser

---
+++ intelmq/bots/parsers/atd/parser.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+ATDParserBot parses data from McAfee Advanced Threat Defense.
+
+The following information is forwarded via queue:
+IP
+URL
+Hash
+"""
+from __future__ import unicode_literals
+import sys
+
+# imports for additional libraries and intelmq
+from intelmq.lib.bot import Bot
+
+
+class ExampleParserBot(Bot):
+    def process(self):
+        report = self.receive_message()
+
+        event = self.new_event(report)  # copies feed.name, time.observation
+        ... # implement the logic here
+        event.add('source.ip', '127.0.0.1')
+        event.add('extra', {"os.name": "Linux"})
+
+        self.send_message(event)
+        self.acknowledge_message()
+
+
+BOT = ATDParserBot
