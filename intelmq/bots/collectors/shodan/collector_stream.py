80bf5aecb0f5a25ab1c2fd5e966d0db472b7a11d
Sebastian Wagner <wagner@cert.at>
2018-06-28 12:41:03 +0200

    BUG: shodan parser: lazy import library

--- intelmq/bots/collectors/shodan/collector_stream.py
+++ intelmq/bots/collectors/shodan/collector_stream.py
@@ -3,13 +3,19 @@
 Parameter:
 * api_key: The API key
 """
-import shodan
+try:
+    import shodan
+except ImportError:
+    shodan = None
 
 from intelmq.lib.bot import CollectorBot
 
 
 class ShodanStreamCollectorBot(CollectorBot):
     def init(self):
+        if shodan is None:
+            raise ValueError("Library 'shodan' is needed but not installed.")
+
         self.set_request_parameters()
         self.api = shodan.Shodan(self.parameters.api_key)
         if isinstance(self.parameters.countries, str):
