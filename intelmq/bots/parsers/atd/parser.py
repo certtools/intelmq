2cf8a59edc8c32d93ebc95cfe2d7730ed853c5a1
tux78 <vdberge.sebastian@googlemail.com>
2018-06-29 09:51:14 +0200

    Parser

--- intelmq/bots/parsers/atd/parser.py
+++ intelmq/bots/parsers/atd/parser.py
@@ -9,22 +9,60 @@ Hash
 """
 from __future__ import unicode_literals
 import sys
+import json
 
 # imports for additional libraries and intelmq
-from intelmq.lib.bot import Bot
+import intelmq.lib.bot as Bot
+import intelmq.lib.utils as utils
 
 
-class ATDParserBot(Bot):
+class ATDParserBot(Bot.ParserBot):
+
+    SUPPORTED_ATD_SECTIONS = [
+        'Processes',
+        'Files',
+        'Urls',
+        'Ips',
+    ]
+
+
+    ATD_TYPE_MAPPING = {
+        'domain': 'source.fqdn',
+        'hostname': 'source.fqdn',
+        'Name': 'malware.name',
+        'Md5': 'malware.hash.md5',
+        'Sha1': 'malware.hash.sha1',
+        'Sha256': 'malware.hash.sha256',
+        'Ipv4': 'destination.ip',
+        'Port': 'destination.port',
+        'Url': 'destination.fqdn',
+    }
+
+
+
+
     def process(self):
         report = self.receive_message()
+        raw_report = utils.base64_decode(report.get('raw'))
+        atd_event = json.loads(raw_report)
+        # print(atd_event['Summary']['Subject'])
 
-        event = self.new_event(report)  # copies feed.name, time.observation
-        ... # implement the logic here
-        event.add('source.ip', '127.0.0.1')
-        event.add('extra', {"os.name": "Linux"})
+        for section in atd_event['Summary']:
+            if (section in self.SUPPORTED_ATD_SECTIONS):
+                for entry in atd_event['Summary'][section]:
+                    event = self.new_event(report)
+                    for key, value in entry.items():
+                        if (key in self.ATD_TYPE_MAPPING):
+                            event.add(self.ATD_TYPE_MAPPING[key], value)
+                    self.send_message(event)
 
-        self.send_message(event)
+        # event = self.new_event(report)  # copies feed.name, time.observation
+        # implement the logic here
+
+        # event.add('source.ip', '127.0.0.1')
+        # event.add('extra', {"os.name": "Linux"})
+
+        # self.send_message(event)
         self.acknowledge_message()
-
 
 BOT = ATDParserBot
