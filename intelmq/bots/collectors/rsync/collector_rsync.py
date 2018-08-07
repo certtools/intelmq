c54c05eb365e442ef3f9a1950a552d1dd8eb1991
dargen3 <dargen3@centrum.cz>
2018-08-07 14:28:34 +0200

    fixed error

--- intelmq/bots/collectors/rsync/collector_rsync.py
+++ intelmq/bots/collectors/rsync/collector_rsync.py
@@ -5,7 +5,7 @@ from intelmq.lib.bot import CollectorBot
 class RsyncCollectorBot(CollectorBot):
     def process(self):
         self.logger.info("Updating file {}.".format(self.parameters.file))
-        os.system("rsync {}/{} {}".format(self.parameters.rsync_path, self.parameters.file, os.path.dirname()))
+        os.system("rsync {}/{} {}".format(self.parameters.rsync_path, self.parameters.file, os.path.dirname(__file__)))
         report = self.new_report()
         with open("{}/{}".format("/".join(os.path.abspath(__file__).split("/")[:-1]), self.parameters.file), "r") as rsync_file:
             report.add("raw", rsync_file.read())
