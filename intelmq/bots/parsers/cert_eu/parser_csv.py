8036cc93a34cb72e88d868bbcde9b420c8d08a70
Antoine Neuenschwander <antoine.neuenschwander@switch.ch>
2018-08-10 16:40:50 +0200

    Improvements to CERT-EU CSV parser.

--- intelmq/bots/parsers/cert_eu/parser_csv.py
+++ intelmq/bots/parsers/cert_eu/parser_csv.py
@@ -2,11 +2,12 @@
 CERT-EU parser
 """
 from intelmq.lib.bot import ParserBot
+from collections import defaultdict
 
 
 class CertEUCSVParserBot(ParserBot):
 
-    abuse_to_intelmq = {
+    abuse_to_intelmq = defaultdict(lambda: "unknown", {
         "backdoor": "backdoor",
         "blacklist": "blacklist",
         "botnet drone": "botnet drone",
@@ -14,7 +15,7 @@ class CertEUCSVParserBot(ParserBot):
         "c&c": "c&c",
         "compromised server": "compromised",
         "ddos infrastructure": "ddos",
-        "ddos target": "--",
+        "ddos target": "ddos",
         "defacement": "defacement",
         "dropzone": "dropzone",
         "exploit url": "exploit",
@@ -26,47 +27,52 @@ class CertEUCSVParserBot(ParserBot):
         "scanner": "scanner",
         "spam infrastructure": "spam",
         "test": "test",
-        "vulnerable service": "vulnerable service",
-        "--": "unknown"
-    }
+        "vulnerable service": "vulnerable service"
+    })
 
     csv_fieldnames = [
         "datasource", "source ip", "observation time", "tlp", "description",
         "type", "count", "source time", "source country", "protocol",
         "destination port", "source latitude", "source city", "source cc",
         "source longitude", "first_seen", "num_sensors", "confidence level",
-        "last_seen", "target", "phishing", "asn", "domain name"]
+        "last_seen", "target", "url", "asn", "domain name"]
     ignore_lines_starting = ["#"]
 
     def parse_line(self, line, report):
+        extra = {}
         event = self.new_event(report)
-        # event["?"] = line["datasource"]
-        event["source.ip"] = line["source ip"]
-        event["time.observation"] = line["observation time"]
-        event["tlp"] = line["tlp"]
-        event["event_description.text"] = line["description"]  # TBC
-        event["classification.type"] = self.abuse_to_intelmq.get(
-            line["type"], "unknown"
-        )
-        # event["?"] = line["count"]
-        event["time.source"] = line["source time"]
-        event["source.geolocation.country"] = line["source country"]
-        event["protocol.application"] = line["protocol"]
-        event["destination.port"] = line["destination port"]
-        event["source.geolocation.latitude"] = line["source latitude"]
-        event["source.geolocation.city"] = line["source city"]
-        event["source.geolocation.geoip_cc"] = line["source cc"]
-        event["source.geolocation.longitude"] = line["source longitude"]
-        # event["?"] = line["first_seen"]
-        # event["?"] = line["num_sensors"]
-        event["feed.accuracy"] = line["confidence level"]  # TBC is 0<=x<=100 ?
-        # event["?"] = line["last seen"]
-        event["event_description.target"] = line["target"]  # TBC
-        event["source.url"] = line["phishing"]  # TBC or destination.url?
-        event["source.asn"] = line["asn"]  # TBC or destination?
-        event["source.fqdn"] = line["domain name"]  # TBC or destination?
+        if "datasource" in line:
+            extra["datasource"] = line["datasource"]
+        event.add("source.ip", line["source ip"])
+        event.add("time.observation", line["observation time"])
+        event.add("tlp", line["tlp"])
+        event.add("event_description.text", line["description"])
+        event.add("classification.type", self.abuse_to_intelmq[line["type"]])
+        if "count" in line:
+            extra["count"] = line["count"]
+        event.add("time.source", line["source time"])
+        event.add("source.geolocation.country", line["source country"])
+        event.add("protocol.application", line["protocol"])
+        event.add("destination.port", line["destination port"])
+        event.add("source.geolocation.latitude", line["source latitude"])
+        event.add("source.geolocation.city", line["source city"])
+        event.add("source.geolocation.geoip_cc", line["source cc"])
+        event.add("source.geolocation.longitude", line["source longitude"])
+        if "first_seen" in line:
+            extra["first_seen"] = line["first_seen"]
+        if "num_sensors" in line:
+            extra["num_sensors"] = line["num_sensors"]
+        event.add("feed.accuracy", line["confidence level"])
+        if "last_seen" in line:
+            extra["last_seen"] = line["last_seen"]
+        event.add("event_description.target", line["target"])
+        event.add("source.url", line["url"])
+        event.add("source.asn", line["asn"])
+        event.add("source.fqdn", line["domain name"])
 
-        event["raw"] = self.recover_line(line)
+        event.add("raw", self.recover_line(line))
+        if extra:
+            event.add("extra", extra)
         yield event
 
     parse = ParserBot.parse_csv_dict
--- intelmq/tests/bots/parsers/cert_eu/test_parser_csv.py
+++ intelmq/tests/bots/parsers/cert_eu/test_parser_csv.py
@@ -20,7 +20,7 @@ EXAMPLE_EVENT1 = {
     "feed.url": "https://www.cert-eu.eu",
     "feed.name": "CERT-EU Feed",
     "source.ip": "109.125.93.10",
-    "raw": "ZGF0YXNvdXJjZSxzb3VyY2UgaXAsb2JzZXJ2YXRpb24gdGltZSx0bHAsZGVzY3JpcHRpb24sdHlwZSxjb3VudCxzb3VyY2UgdGltZSxzb3VyY2UgY291bnRyeSxwcm90b2NvbCxkZXN0aW5hdGlvbiBwb3J0LHNvdXJjZSBsYXRpdHVkZSxzb3VyY2UgY2l0eSxzb3VyY2UgY2Msc291cmNlIGxvbmdpdHVkZSxmaXJzdF9zZWVuLG51bV9zZW5zb3JzLGNvbmZpZGVuY2UgbGV2ZWwsbGFzdF9zZWVuLHRhcmdldCxwaGlzaGluZyxhc24sZG9tYWluIG5hbWUNClNSLDEwOS4xMjUuOTMuMTAsMjAxOC0wNy0wNiAxMDo1OTowNlosZ3JlZW4sQSBVUkwgaXMgdGhlIG1vc3QgY29tbW9uIHJlc291cmNlIHdpdGggcmVmZXJlbmNlIHRvIG1hbHdhcmUgYmluYXJ5IGRpc3RyaWJ1dGlvbi4sbWFsd2FyZSB1cmwsNiwyMDE4LTA3LTA2IDA4OjUyOjE2WixHZXJtYW55LHNtYmQsNDQ1LDQ4LjEzNzQyOCxNdW5pY2gsREUsMTEuNTc1NDksMjAxOC0wNy0wNlQwODozNDo1NS43NTkwMDAsMSw1MCwyMDE4LTA3LTA2VDA4OjUyOjE2LjEwOTAwMCwsLCwNCg==",
+    "raw": "ZGF0YXNvdXJjZSxzb3VyY2UgaXAsb2JzZXJ2YXRpb24gdGltZSx0bHAsZGVzY3JpcHRpb24sdHlwZSxjb3VudCxzb3VyY2UgdGltZSxzb3VyY2UgY291bnRyeSxwcm90b2NvbCxkZXN0aW5hdGlvbiBwb3J0LHNvdXJjZSBsYXRpdHVkZSxzb3VyY2UgY2l0eSxzb3VyY2UgY2Msc291cmNlIGxvbmdpdHVkZSxmaXJzdF9zZWVuLG51bV9zZW5zb3JzLGNvbmZpZGVuY2UgbGV2ZWwsbGFzdF9zZWVuLHRhcmdldCx1cmwsYXNuLGRvbWFpbiBuYW1lDQpTUiwxMDkuMTI1LjkzLjEwLDIwMTgtMDctMDYgMTA6NTk6MDZaLGdyZWVuLEEgVVJMIGlzIHRoZSBtb3N0IGNvbW1vbiByZXNvdXJjZSB3aXRoIHJlZmVyZW5jZSB0byBtYWx3YXJlIGJpbmFyeSBkaXN0cmlidXRpb24uLG1hbHdhcmUgdXJsLDYsMjAxOC0wNy0wNiAwODo1MjoxNlosR2VybWFueSxzbWJkLDQ0NSw0OC4xMzc0MjgsTXVuaWNoLERFLDExLjU3NTQ5LDIwMTgtMDctMDZUMDg6MzQ6NTUuNzU5MDAwLDEsNTAsMjAxOC0wNy0wNlQwODo1MjoxNi4xMDkwMDAsLCwsDQo=",
     "time.observation": "2018-07-06T10:59:06+00:00",
     "classification.type": "malware",
     "time.source": "2018-07-06T08:52:16+00:00",
@@ -34,11 +34,11 @@ EXAMPLE_EVENT1 = {
     "source.geolocation.geoip_cc": "DE",
     "source.geolocation.longitude": 11.57549,
     "feed.accuracy": 50.0,
-    # "event_description.target": None,
-    # "source.url": None,
-    # "source.asn": None,
-    # "source.asn": None,
-    # "source.fqdn": None,
+    "extra.count": '6',
+    "extra.datasource": "SR",
+    "extra.first_seen": "2018-07-06T08:34:55.759000",
+    "extra.last_seen": "2018-07-06T08:52:16.109000",
+    "extra.num_sensors": "1",
     "__type": "Event"
 }
 
@@ -46,7 +46,7 @@ EXAMPLE_EVENT2 = {
     "feed.url": "https://www.cert-eu.eu",
     "feed.name": "CERT-EU Feed",
     "source.ip": "144.76.221.34",
-    "raw": "ZGF0YXNvdXJjZSxzb3VyY2UgaXAsb2JzZXJ2YXRpb24gdGltZSx0bHAsZGVzY3JpcHRpb24sdHlwZSxjb3VudCxzb3VyY2UgdGltZSxzb3VyY2UgY291bnRyeSxwcm90b2NvbCxkZXN0aW5hdGlvbiBwb3J0LHNvdXJjZSBsYXRpdHVkZSxzb3VyY2UgY2l0eSxzb3VyY2UgY2Msc291cmNlIGxvbmdpdHVkZSxmaXJzdF9zZWVuLG51bV9zZW5zb3JzLGNvbmZpZGVuY2UgbGV2ZWwsbGFzdF9zZWVuLHRhcmdldCxwaGlzaGluZyxhc24sZG9tYWluIG5hbWUNClosMTQ0Ljc2LjIyMS4zNCwyMDE4LTA3LTA2IDExOjM0OjUzWixncmVlbixUaGlzIHR5cGUgbW9zdCBvZnRlbiByZWZlcnMgdG8gYSBVUkwgd2hpY2ggaXMgdHJ5aW5nIHRvIGRlZnJhdWQgdGhlIHVzZXJzIG9mIHRoZWlyIGNyZWRlbnRpYWxzLixwaGlzaGluZywsMjAxOC0wNy0wNSAxNDozMjowNlosR2VybWFueSwsLDQ5LjQ0Nzc4MSxOdXJlbWJlcmcsREUsMTEuMDY4MzMsLCw1MCwsT3RoZXIsaHR0cHM6Ly9qYW5hZ2FtZXMuY29tL2RldGFpbHMvcGFja2FnZS12aWV3L2RobC5jb20vLDI0OTQwLGphbmFnYW1lcy5jb20NCg==",
+    "raw": "ZGF0YXNvdXJjZSxzb3VyY2UgaXAsb2JzZXJ2YXRpb24gdGltZSx0bHAsZGVzY3JpcHRpb24sdHlwZSxjb3VudCxzb3VyY2UgdGltZSxzb3VyY2UgY291bnRyeSxwcm90b2NvbCxkZXN0aW5hdGlvbiBwb3J0LHNvdXJjZSBsYXRpdHVkZSxzb3VyY2UgY2l0eSxzb3VyY2UgY2Msc291cmNlIGxvbmdpdHVkZSxmaXJzdF9zZWVuLG51bV9zZW5zb3JzLGNvbmZpZGVuY2UgbGV2ZWwsbGFzdF9zZWVuLHRhcmdldCx1cmwsYXNuLGRvbWFpbiBuYW1lDQpaLDE0NC43Ni4yMjEuMzQsMjAxOC0wNy0wNiAxMTozNDo1M1osZ3JlZW4sVGhpcyB0eXBlIG1vc3Qgb2Z0ZW4gcmVmZXJzIHRvIGEgVVJMIHdoaWNoIGlzIHRyeWluZyB0byBkZWZyYXVkIHRoZSB1c2VycyBvZiB0aGVpciBjcmVkZW50aWFscy4scGhpc2hpbmcsLDIwMTgtMDctMDUgMTQ6MzI6MDZaLEdlcm1hbnksLCw0OS40NDc3ODEsTnVyZW1iZXJnLERFLDExLjA2ODMzLCwsNTAsLE90aGVyLGh0dHBzOi8vamFuYWdhbWVzLmNvbS9kZXRhaWxzL3BhY2thZ2Utdmlldy9kaGwuY29tLywyNDk0MCxqYW5hZ2FtZXMuY29tDQo=",
     "time.observation": "2018-07-06T10:59:06+00:00",
     "time.source": "2018-07-05T14:32:06+00:00",
     "tlp": "GREEN",
@@ -62,6 +62,7 @@ EXAMPLE_EVENT2 = {
     "source.asn": 24940,
     "source.fqdn": "janagames.com",
     "classification.type": "phishing",
+    "extra.datasource": "Z",
     "__type": "Event"
  }
 
