637dfab36a36ab1ba86e817c7fc5a787f34960bb
Sebastian Wagner <wagner@cert.at>
2018-11-29 12:05:23 +0100

    ENH: some feedback from lgtm implemented

--- intelmq/bots/parsers/shadowserver/parser.py
+++ intelmq/bots/parsers/shadowserver/parser.py
@@ -44,7 +44,7 @@ class ShadowserverParserBot(ParserBot):
         conf = self.sparser_config
 
         # https://github.com/certtools/intelmq/issues/1271
-        if conf is config.drone and row.get('infection') == 'spam':
+        if conf == config.drone and row.get('infection') == 'spam':
             conf = config.drone_spam
 
         # we need to copy here...
