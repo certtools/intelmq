539a0bdaa91cb1f3cbdb8b6b43e69823c5ccba6e
Tomas Bellus <tomas.bellus@gmail.com>
2019-03-06 12:53:51 +0100

    TST: adapt tests

--- intelmq/tests/bots/parsers/abusech/test_parser_ip.py
+++ intelmq/tests/bots/parsers/abusech/test_parser_ip.py
@@ -8,27 +8,27 @@ import intelmq.lib.utils as utils
 from intelmq.bots.parsers.abusech.parser_ip import AbusechIPParserBot
 
 with open(os.path.join(os.path.dirname(__file__), 'feodoips.txt')) as handle:
-    EXAMPLE_FILE = handle.read()
+    EXAMPLE_FEODO_FILE = handle.read()
 
+EXAMPLE_FEODO_REPORT = {"feed.url": "https://feodotracker.abuse.ch/downloads/ipblocklist.csv",
+                        "feed.name": "AbuseCH Feodotracker",
+                        "__type": "Report",
+                        "raw": utils.base64_encode(EXAMPLE_FEODO_FILE),
+                        "time.observation": "2015-11-02T13:11:44+00:00"
+                       }
 
-EXAMPLE_REPORT = {"feed.url": "https://feodotracker.abuse.ch/blocklist/?download=ipblocklist",
-                  "feed.name": "AbuseCH Feodotracker",
-                  "__type": "Report",
-                  "raw": utils.base64_encode(EXAMPLE_FILE),
-                  "time.observation": "2015-11-02T13:11:44+00:00"
-                  }
-
-EXAMPLE_EVENT = {"feed.url": "https://feodotracker.abuse.ch/blocklist/?download=ipblocklist",
-                 "feed.name": "AbuseCH Feodotracker",
-                 "source.ip": "103.232.215.133",
-                 "raw": "MTAzLjIzMi4yMTUuMTMz",
-                 "time.observation": "2015-11-02T13:11:44+00:00",
-                 "time.source": "2016-02-23T15:11:38+00:00",
-                 "classification.taxonomy": "malicious code",
-                 "classification.type": "c&c",
-                 "malware.name": "cridex",
-                 "__type": "Event"
-                 }
+EXAMPLE_FEODO_EVENT = {"feed.url": "https://feodotracker.abuse.ch/downloads/ipblocklist.csv",
+                       "feed.name": "AbuseCH Feodotracker",
+                       "source.ip": "110.93.230.101",
+                       "source.port": 990,
+                       "raw": utils.base64_encode("2019-03-05 22:10:24,110.93.230.101,990,Heodo"),
+                       "time.observation": "2015-11-02T13:11:44+00:00",
+                       "time.source": "2019-03-05T22:10:24+00:00",
+                       "classification.taxonomy": "malicious code",
+                       "classification.type": "c&c",
+                       "malware.name": "heodo",
+                       "__type": "Event"
+                       }
 
 
 class TestAbusechIPParserBot(test.BotTestCase, unittest.TestCase):
@@ -39,12 +39,12 @@ class TestAbusechIPParserBot(test.BotTestCase, unittest.TestCase):
     @classmethod
     def set_bot(cls):
         cls.bot_reference = AbusechIPParserBot
-        cls.default_input_message = EXAMPLE_REPORT
+        cls.default_input_message = EXAMPLE_FEODO_REPORT
 
-    def test_event(self):
+    def test_feodo_event(self):
         """ Test if correct Event has been produced. """
         self.run_bot()
-        self.assertMessageEqual(0, EXAMPLE_EVENT)
+        self.assertMessageEqual(0, EXAMPLE_FEODO_EVENT)
 
 
 if __name__ == '__main__':  # pragma: no cover
