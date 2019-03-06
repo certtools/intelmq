5eae97ff37a5efd9ae40f91a7b8e498b4921e3d2
Tomas Bellus <tomas.bellus@gmail.com>
2019-03-06 15:47:00 +0100

    Minor abusech ip parser refactor
    Get time observation from Feodotracker

--- intelmq/bots/parsers/abusech/parser_ip.py
+++ intelmq/bots/parsers/abusech/parser_ip.py
@@ -14,7 +14,6 @@ from intelmq.lib.bot import ParserBot
 from intelmq.lib import utils
 from intelmq.lib.exceptions import PipelineError
 
-
 FEEDS = {
     'https://feodotracker.abuse.ch/downloads/ipblocklist.csv': {
         'format': [
@@ -48,12 +47,13 @@ class AbusechIPParserBot(ParserBot):
         regex = re.compile(r'^#+.*')  # filter out comments
         top_line = list(filter(lambda i: not regex.search(i), lines))[0]
         feed = report['feed.url']
+        # TODO: Edit lastgenerated to correspond to the date in the feed file header online
 
         fields = top_line.split(',')
         if len(fields) is not len(FEEDS[feed]['format']):
             self.logger.warning(
                 f"Feed '{feed}' has not the expected length of fields: {len(fields)} != {len(FEEDS[feed]['format'])}")
-            raise PipelineError("Abusech ip parser bad config")
+            raise PipelineError("Abusech ip parser is not up to date with the format online")
 
         for line in utils.base64_decode(report.get("raw")).splitlines():
             line = line.strip()
@@ -63,36 +63,42 @@ class AbusechIPParserBot(ParserBot):
     def parse_line(self, line, report):
         if line.startswith('#'):
             self.tempdata.append(line)
-            if 'Generated on' in line:
-                row = line.strip('# ')[13:]
-                self.lastgenerated = dateutil.parser.parse(row).isoformat()
+            if 'Last updated' in line:
+                self.lastgenerated = dateutil.parser.parse(
+                    re.search('[0-9]{4}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}( UTC)?', line).group(0)).isoformat()
         else:
             event = self.new_event(report)
-            self.__process_feed_fields(event, line, report['feed.url'])
+            self.__process_defaults(event, line, report['feed.url'])
+            self.__process_fields(event, line, report['feed.url'])
+            print(f'------------------ event: {event}')
             yield event
+
+    def __process_defaults(self, event, line, feed_url):
+        defaults = set([
+            ('time.source', None),
+            ('malware.name', FEEDS[feed_url]['malware']),
+            ('raw', line),
+            ('classification.type', 'c&c'),
+            ('classification.taxonomy', 'malicious code'),
+            ('time.observation', self.lastgenerated)
+        ])
+
+        for i in defaults:
+            if i[0] not in FEEDS[feed_url]['format']:
+                if i[0] == 'time.observation' and i[1] is not None:
+                    print(f'------------------ is: {i[0]} {i[1]}')
+                    event.add(i[0], i[1], overwrite=True)
+                elif i[0] != 'time.observation':
+                    event.add(i[0], i[1])
+                else:
+                    pass
+
+    def __process_fields(self, event, line, feed_url):
+        for field, value in zip(FEEDS[feed_url]['format'], line.split(',')):
+            event.add(field, value)
 
     def recover_line(self, line):
         return '\n'.join(self.tempdata + [line])
-
-    def __process_feed_fields(self, event, line, feed):
-        event.add('classification.taxonomy', 'malicious code')
-        event.add('classification.type', 'c&c')
-        event.add('raw', line)
-
-        current_feed_format = FEEDS[feed]['format']
-        data = line.split(',') if len(current_feed_format) > 1 else line
-
-        if isinstance(data, list):
-            # has multiple fields
-            i = 0
-            for field in current_feed_format:
-                event.add(field, data[i])
-                i += 1
-        else:
-            # has only one 'ip' field
-            event.add('time.source', self.lastgenerated)
-            event.add('source.ip', line)
-            event.add('malware.name', FEEDS[feed]['malware'])
 
 
 BOT = AbusechIPParserBot
