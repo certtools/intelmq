6a5488d3cb8ee64500c167f419ce5620d97ab45a
Tomas Bellus <tomas.bellus@gmail.com>
2019-03-19 13:43:04 +0100

    Requested changes
    Minor code cleanup and refactoring

--- intelmq/bots/parsers/abusech/parser_ip.py
+++ intelmq/bots/parsers/abusech/parser_ip.py
@@ -40,47 +40,47 @@ FEEDS = {
 
 
 class AbusechIPParserBot(ParserBot):
-    lastgenerated = None
+    __last_generated_date = None
+    __is_comment_line_regex = re.compile(r'^#+.*')
+    __date_regex = re.compile(r'[0-9]{4}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}( UTC)?')
 
     def parse(self, report: dict):
-        lines = utils.base64_decode(report.get("raw")).splitlines()
-        regex = re.compile(r'^#+.*')  # filter out comments
-        top_line = list(filter(lambda i: not regex.search(i), lines))[0]
         feed = report['feed.url']
 
-        fields = top_line.split(',')
+        raw_lines = utils.base64_decode(report.get("raw")).splitlines()
+        comments = list(r for r in raw_lines if self.__is_comment_line_regex.search(r))
+
+        fields = comments[-1].split(',')
         if len(fields) is not len(FEEDS[feed]['format']):
             self.logger.warning("Feed '{}' has not the expected fields: {} != {}".format(feed,
                                                                                          len(fields),
                                                                                          len(FEEDS[feed]['format'])))
             raise PipelineError("Abusech ip parser is not up to date with the format online")
 
-        for line in utils.base64_decode(report.get("raw")).splitlines():
+        for line in comments:
+            if 'Last updated' in line:
+                self.__last_generated_date = dateutil.parser.parse(self.__date_regex.search(line).group(0)).isoformat()
+
+        lines = (l for l in raw_lines if not self.__is_comment_line_regex.search(l))
+        for line in lines:
             line = line.strip()
             if not any([line.startswith(prefix) for prefix in self.ignore_lines_starting]):
                 yield line
 
     def parse_line(self, line, report):
-        if line.startswith('#'):
-            self.tempdata.append(line)
-            if 'Last updated' in line:
-                self.lastgenerated = dateutil.parser.parse(
-                    re.search('[0-9]{4}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}( UTC)?', line).group(
-                        0)).isoformat()
-        else:
-            event = self.new_event(report)
-            self.__process_defaults(event, line, report['feed.url'])
-            self.__process_fields(event, line, report['feed.url'])
-            yield event
+        event = self.new_event(report)
+        self.__process_defaults(event, line, report['feed.url'])
+        self.__process_fields(event, line, report['feed.url'])
+        yield event
 
     def __process_defaults(self, event, line, feed_url):
-        defaults = set([
+        defaults = {
             ('malware.name', FEEDS[feed_url]['malware']),
             ('raw', line),
             ('classification.type', 'c&c'),
             ('classification.taxonomy', 'malicious code'),
-            ('time.observation', self.lastgenerated)
-        ])
+            ('time.observation', self.__last_generated_date)
+        }
 
         for i in defaults:
             if i[0] not in FEEDS[feed_url]['format']:
@@ -89,7 +89,8 @@ class AbusechIPParserBot(ParserBot):
                 else:
                     event.add(i[0], i[1], overwrite=True)
 
-    def __process_fields(self, event, line, feed_url):
+    @staticmethod
+    def __process_fields(event, line, feed_url):
         for field, value in zip(FEEDS[feed_url]['format'], line.split(',')):
             if field == 'time.source':
                 ts = dateutil.parser.parse(value + ' UTC').isoformat() if not value.endswith(' UTC') else value
