5ffae01b5d2e38538d9b1789c332fb3a61fdb2e0
Sebastian Wagner <wagner@cert.at>
2019-05-13 14:17:50 +0200

    BUG: lib/bot: fix __init__ parameters
    this caused the sighup handling to fail for all collectors and parsers

--- intelmq/lib/bot.py
+++ intelmq/lib/bot.py
@@ -760,7 +760,7 @@ class ParserBot(Bot):
     handle = None
     current_line = None
 
-    def __init__(self, bot_id):
+    def __init__(self, bot_id: str, start=False, sighup_event=None):
         super().__init__(bot_id=bot_id)
         if self.__class__.__name__ == 'ParserBot':
             self.logger.error('ParserBot can\'t be started itself. '
@@ -934,7 +934,7 @@ class CollectorBot(Bot):
     Does some sanity checks on message sending.
     """
 
-    def __init__(self, bot_id: str):
+    def __init__(self, bot_id: str, start=False, sighup_event=None):
         super().__init__(bot_id=bot_id)
         if self.__class__.__name__ == 'CollectorBot':
             self.logger.error('CollectorBot can\'t be started itself. '
