ccd30b9fe0ed8542d9386fc4c88266f9698f3dc8
Jeremy Crews <jeremy.m.crews@gmail.com>
2019-07-26 19:37:37 +0000

    hajime-scanner

--- intelmq/bots/parsers/netlab_360/parser.py
+++ intelmq/bots/parsers/netlab_360/parser.py
@@ -46,6 +46,11 @@ class Netlab360ParserBot(ParserBot):
                 event.add('destination.port', value[2].replace('dport=', ''))
                 event.add('classification.type', 'scanner')
                 event.add('classification.identifier', 'mirai', overwrite=True)
+            elif report['feed.url'] in Netlab360ParserBot.HAJIME_SCANNER_FEED:
+                event.add('time.source', value[0] + ' UTC')
+                event.add('source.ip', value[1].replace('ip=', ''))
+                event.add('classification.type', 'scanner')
+                event.add('classification.identifier', 'hajime', overwrite=True)
             else:
                 raise ValueError('Unknown data feed %s.' % report['feed.url'])
 
