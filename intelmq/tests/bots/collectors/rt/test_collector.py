117738279ecfb4a0b678e1415263bd86ac6f46ac
Sebastian Wagner <wagner@cert.at>
2019-08-14 17:53:33 +0200

    TST: rt collector test: handle msising ticket id
    if the ticket id was missing, the test loading failed

--- intelmq/tests/bots/collectors/rt/test_collector.py
+++ intelmq/tests/bots/collectors/rt/test_collector.py
@@ -15,21 +15,6 @@ rt_password = os.getenv('INTELMQ_TEST_RT_PASSWORD')
 rt_subject = os.getenv('INTELMQ_TEST_RT_SUBJECT')
 rt_ticket = os.getenv('INTELMQ_TEST_RT_TICKET')
 
-OUTPUT = {"__type": "Report",
-          "feed.name": "Example feed",
-          "feed.accuracy": 100.,
-          "raw": utils.base64_encode('bar text\n'),
-          "extra.file_name": "foobar",
-          "extra.email_subject": "Incoming IntelMQ Test Report",
-          "extra.ticket_subject": "Incoming IntelMQ Test Report",
-          "extra.ticket_requestors": "wagner@cert.at",
-          "extra.email_from": "wagner@cert.at",
-          "extra.ticket_queue": "Incident Reports",
-          "extra.ticket_status": "new",
-          "extra.ticket_owner": "intelmq",
-          "rtir_id": utils.lazy_int(rt_ticket),
-          }
-
 
 @unittest.skipIf(not all((rt_url, rt_username, rt_password, rt_subject, rt_ticket)),
                  "Not provided all env variables needed for this test.")
@@ -55,9 +40,25 @@ class TestRTCollectorBot(test.BotTestCase, unittest.TestCase):
                          }
 
     def test_report(self):
-        """ Test if Report has been produced. """
+        """
+        Test if Report has been produced.
+        output defined inline as rt_ticket must be known and must not be None
+        """
         self.run_bot(iterations=1)
-        self.assertMessageEqual(0, OUTPUT)
+        self.assertMessageEqual(0, {"__type": "Report",
+              "feed.name": "Example feed",
+              "feed.accuracy": 100.,
+              "raw": utils.base64_encode('bar text\n'),
+              "extra.file_name": "foobar",
+              "extra.email_subject": "Incoming IntelMQ Test Report",
+              "extra.ticket_subject": "Incoming IntelMQ Test Report",
+              "extra.ticket_requestors": "wagner@cert.at",
+              "extra.email_from": "wagner@cert.at",
+              "extra.ticket_queue": "Incident Reports",
+              "extra.ticket_status": "new",
+              "extra.ticket_owner": "intelmq",
+              "rtir_id": utils.lazy_int(rt_ticket),
+              })
 
 
 if __name__ == '__main__':  # pragma: no cover
