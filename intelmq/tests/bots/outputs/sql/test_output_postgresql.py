a6852cb2a2448e39b9308831139a51592de5f3a7
Sebastian Wagner <wagner@cert.at>
2019-09-11 15:26:01 +0200

    ENH: Add postgresql output bot compatibility layer

--- intelmq/tests/bots/outputs/sql/test_output_postgresql.py
+++ intelmq/tests/bots/outputs/sql/test_output_postgresql.py
@@ -4,6 +4,7 @@ import unittest
 
 import intelmq.lib.test as test
 from intelmq.bots.outputs.sql.output import SQLOutputBot
+from intelmq.bots.outputs.postgresql.output import PostgreSQLOutputBot
 
 if os.environ.get('INTELMQ_TEST_DATABASES'):
     import psycopg2
@@ -17,6 +18,8 @@ INPUT1 = {"__type": "Event",
           "source.ip": "192.0.2.1",
           "feed.name": "Example Feed",
           }
+OUTPUT1 = INPUT1.copy()
+del OUTPUT1['__type']
 INPUT_EXTRA = {"__type": "Event",
                "classification.type": "vulnerable service",
                "extra.asn": 64496,
@@ -56,9 +59,8 @@ class TestSQLOutputBot(test.BotTestCase, unittest.TestCase):
         self.cur.execute('SELECT "classification.identifier", "classification.type", "source.asn",'
                          ' "source.ip", "feed.name" FROM tests WHERE "source.asn" = 64496')
         self.assertEqual(self.cur.rowcount, 1)
-        del INPUT1['__type']
         from_db = {k: v for k, v in self.cur.fetchone().items() if v is not None}
-        self.assertDictEqual(from_db, INPUT1)
+        self.assertDictEqual(from_db, OUTPUT1)
 
     def test_extra(self):
         """
@@ -71,6 +73,50 @@ class TestSQLOutputBot(test.BotTestCase, unittest.TestCase):
         self.assertEqual(self.cur.rowcount, 1)
         from_db = {k: v for k, v in self.cur.fetchone().items() if v is not None}
         self.assertEqual(from_db['extra'], {"asn": 64496, "ip": "192.0.2.1"})
+
+    @classmethod
+    def tearDownClass(cls):
+        if not os.environ.get('INTELMQ_TEST_DATABASES'):
+            return
+        cls.cur.execute('TRUNCATE "tests"')
+        cls.cur.close()
+        cls.con.close()
+
+
+@test.skip_database()
+class TestPostgreSQLOutputBot(test.BotTestCase, unittest.TestCase):
+
+    @classmethod
+    def set_bot(cls):
+        cls.bot_reference = PostgreSQLOutputBot
+        cls.default_input_message = INPUT1
+        cls.sysconfig = {"host": "localhost",
+                         "port": 5432,
+                         "database": "intelmq",
+                         "user": "intelmq",
+                         "password": "intelmq",
+                         "sslmode": "allow",
+                         "table": "tests"}
+        if not os.environ.get('INTELMQ_TEST_DATABASES'):
+            return
+        cls.con = psycopg2.connect(database=cls.sysconfig['database'],
+                                   user=cls.sysconfig['user'],
+                                   password=cls.sysconfig['password'],
+                                   host=cls.sysconfig['host'],
+                                   port=cls.sysconfig['port'],
+                                   sslmode=cls.sysconfig['sslmode'],
+                                   )
+        cls.con.autocommit = True
+        cls.cur = cls.con.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    def test_event(self):
+        self.allowed_warning_count = 1
+        self.run_bot()
+        self.cur.execute('SELECT "classification.identifier", "classification.type", "source.asn",'
+                         ' "source.ip", "feed.name" FROM tests WHERE "source.asn" = 64496')
+        self.assertEqual(self.cur.rowcount, 1)
+        from_db = {k: v for k, v in self.cur.fetchone().items() if v is not None}
+        self.assertDictEqual(from_db, OUTPUT1)
 
     @classmethod
     def tearDownClass(cls):
