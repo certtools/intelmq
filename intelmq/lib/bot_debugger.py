1ed633ce6bbffc3b8b9436cbdd66091a7566eae9
Sebastian Wagner <wagner@cert.at>
2019-09-18 15:52:49 +0200

    BUG: lib/bot_debugger: fix mocked send_message
    fixes certtools/intelmq#1453

--- intelmq/lib/bot_debugger.py
+++ intelmq/lib/bot_debugger.py
@@ -44,7 +44,7 @@ class BotDebugger:
 
         bot = getattr(module, 'BOT')
         if run_subcommand == "message":
-            bot.init = lambda *args: None
+            bot.init = lambda *args, **kwargs: None
         self.instance = bot(bot_id, disable_multithreading=True)
 
         if not run_subcommand:
@@ -123,20 +123,22 @@ class BotDebugger:
             if not self.instance._Bot__source_pipeline:
                 # is None if source pipeline does not exist
                 self.instance._Bot__source_pipeline = Pipeline(None)
-            self.instance._Bot__source_pipeline.receive = lambda: msg
-            self.instance._Bot__source_pipeline.acknowledge = lambda: None
+            self.instance._Bot__source_pipeline.receive = lambda *args, **kwargs: msg
+            self.instance._Bot__source_pipeline.acknowledge = lambda *args, **kwargs: None
             self.instance.logger.info(" * Message from cli will be used when processing.")
 
         if dryrun:
-            self.instance.send_message = lambda msg, path="_default": self.instance.logger.info(
-                "DRYRUN: Message would be sent now{}!".format(" to the {} path".format(path) if (path != "_default") else ""))
-            self.instance.acknowledge_message = lambda: self.instance.logger.info("DRYRUN: Message would be acknowledged now!")
+            self.instance.send_message = lambda *args, **kwargs: self.instance.logger.info(
+                "DRYRUN: Message would be sent now to %r!",
+                kwargs.get('path', "_default"))
+            self.instance.acknowledge_message = lambda *args, **kwargs: self.instance.logger.info(
+                "DRYRUN: Message would be acknowledged now!")
             self.instance.logger.info(" * Dryrun only, no message will be really sent through.")
 
         if show:
             fn = self.instance.send_message
-            self.instance.send_message = lambda msg=None, path="_default": [self.pprint(msg or "No message generated"),
-                                                                            fn(msg, path=path)]
+            self.instance.send_message = lambda *args, **kwargs: [self.pprint(args or "No message generated"),
+                                                                  fn(*args, **kwargs)]
 
         self.instance.logger.info("Processing...")
         self.instance.process()
