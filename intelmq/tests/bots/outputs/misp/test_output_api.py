3bfe230bc3a44e012cc2a48827ca43d58b171138
Bernhard Reiter <bernhard@intevation.de>
2020-02-18 15:59:54 +0100

    STY: fix codestyle of test_output_api

--- intelmq/tests/bots/outputs/misp/test_output_api.py
+++ intelmq/tests/bots/outputs/misp/test_output_api.py
@@ -1,10 +1,9 @@
 import os
-import sys
-import unittest
+#  import unittest
 
-import intelmq.lib.test as test
+#  import intelmq.lib.test as test
 if os.environ.get('INTELMQ_TEST_EXOTIC'):
-    from intelmq.bots.outputs.misp.output_api import MISPAPIOutputBot
+    from intelmq.bots.outputs.misp.output_api import MISPAPIOutputBot  # noqa
 
 # This file is a stub
 # We cannot do much more as we are missing a mock MISP instance to use
