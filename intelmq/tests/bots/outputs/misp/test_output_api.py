3ecf7464af0a64c90fd27698fd601582e91d35c9
Bernhard Reiter <bernhard@intevation.de>
2020-02-18 12:59:27 +0100

    ENH: MISP API Output bot, add test stub

---
+++ intelmq/tests/bots/outputs/misp/test_output_api.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import unittest
+
+import intelmq.lib.test as test
+if os.environ.get('INTELMQ_TEST_EXOTIC'):
+    from intelmq.bots.outputs.misp.output_api import MISPAPIOutputBot
+
+# This file is a stub
+# We cannot do much more as we are missing a mock MISP instance to use
+# to initialise pymisp
