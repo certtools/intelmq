64191bf8708330b28a2d15096393512bb938e2ad
Mikk Margus Möll <mikk@cert.ee>
2020-06-25 14:12:28 +0300

    Remove f-string, correct indentation

--- intelmq/bots/collectors/eset/collector.py
+++ intelmq/bots/collectors/eset/collector.py
@@ -3,50 +3,50 @@ from intelmq.lib.bot import CollectorBot
 from intelmq.lib.utils import base64_encode
 
 try:
-  import cabby
+    import cabby
 except ImportError:
-  cabby = None
+    cabby = None
 
 
 class ESETCollectorBot(CollectorBot):
-  def init(self):
-    if cabby == None:
-      raise ValueError('Library "cabby" not installed')
+    def init(self):
+        if cabby == None:
+            raise ValueError('Library "cabby" not installed')
 
-  def process(self):
-    user = self.parameters.username
-    passwd = self.parameters.password
-    endpoint = self.parameters.endpoint
-    time_delta = self.parameters.time_delta
-    collection = self.parameters.collection
+    def process(self):
+        user = self.parameters.username
+        passwd = self.parameters.password
+        endpoint = self.parameters.endpoint
+        time_delta = self.parameters.time_delta
+        collection = self.parameters.collection
 
-    domains = []
-    end = datetime.datetime.now(datetime.timezone.utc)
-    start = end - datetime.timedelta(seconds=time_delta) # only fetch from up to time_delta seconds ago
+        domains = []
+        end = datetime.datetime.now(datetime.timezone.utc)
+        start = end - datetime.timedelta(seconds=time_delta) # only fetch from up to time_delta seconds ago
 
-    self.logger.info('Authenticating')
+        self.logger.info('Authenticating')
 
-    client = cabby.create_client(endpoint, discovery_path="/taxiiservice/discovery", use_https=True)
-    client.set_auth(username=user, password=passwd)
+        client = cabby.create_client(endpoint, discovery_path="/taxiiservice/discovery", use_https=True)
+        client.set_auth(username=user, password=passwd)
 
-    self.logger.info('Fetching data from ESET TAXII endpoint')
+        self.logger.info('Fetching data from ESET TAXII endpoint')
 
-    for item in client.poll(collection, begin_date=start, end_date=end):
-      if not item.content:
-        continue # skip empty items
+        for item in client.poll(collection, begin_date=start, end_date=end):
+            if not item.content:
+                continue # skip empty items
 
-      data = json.loads(item.content)
-      self.logger.debug('data: ' + str(data))
-      self.logger.debug('domains: ' + str(domains))
-      for domain in data:
-        domain['_eset_feed'] = self.parameters.collection
+            data = json.loads(item.content)
+            self.logger.debug('data: ' + str(data))
+            self.logger.debug('domains: ' + str(domains))
+            for domain in data:
+                domain['_eset_feed'] = self.parameters.collection
 
-      domains += data
+            domains += data
 
-    self.logger.info('Submitting data')
+        self.logger.info('Submitting data')
 
-    report = self.new_report()
-    report.add('raw', json.dumps(domains))
-    self.send_message(report)
+        report = self.new_report()
+        report.add('raw', json.dumps(domains))
+        self.send_message(report)
 
 BOT = ESETCollectorBot
--- intelmq/bots/parsers/eset/parser.py
+++ intelmq/bots/parsers/eset/parser.py
@@ -2,82 +2,82 @@ import json, re
 from intelmq.lib.utils import base64_encode, base64_decode
 
 message_taxonomy_map = {
-  'Host actively distributes high-severity threat in the form of executable code.': 'malware',
-  'Host is known to be distributing low-risk and potentially unwanted content.': 'malware',
-  'Host is known source of phishing or other fraudulent content.': 'phishing',
-  'Host is known to be actively distributing adware or other medium-risk software.': 'other',
-  'Host is known source of active fraudulent content.': 'other'
+    'Host actively distributes high-severity threat in the form of executable code.': 'malware',
+    'Host is known to be distributing low-risk and potentially unwanted content.': 'malware',
+    'Host is known source of phishing or other fraudulent content.': 'phishing',
+    'Host is known to be actively distributing adware or other medium-risk software.': 'other',
+    'Host is known source of active fraudulent content.': 'other'
 }
 
 from intelmq.lib.bot import ParserBot
 
 class ESETParserBot(ParserBot):
-  def init(self):
-    self.f_map = {
-      'ei.urls (json)': self.urls_parse,
-      'ei.domains v2 (json)': self.domains_parse
-    }
-    self.pattern = re.compile('\.'.join(['(25[0-5]|2[0-4][0-9]|1?[0-9]?[0-9])']*4)) # IPv4
+    def init(self):
+        self.f_map = {
+            'ei.urls (json)': self.urls_parse,
+            'ei.domains v2 (json)': self.domains_parse
+        }
+        self.pattern = re.compile('\.'.join(['(25[0-5]|2[0-4][0-9]|1?[0-9]?[0-9])']*4)) # IPv4
 
-  def parse(self, report): # yield single sections for parse_line to parse
-    data = json.loads(base64_decode(report['raw']))
-    for section in data:
-      yield json.dumps(section)
+    def parse(self, report): # yield single sections for parse_line to parse
+        data = json.loads(base64_decode(report['raw']))
+        for section in data:
+            yield json.dumps(section)
 
-  def parse_line(self, line, report): # parse a section of the received report
-    event = self.new_event(report)
-    line = json.loads(line)
-    feed_name = line['_eset_feed']
+    def parse_line(self, line, report): # parse a section of the received report
+        event = self.new_event(report)
+        line = json.loads(line)
+        feed_name = line['_eset_feed']
 
-    self.common_parse(event, line)
-    f = self.f_map.get(feed_name, None)
-    if not f:
-      raise ValueError('Unsupported feed')
+        self.common_parse(event, line)
+        f = self.f_map.get(feed_name, None)
+        if not f:
+            raise ValueError('Unsupported feed')
 
-    f(event, line)
+        f(event, line)
 
-    yield event
-
-
-  @staticmethod
-  def _get_taxonomy(reason):
-    tax = message_taxonomy_map.get(reason, None)
-    if tax: # was found in dictionary
-      return tax
-    elif reason.startswith('Host is used as command and control server'): # dynamic section after that
-      return 'c2server'
-    else:
-      return 'other'
-
-  @staticmethod
-  def domains_parse(event, line):
-    event.add('time.source', line['last_seen'])
+        yield event
 
 
-  @staticmethod
-  def urls_parse(event, line):
-    event.add('time.source', line['domain_last_seen'])
-    event.add('source.url', line['url'])
+    @staticmethod
+    def _get_taxonomy(reason):
+        tax = message_taxonomy_map.get(reason, None)
+        if tax: # was found in dictionary
+            return tax
+        elif reason.startswith('Host is used as command and control server'): # dynamic section after that
+            return 'c2server'
+        else:
+            return 'other'
+
+    @staticmethod
+    def domains_parse(event, line):
+        event.add('time.source', line['last_seen'])
 
 
-  def common_parse(self, event, line):
-    type = self._get_taxonomy(line['reason'])
-    feed_name = line['_eset_feed']
+    @staticmethod
+    def urls_parse(event, line):
+        event.add('time.source', line['domain_last_seen'])
+        event.add('source.url', line['url'])
 
-    event.add('raw', base64_encode(json.dumps(line)))
-    event.add('event_description.text', line['reason'])
-    event.add('classification.type', type)
-    if not self.is_ipv4(line['domain']):
-      event.add('source.fqdn', line['domain']) # IP addresses are not permitted in FQDN, only domain names
 
-    event.add('source.ip', line['ip'])
-    event.add('feed.name', f'ESET Threat Intelligence Service ({feed_name})')
-    event.add('feed.provider', 'ESET')
-    event.add('feed.url', 'https://eti.eset.com/taxiiservice/discovery')
-    event.add('feed.documentation', 'https://www.eset.com/int/business/services/threat-intelligence/')
+    def common_parse(self, event, line):
+        type = self._get_taxonomy(line['reason'])
+        feed_name = line['_eset_feed']
 
-  def is_ipv4(self, address):
-    return bool(self.pattern.fullmatch(address))
+        event.add('raw', base64_encode(json.dumps(line)))
+        event.add('event_description.text', line['reason'])
+        event.add('classification.type', type)
+        if not self.is_ipv4(line['domain']):
+            event.add('source.fqdn', line['domain']) # IP addresses are not permitted in FQDN, only domain names
+
+        event.add('source.ip', line['ip'])
+        event.add('feed.name', 'ESET Threat Intelligence Service ({})'.format(feed_name))
+        event.add('feed.provider', 'ESET')
+        event.add('feed.url', 'https://eti.eset.com/taxiiservice/discovery')
+        event.add('feed.documentation', 'https://www.eset.com/int/business/services/threat-intelligence/')
+
+    def is_ipv4(self, address):
+        return bool(self.pattern.fullmatch(address))
 
 
 BOT = ESETParserBot
