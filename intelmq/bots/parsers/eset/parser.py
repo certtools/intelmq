c02a664ef677b1186cdca66ea0d69dde6c0e3361
Mikk Margus Möll <mikk@cert.ee>
2020-07-14 09:49:05 +0300

    ESET: Filter out None in line["ip"] as well

--- intelmq/bots/parsers/eset/parser.py
+++ intelmq/bots/parsers/eset/parser.py
@@ -61,7 +61,7 @@ class ESETParserBot(ParserBot):
         event.add('source.fqdn', line['domain'], raise_failure=False)  # IP addresses may be passed in line['domain']
 
         ip = line['ip']
-        if not ip.endswith('_NXDOMAIN'):
+        if ip and not ip.endswith('_NXDOMAIN'):
             event.add('source.ip', ip)
 
     recover_line = ParserBot.recover_line_json
