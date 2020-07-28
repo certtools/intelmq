83621f863bd0df7a8318441852b1ce888059ec2c
Thomas Hungenberg <th-certbund@users.noreply.github.com>
2020-07-28 12:07:35 +0200

    Shadowserver 'blacklist' report renamed to 'blocklist'

--- intelmq/tests/bots/parsers/shadowserver/test_blocklist.py
+++ intelmq/tests/bots/parsers/shadowserver/test_blocklist.py
@@ -7,20 +7,20 @@ import intelmq.lib.test as test
 import intelmq.lib.utils as utils
 from intelmq.bots.parsers.shadowserver.parser import ShadowserverParserBot
 
-with open(os.path.join(os.path.dirname(__file__), 'testdata/blacklist.csv')) as handle:
+with open(os.path.join(os.path.dirname(__file__), 'testdata/blocklist.csv')) as handle:
     EXAMPLE_FILE = handle.read()
 EXAMPLE_LINES = EXAMPLE_FILE.splitlines()
 
 EXAMPLE_REPORT = {
-    'feed.name': 'Blacklist',
+    'feed.name': 'Blocklist',
     "raw": utils.base64_encode(EXAMPLE_FILE),
     "__type": "Report",
     "time.observation": "2015-01-01T00:00:00+00:00",
-    "extra.file_name": "2019-01-01-blacklist-test-geo.csv",
+    "extra.file_name": "2019-01-01-blocklist-test-geo.csv",
 }
 EVENTS = [{
     '__type': 'Event',
-    'feed.name': 'Blacklist',
+    'feed.name': 'Blocklist',
     "classification.identifier": "blacklisted-ip",
     "classification.taxonomy": "other",
     "classification.type": "blacklist",
@@ -40,8 +40,8 @@ EVENTS = [{
 },
 {
     '__type': 'Event',
-    'feed.name': 'Blacklist',
-     "classification.identifier": "blacklisted-ip",
+    'feed.name': 'Blocklist',
+    "classification.identifier": "blacklisted-ip",
     "classification.taxonomy": "other",
     "classification.type": "blacklist",
     "extra.naics": 517311,
