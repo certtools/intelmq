a2d20df6fd4fa0386fe79e66156537336faf92b0
Sebastian Wagner <wagner@cert.at>
2020-08-11 18:57:51 +0200

    ENH/STY: RT output bot: dependency exception
    and some codestyle fixes

--- intelmq/bots/outputs/rt/output.py
+++ intelmq/bots/outputs/rt/output.py
@@ -7,7 +7,7 @@ Parameters:
 rt_uri, rt_user, rt_password, verify_cert -  RT API endpoint
 queue - ticket destination queue
 CF_mapping - mapping attributes-ticket CFs
-final_status - what is final status for the created ticket 
+final_status - what is final status for the created ticket
 create_investigation - should we create Investigation ticket (in case of RTIR workflow)
 fieldnames - attributes to include into investigation ticket
 description_attr - which event attribute contains text message being sent to the recipient
@@ -15,23 +15,25 @@ description_attr - which event attribute contains text message being sent to the
 """
 
 from intelmq.lib.bot import Bot
+from intelmq.lib.exceptions import MissingDependencyError
+
 try:
     import rt
 except ImportError:
     rt = None
-    
+
+
 class RTOutputBot(Bot):
 
     def init(self):
         if rt is None:
-            self.logger.error('Could not import rt. Please install it.')
-            self.stop()
+            raise MissingDependencyError('rt', version='1.0.9')
 
         # Event attributes to be included in Investigation ticket communication
         self.fieldnames = getattr(self.parameters, 'investigation_fields')
         if isinstance(self.fieldnames, str):
             self.fieldnames = self.fieldnames.split(',')
-        # Investigations ticket can be created only when we work with Incidents ticket 
+        # Investigations ticket can be created only when we work with Incidents ticket
         # and there is a parameter create_investigation provided
         if getattr(self.parameters, 'queue', 'None') == 'Incidents' and getattr(self.parameters, 'create_investigation', False):
             self.create_investigation = True
@@ -44,24 +46,24 @@ class RTOutputBot(Bot):
         self.user = getattr(self.parameters, 'rt_user', None)
         self.password = getattr(self.parameters, 'rt_password', None)
         self.description_attr = getattr(self.parameters, 'description_attr', None)
-         
+
     def process(self):
         event = self.receive_message()
         RT = rt.Rt(self.uri, verify_cert=self.verify_cert)
         if not RT.login(self.user,
-                   self.password):
-            raise ValueError('Login failed.')          
+                        self.password):
+            raise ValueError('Login failed.')
         kwargs = {}
         # we make subject in form of "Incident notification, IP"
-        self.subject = 'Incident notification';
-        self.content = '';
+        self.subject = 'Incident notification'
+        self.content = ''
 
         if event.get('source.ip'):
-            self.subject += ", " + event['source.ip']       
+            self.subject += ", " + event['source.ip']
         if event.get(self.description_attr):
             self.content = event[self.description_attr] + "\n\n"
-        
-        ticket_content = self.content    
+
+        ticket_content = self.content
         for key, value in event.items():
             # Add all event attributes to the body of the incident ticket
             ticket_content += key + ": " + str(value) + "\n"
@@ -84,7 +86,7 @@ class RTOutputBot(Bot):
                 # Creating Investigation ticket
                 # Communication supposed to be done by RT On Create scrip
                 investigation_id = RT.create_ticket(Queue="Investigations", Subject=self.subject, Text=ticket_content, Requestor=requestor)
-                
+
                 if investigation_id > -1:
                     self.logger.debug("Investigation ticket created: %i", investigation_id)
                     # make a link between Incident and Investigation tickets
@@ -98,9 +100,10 @@ class RTOutputBot(Bot):
                 if RT.edit_ticket(ticket_id, Status=self.final_status):
                     self.logger.debug("Status changed to %s for ticket %i", self.final_status, ticket_id)
                 else:
-                    self.logger.error("Failed to change status for RT ticket %i", ticket_id)            
+                    self.logger.error("Failed to change status for RT ticket %i", ticket_id)
         else:
             self.logger.error("Failed to create RT ticket")
         self.acknowledge_message()
+
 
 BOT = RTOutputBot
