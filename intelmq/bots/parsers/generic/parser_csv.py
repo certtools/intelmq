e4bdba7cf84ecb9c0f0fc15702e4f4b426ea23e7
kbrajneesh <kbrajneesh@gmail.com>
2020-12-01 20:53:23 +0530

    ENH: ignore tabs,spaces in new line or before comment

--- intelmq/bots/parsers/generic/parser_csv.py
+++ intelmq/bots/parsers/generic/parser_csv.py
@@ -73,10 +73,12 @@ class GenericCsvParserBot(ParserBot):
     def parse(self, report):
         raw_report = utils.base64_decode(report.get("raw"))
         raw_report = raw_report.translate({0: None})
-        # ignore lines starting with #
-        raw_report = re.sub(r'(?m)^#.*\n?', '', raw_report)
+        # ignore lines starting with #. # can have leading spaces/tabs
+        raw_report = re.sub(r'(?m)^[ \t]*#.*\n?', '', raw_report)
         # ignore null bytes
         raw_report = re.sub(r'(?m)\0', '', raw_report)
+        # ignore lines having mix of spaces and tabs only
+        raw_report = re.sub(r'(?m)^[ \t]*\n?', '', raw_report)
         # skip header
         if getattr(self.parameters, 'skip_header', False):
             self.tempdata.append(raw_report[:raw_report.find('\n')])
