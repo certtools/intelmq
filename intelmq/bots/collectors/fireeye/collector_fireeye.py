99c2befb5aac3c5ebe051d745bfef74a4f4e5713
CysihZ <milcert-git@bmlv.gv.at>
2021-01-04 15:22:32 +0100

    modified:   collectors/fireeye/collector_fireeye.py 	modified:   parsers/fireeye/parser.py

--- intelmq/bots/collectors/fireeye/collector_fireeye.py
+++ intelmq/bots/collectors/fireeye/collector_fireeye.py
@@ -51,7 +51,7 @@ class FireeyeCollectorBot(CollectorBot):
                             new_report.add("raw", binary)
                             self.send_message(new_report)
                 except KeyError:
-                    self.logger.debug("no Iocs Available")
+                    self.logger.debug("No Iocs Available")
 
     def init(self):
         if requests is None:
@@ -61,55 +61,54 @@ class FireeyeCollectorBot(CollectorBot):
 
         self.session = create_request_session_from_bot(self)
         # kein proxy im lokalen netz
-        self.session.proxies={}
+        self.session.proxies = {}
 
     def process(self):
-        # define params 
-        dns_name=self.parameters.dns_name
-        request_duration=self.parameters.request_duration
-        user=self.parameters.http_username
-        pw=self.parameters.http_password
+        # define params
+        dns_name = self.parameters.dns_name
+        request_duration = self.parameters.request_duration
+        user = self.parameters.http_username
+        pw = self.parameters.http_password
 
         # create auth token
-        token= user + ":" + pw
-        message_bytes=token.encode('ascii')
-        base64_bytes=base64.b64encode(message_bytes)
-        base64_message=base64_bytes.decode('ascii')
-        http_header={'Authorization': 'Basic ' + base64_message}
+        token = user + ":" + pw
+        message_bytes = token.encode('ascii')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('ascii')
+        http_header = {'Authorization': 'Basic ' + base64_message}
         # get token for requestst
-        auth_url="https://" + dns_name + "/wsapis/v2.0.0/auth/login"
-        resp=self.session.post(url=auth_url, headers=http_header)
+        auth_url = "https://" + dns_name + "/wsapis/v2.0.0/auth/login"
+        resp = self.session.post(url=auth_url, headers=http_header)
 
         # extract token and build auth header
-        token=resp.headers['X-FeApi-Token']
+        token = resp.headers['X-FeApi-Token']
         self.logger.info('Token:   ' + token)
-        http_header={'X-FeApi-Token': token, 'Accept': 'application/json'}
-        http_url="https://" + dns_name + "/wsapis/v2.0.0/alerts?duration=" + request_duration
+        http_header = {'X-FeApi-Token': token, 'Accept': 'application/json'}
+        http_url = "https://" + dns_name + "/wsapis/v2.0.0/alerts?duration=" + request_duration
 
         self.logger.info("Downloading report from %r.", http_url)
-        resp=self.session.get(url=http_url , headers=http_header)
+        resp = self.session.get(url=http_url, headers=http_header)
         if resp.status_code // 100 != 2:
             self.logger.info('Could not connect to appliance check User/PW. Is the aplliance reachable?')
             raise ValueError('HTTP response status code was %i.' % resp.status_code)
 
         self.logger.debug("Report downloaded.")
-        message=json.loads(resp.content)
-        if  message['alert'][0]:
-            new_report=self.new_report()
+        message = json.loads(resp.content)
+        if message['alert'][0]:
+            new_report = self.new_report()
             for alert in message['alert']:
-               self.logger.debug("got a new message")
-# if (alert['product'] == 'WEB_MPS' and alert['severity'] != 'MINR' and alert['name'] == 'MALWARE_CALLBACK'):
-# web fireye mit kritischen alert md5sum ist hier der gecklickten link!
+                self.logger.debug("got a new message")
                 self.logger.debug('PRODUCT: ' + alert['product'] + "  UUID:  " + alert['uuid'])
                 if alert['product'] == 'EMAIL_MPS' and alert['name'] == 'MALWARE_OBJECT':
-                   for k, v in alert['src'].items():
-                       uuid=alert['uuid']
-                       event=self.xml_processor(uuid, token, new_report, dns_name, product="EMAIL_MPS")
+                    for k, v in alert['src'].items():
+                        uuid=alert['uuid']
+                        event=self.xml_processor(uuid, token, new_report, dns_name, product = "EMAIL_MPS")
                 if alert['product'] == 'MAS' and alert['name'] == 'MALWARE_OBJECT':
                     uuid=alert['uuid']
                     self.xml_processor(uuid, token, new_report, dns_name, product="MAS")
 
             self.logger.debug("Report transmitted")
             self.logger.debug(str(resp.content))
+
 
 BOT = FireeyeCollectorBot
--- intelmq/bots/parsers/fireeye/parser.py
+++ intelmq/bots/parsers/fireeye/parser.py
@@ -3,7 +3,7 @@
 Fireeye Parser Bot
 Retrieves a base64 encoded JSON-String from raw and converts it into an
 event.
-""" 
+"""
 from intelmq.lib import utils
 from intelmq.lib.bot import ParserBot
 from intelmq.lib.message import MessageFactory
@@ -26,69 +26,67 @@ class FireeyeParserBot(ParserBot):
                 hashValue = indicator['Content']['#text']
                 indicatorType = indicator['Context']['@search']
                 if indicatorType == 'FileItem/Md5sum':
-                   event.add('malware.hash.md5', indicator['Content']['#text'])
+                    event.add('malware.hash.md5', indicator['Content']['#text'])
                 if indicatorType == 'FileItem/Sha256sum':
-                   self.logger.debug('FileItem/Md5sum aus uuid'+ indicator['Content']['#text'])
-                   event.add('malware.hash.sha256', indicator['Content']['#text'])
-                   self.send_message(event)
-                   data = raw_report.split('<Indicator id')
-                   uuidres = data[0].split('"alert_id">')
-                   uuid = uuidres[1].split('"')
-                   self.logger.debug("My UUDI is:  " + uuid[0])
-                   data.pop(0)
-                   data.pop(0)
-                   for Indicator in data:
-                      event = self.new_event(report)
-                      if "Network" in Indicator:
-                         fqdn=""
-                         urlpath=""
-                         IndicatorItem = Indicator.split('<IndicatorItem condition')
-                         for datainIndicator in IndicatorItem:
-                            if "search=" in datainIndicator:
-                                   search = datainIndicator.split('search="')
-                                   for searchIndicator in search:
-                                          classification = ""
-                                          if '"/>' in searchIndicator:
-                                             context_search = searchIndicator.split('"/>')
-                                             # context inhalt
-                                             if context_search[0] == "Network/HTTP/RequestURI":
+                    self.logger.debug('FileItem/Md5sum aus uuid'+ indicator['Content']['#text'])
+                    event.add('malware.hash.sha256', indicator['Content']['#text'])
+                    self.send_message(event)
+                    data = raw_report.split('<Indicator id')
+                    uuidres = data[0].split('"alert_id">')
+                    uuid = uuidres[1].split('"')
+                    self.logger.debug("My UUDI is:  " + uuid[0])
+                    data.pop(0)
+                    data.pop(0)
+                    for Indicator in data:
+                        event = self.new_event(report)
+                        if "Network" in Indicator:
+                            fqdn=""
+                            urlpath=""
+                            IndicatorItem = Indicator.split('<IndicatorItem condition')
+                            for datainIndicator in IndicatorItem:
+                                if "search=" in datainIndicator:
+                                    search = datainIndicator.split('search="')
+                                    for searchIndicator in search:
+                                        classification = ""
+                                        if '"/>' in searchIndicator:
+                                            context_search = searchIndicator.split('"/>')
+                                            # context inhalt
+                                            if context_search[0] == "Network/HTTP/RequestURI":
                                                 classification = "destination.urlpath"
-                                             if context_search[0] == "Network/HTTP/Host":
+                                            if context_search[0] == "Network/HTTP/Host":
                                                 classification = "destination.ip"
-                                             if context_search[0] == "Network/Connection/RemotePort":
+                                            if context_search[0] == "Network/Connection/RemotePort":
                                                 classification = "destination.port"
-                                             if context_search[0] == "Network/Connection/Protocol":
+                                            if context_search[0] == "Network/Connection/Protocol":
                                                 classification = "protocol.transport"
-                                             if context_search[0] == "Network/DNS":
+                                            if context_search[0] == "Network/DNS":
                                                 classification = "destination.fqdn"
-                                             #if context_search[0] == "Network/HTTP/UserAgent":
-                                             #   classification = ""
-                                          if 'Content' in searchIndicator:
-                                             Content_search= searchIndicator.split('">')
-                                             context = Content_search[1].split('</Content>')
-                                             self.logger.debug(classification   +"   "+context[0])
-                                             if fqdn != "" and urlpath!= "":
+                                        if 'Content' in searchIndicator:
+                                            Content_search= searchIndicator.split('">')
+                                            context = Content_search[1].split('</Content>')
+                                            self.logger.debug(classification   +"   "+context[0])
+                                            if fqdn != "" and urlpath!= "":
                                                 event.add("destination.url", "http://"+fqdn+urlpath)
                                                 fqdn=""
                                                 urlpath=""
-                                             if classification == "destination.ip":
+                                            if classification == "destination.ip":
                                                 try: 
                                                    ipaddress.IPv4Network(context[0])
                                                    event.add('destination.ip', context[0])
                                                    break
                                                 except ValueError: 
                                                    break
-                                             elif classification == "destination.fqdn":
+                                            elif classification == "destination.fqdn":
                                                 fqdn = context[0]
                                                 event.add(classification, context[0])
-                                             elif classification == "destination.urlpath":
+                                            elif classification == "destination.urlpath":
                                                 urlpath = context[0]
                                                 event.add(classification, context[0])
-                                             elif classification == "destination.port":
+                                            elif classification == "destination.port":
                                                 event.add(classification, int(context[0]))
-                      self.send_message(event)
-        self.acknowledge_message()
+                    self.send_message(event) 
+            self.acknowledge_message()
         except KeyError:
-            self.logger.info("no Iocs Available")
+            self.logger.info("No Iocs Available")
 
 BOT = FireeyeParserBot
