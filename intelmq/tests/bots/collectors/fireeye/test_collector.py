1e32caff186f85f7b58ddb4bf2bcec1198ce26a7
CysihZ <milcert-git@bmlv.gv.at>
2021-02-15 15:59:49 +0100

    deleted:    intelmq/tests/bots/collectors/fireeye/fireeyeFristRequest.json 	modified:   intelmq/tests/bots/collectors/fireeye/test_collector.py 	modified:   intelmq/tests/bots/parsers/fireeye/test_parser_fireeye.py

--- intelmq/tests/bots/collectors/fireeye/test_collector.py
+++ intelmq/tests/bots/collectors/fireeye/test_collector.py
@@ -8,15 +8,15 @@ import intelmq.lib.test as test
 from intelmq.bots.collectors.fireeye.collector_fireeye import FireeyeCollectorBot
 
 RANDSTR = secrets.token_urlsafe(50)
-ASSET_PATH_FIRST = pathlib.Path(__file__).parent /'../../../assets/fireeyeFristRequest.json'
-ASSET_PATH_SECOND = pathlib.Path(__file__).parent /'../../../assets/fireeyeSecondRequest.xml'
+ASSET_PATH_FIRST = pathlib.Path(__file__).parent / '../../../assets/fireeyeFristRequest.json'
+ASSET_PATH_SECOND = pathlib.Path(__file__).parent / '../../../assets/fireeyeSecondRequest.xml'
 PARAMETERS = {'dns_name': 'myfireeye.local', 'http_username': RANDSTR, 'http_password': RANDSTR, 'logging_level': 'DEBUG', 'request_duration': '24_hours', 'name': 'FireeyeCollector'}
 
 
 def prepare_mocker(mocker):
-    mocker.post('https://myfireeye.local/wsapis/v2.0.0/auth/login', headers= {'X-FeApi-Token': '1234567890'} )
-    mocker.get('https://myfireeye.local/wsapis/v2.0.0/alerts?duration=24_hours',text=ASSET_PATH_FIRST.read_text())
-    mocker.get('https://myfireeye.local/wsapis/v2.0.0/openioc?alert_uuid=1591de22-4926-4124-b3ed-ffff96766295',text=ASSET_PATH_SECOND.read_text())
+    mocker.post('https://myfireeye.local/wsapis/v2.0.0/auth/login', headers={'X-FeApi-Token': '1234567890'})
+    mocker.get('https://myfireeye.local/wsapis/v2.0.0/alerts?duration=24_hours', text=ASSET_PATH_FIRST.read_text())
+    mocker.get('https://myfireeye.local/wsapis/v2.0.0/openioc?alert_uuid=1591de22-4926-4124-b3ed-ffff96766295', text=ASSET_PATH_SECOND.read_text())
 
 
 @requests_mock.Mocker()
@@ -40,13 +40,13 @@ class TestFireeyeCollectorBot(test.BotTestCase, unittest.TestCase):
 
     def test_wrong_login(self, mocker):
         prepare_mocker(mocker)
-        mocker.post('https://myfireeye.local/wsapis/v2.0.0/auth/login', status_code= 500)
-        self.run_bot(iterations=1, parameters=PARAMETERS,allowed_error_count=1,allowed_warning_count=1)
+        mocker.post('https://myfireeye.local/wsapis/v2.0.0/auth/login', status_code=500)
+        self.run_bot(iterations=1, parameters=PARAMETERS, allowed_error_count=1, allowed_warning_count=1)
         self.assertLogMatches('ValueError: Could not connect to appliance check User/PW. HTTP response status code was 500.')
 
     def test_report_send(self, mocker):
         prepare_mocker(mocker)
-        self.run_bot(iterations=1, parameters=PARAMETERS,allowed_warning_count=1)
+        self.run_bot(iterations=1, parameters=PARAMETERS, allowed_warning_count=1)
         self.assertAnyLoglineEqual('Processed 1 messages since last logging.', 'INFO')
 
 
