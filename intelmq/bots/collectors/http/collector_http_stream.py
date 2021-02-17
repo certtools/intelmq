929fd5df1e5c2078d759f4dceb219832db23608a
Sebastian Waldbauer <waldbauer@cert.at>
2021-02-17 12:14:23 +0100

    [FIX] HTTP Stream Collector better error handling
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- intelmq/bots/collectors/http/collector_http_stream.py
+++ intelmq/bots/collectors/http/collector_http_stream.py
@@ -19,6 +19,9 @@ try:
 except ImportError:
     requests = None
 
+from http.client import IncompleteRead
+from urllib3.exceptions import ProtocolError
+
 from intelmq.lib.bot import CollectorBot
 from intelmq.lib.utils import decode, create_request_session
 from intelmq.lib.exceptions import MissingDependencyError
@@ -34,6 +37,9 @@ class HTTPStreamCollectorBot(CollectorBot):
 
         self.set_request_parameters()
         self.session = create_request_session(self)
+        if(not self.parameters.max_error_retries):
+            self.parameters.max_error_retries = 3
+        self.err_count = 0
 
     def process(self):
         self.logger.info("Connecting to stream at %r.", self.parameters.http_url)
@@ -48,18 +54,37 @@ class HTTPStreamCollectorBot(CollectorBot):
                 raise ValueError('HTTP response status code was {}.'
                                  ''.format(req.status_code))
 
-            for line in req.iter_lines():
-                if self.parameters.strip_lines:
-                    line = line.strip()
+            try:
+                for line in req.iter_lines():
+                    if self.parameters.strip_lines:
+                        line = line.strip()
 
-                if not line:
-                    # filter out keep-alive new lines and empty lines
-                    continue
+                    if not line:
+                        # filter out keep-alive new lines and empty lines
+                        continue
 
-                report = self.new_report()
-                report.add("raw", decode(line))
-                report.add("feed.url", self.parameters.http_url)
-                self.send_message(report)
+                    self.err_count = 0
+
+                    report = self.new_report()
+                    report.add("raw", decode(line))
+                    report.add("feed.url", self.parameters.http_url)
+                    self.send_message(report)
+            except requests.exceptions.ChunkedEncodingError:
+                self.err_count = self.err_count + 1
+                if(self.err_count >= self.parameters.error_max_retries):
+                    self.err_count = 0
+                    raise requests.exceptions.ChunkedEncodingError
+            except ProtocolError:
+                self.err_count = self.err_count + 1
+                if(self.err_count >= self.parameters.error_max_retries):
+                    self.err_count = 0
+                    raise ProtocolError
+            except IncompleteRead:
+                self.err_count = self.err_count + 1
+                if(self.err_count >= self.parameters.error_max_retries):
+                    self.err_count = 0
+                    raise IncompleteRead
+            
             self.logger.info('Stream stopped.')
 
 
