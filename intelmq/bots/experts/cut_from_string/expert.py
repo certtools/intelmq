ed0299d18d0a2a4b1a4ac6415b085890ddfb66c2
marius.karotkis <marius.karotkis@gmail.com>
2021-06-08 09:45:35 +0300

    change field name, remove init function

--- intelmq/bots/experts/cut_from_string/expert.py
+++ intelmq/bots/experts/cut_from_string/expert.py
@@ -8,23 +8,20 @@ from intelmq.lib.bot import Bot
 class CutFromStringExpertBot(Bot):
     string_from_start: bool = True  # True - from start, False - from end
     string_for_cut: str = 'www.'
-    field_for_cut: str = 'source.fqdn'
-
-    def init(self):
-        pass
+    field: str = 'source.fqdn'
 
     def process(self):
         event = self.receive_message()
 
-        if self.field_for_cut in event:
-            field_string = event[self.field_for_cut]
+        if self.field in event:
+            field_string = event[self.field]
             if self.string_from_start == 1 and field_string.startswith(self.string_for_cut):
                 field_string = field_string[len(self.string_for_cut):]
-                event.change(self.field_for_cut, field_string)
+                event.change(self.field, field_string)
 
             if self.string_from_start == 0 and field_string.endswith(self.string_for_cut):
                 field_string = field_string[:-len(self.string_for_cut)]
-                event.change(self.field_for_cut, field_string)
+                event.change(self.field, field_string)
 
         self.send_message(event)
         self.acknowledge_message()
