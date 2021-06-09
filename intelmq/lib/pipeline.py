277f72d019eab234501a3e91176186e673b880ba
Birger Schacht <schacht@cert.at>
2021-06-09 12:22:28 +0200

    FIX: Use the default loader of ruamel yaml
    Ruamel YAML defaults to the rt (round-trip) loader, which preservs
    comments.

--- intelmq/lib/pipeline.py
+++ intelmq/lib/pipeline.py
@@ -105,15 +105,15 @@ class Pipeline(object):
                 q = {"_default": queues}
             elif type_ is str:
                 q = {"_default": queues.split()}
-            elif type_ is dict:
+            elif isinstance(queues, dict):
                 q = queues
                 for key, val in queues.items():
-                    q[key] = val if type(val) is list else val.split()
+                    q[key] = val if isinstance(val, list) else str(val).split()
             else:
                 raise exceptions.InvalidArgument(
                     'queues', got=queues,
                     expected=["None", "list of strings", "dict (of strings or lists that should have the _default key)"])
-            self.destination_queues = q
+            self.destination_queues = dict(q)
         else:
             raise exceptions.InvalidArgument('queues_type', got=queues_type, expected=['source', 'destination'])
 
