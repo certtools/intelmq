ebd9e99f18214f2dde355637e42da2a6dc63c0c8
marius.karotkis <marius.karotkis@gmail.com>
2021-06-14 16:30:18 +0300

    Small changes

--- intelmq/bots/experts/time_filter/expert.py
+++ intelmq/bots/experts/time_filter/expert.py
@@ -7,25 +7,25 @@ from datetime import timezone
 
 
 class TimeFilterExpertBot(Bot):
-    search_field: str = 'time.source'
-    search_from: str  = '1d'
+    """ Time based filtering """
+    field: str = 'time.source'
+    timespan: str  = '1d'
 
-    not_after = None
+    __delta = None
 
     def init(self):
-        self.search_field = self.search_field
-        if self.search_from:
-            timedelta_params = get_timedelta(self.search_from)
-            self.not_after = datetime.now(tz=timezone.utc) - timedelta(**timedelta_params)
+        if self.field:
+            timedelta_params = get_timedelta(self.timespan)
+            self.__delta = datetime.now(tz=timezone.utc) - timedelta(**timedelta_params)
 
     def process(self):
         event = self.receive_message()
-        # time based filtering
-        if self.search_field in event:
+        event_time = self.__delta
+
+        if self.field in event:
             try:
-                event_time = parser.parse(str(event.get(self.search_field)))
+                event_time = parser.parse(str(event.get(self.field)))
             except ValueError:
-                event_time = self.not_after
                 self.process_message(event_time, event)
                 return
             else:
@@ -33,23 +33,19 @@ class TimeFilterExpertBot(Bot):
                 return
         else:
             # not found field
-            event_time = self.not_after
             self.process_message(event_time, event)
             return
 
     def process_message(self, event_time, event):
         event_time = event_time.replace(tzinfo=None)
-        self.not_after = self.not_after.replace(tzinfo=None)
+        self.__delta = self.__delta.replace(tzinfo=None)
 
-        if event_time < self.not_after:
-            self.acknowledge_message()
-            self.logger.debug(
-                f"Filtered out event with search field {self.search_field} and event time {event_time} .")
-            return
-        else:
+        if event_time > self.__delta:
             self.send_message(event)
-            self.acknowledge_message()
-            return
-
+        else:
+            self.logger.debug(f"Filtered out event with search field {self.field} and event time {event_time} .")
+            
+        self.acknowledge_message()
+            
 
 BOT = TimeFilterExpertBot
