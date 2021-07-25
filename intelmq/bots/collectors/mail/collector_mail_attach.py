e0b1ac63a8ece9ee2a2d705dc596b74d96f28b61
Alex Kaplan <alexkaplan.mail@gmail.com>
2021-07-25 13:35:17 +0300

    BUG: provide filename when reading a non-archived file from mail

--- intelmq/bots/collectors/mail/collector_mail_attach.py
+++ intelmq/bots/collectors/mail/collector_mail_attach.py
@@ -60,7 +60,7 @@ class MailAttachCollectorBot(MailCollectorBot):
                     raw_reports = unzip(attach['content'].read(), self.extract_files,
                                         return_names=True, logger=self.logger)
                 else:
-                    raw_reports = ((None, attach['content'].read()), )
+                    raw_reports = ((attach_filename, attach['content'].read()), )
 
                 for file_name, raw_report in raw_reports:
                     report = self.new_report()
