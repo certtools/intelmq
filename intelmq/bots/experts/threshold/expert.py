certtools/intelmq

commit 2a1b12640947906e9ee1ae6a62aa48d16ed3c487

FIX: Use CacheMixins instead of lib.cache
Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

intelmq/bots/experts/threshold/expert.py
@@ -50,11 +50,11 @@ Parameters:
 from typing import Iterable, Optional
 
 from intelmq.lib.bot import Bot
-from intelmq.lib.cache import Cache
 from intelmq.lib.exceptions import ConfigurationError
+from intelmq.lib.mixins import CacheMixin
 
 
-class ThresholdExpertBot(Bot):
+class ThresholdExpertBot(Bot, CacheMixin):
     """Check if the number of similar messages during a specified time interval exceeds a set value"""
     add_keys: dict = {"comment": "Threshold reached"}
     filter_keys: Iterable = ["raw", "time.observation"]
@@ -72,12 +72,6 @@ class ThresholdExpertBot(Bot):
     bypass = False
 
     def init(self):
-        self.cache = Cache(self.redis_cache_host,
-                           self.redis_cache_port,
-                           self.redis_cache_db,
-                           self.timeout,
-                           self.redis_cache_password
-                           )
         if self.timeout <= 0:
             raise ConfigurationError('Timeout', 'Invalid timeout specified, use positive integer seconds.')
         if self.threshold <= 0:
@@ -91,12 +85,12 @@ class ThresholdExpertBot(Bot):
         else:
             message_hash = message.hash(filter_keys=self.filter_keys,
                                         filter_type=self.filter_type)
-            old_count = int(self.cache.get(message_hash) or 0)
+            old_count = int(self.cache_get(message_hash) or 0)
             self.logger.debug('Message %s has been seen %i times before.',
                               message_hash, old_count)
             # Use Redis "set" instead of "incr" to reset the timeout
             # every time
-            self.cache.set(message_hash, str(old_count + 1))
+            self.cache_set(message_hash, str(old_count + 1))
             if old_count + 1 == self.threshold:
                 self.logger.debug('Threshold reached, forwarding message.')
                 message.update(self.add_keys)

intelmq/bots/experts/cymru_whois/expert.py
@@ -2,15 +2,15 @@
 import json
 
 from intelmq.lib.bot import Bot
-from intelmq.lib.cache import Cache
 from intelmq.lib.harmonization import IPAddress
+from intelmq.lib.mixins import CacheMixin
 
 from ._lib import Cymru
 
 CACHE_KEY = "%d_%s"
 
 
-class CymruExpertBot(Bot):
+class CymruExpertBot(Bot, CacheMixin):
     """Add ASN, netmask, AS name, country, registry and allocation time from the Cymru Whois DNS service"""
     overwrite = False
     redis_cache_db: int = 5
@@ -18,14 +18,6 @@ class CymruExpertBot(Bot):
     redis_cache_password: str = None
     redis_cache_port: int = 6379
     redis_cache_ttl: int = 86400
-
-    def init(self):
-        self.cache = Cache(self.redis_cache_host,
-                           self.redis_cache_port,
-                           self.redis_cache_db,
-                           self.redis_cache_ttl,
-                           self.redis_cache_password
-                           )
 
     def process(self):
         event = self.receive_message()
@@ -40,7 +32,7 @@ class CymruExpertBot(Bot):
 
             address = event.get(ip_key)
             cache_key = CACHE_KEY % (IPAddress.version(address), address)
-            result_json = self.cache.get(cache_key)
+            result_json = self.cache_get(cache_key)
 
             if result_json:
                 result = json.loads(result_json)
@@ -50,7 +42,7 @@ class CymruExpertBot(Bot):
                     self.logger.info('Got no result from Cymru for IP address %r.',
                                      address)
                 result_json = json.dumps(result)
-                self.cache.set(cache_key, result_json)
+                self.cache_set(cache_key, result_json)
 
             if not result:
                 continue

intelmq/bots/experts/deduplicator/expert.py
@@ -23,10 +23,10 @@ Parameters:
 """
 
 from intelmq.lib.bot import Bot
-from intelmq.lib.cache import Cache
+from intelmq.lib.mixins import CacheMixin
 
 
-class DeduplicatorExpertBot(Bot):
+class DeduplicatorExpertBot(Bot, CacheMixin):
     """Detection and drop exact duplicate messages. Message hashes are cached in the Redis database"""
     filter_keys: str = "raw,time.observation"  # TODO: could be List[str]
     filter_type: str = "blacklist"
@@ -41,12 +41,6 @@ class DeduplicatorExpertBot(Bot):
     filter_keys = None
 
     def init(self):
-        self.cache = Cache(self.redis_cache_host,
-                           self.redis_cache_port,
-                           self.redis_cache_db,
-                           self.redis_cache_ttl,
-                           self.redis_cache_password
-                           )
         self.filter_keys = {k.strip() for k in
                             self.filter_keys.split(',')}
 
@@ -59,8 +53,8 @@ class DeduplicatorExpertBot(Bot):
             message_hash = message.hash(filter_keys=self.filter_keys,
                                         filter_type=self.filter_type)
 
-            if not self.cache.exists(message_hash):
-                self.cache.set(message_hash, 'hash')
+            if not self.cache_exists(message_hash):
+                self.cache_set(message_hash, 'hash')
                 self.send_message(message)
             else:
                 self.logger.debug('Dropped message.')

intelmq/bots/experts/rdap/expert.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from intelmq.lib.bot import Bot
-from intelmq.lib.cache import Cache
 from intelmq.lib.utils import create_request_session
 from intelmq.lib.exceptions import MissingDependencyError
+from intelmq.lib.mixins import CacheMixin
 
 try:
     import requests
@@ -10,7 +10,7 @@ except ImportError:
     requests = None
 
 
-class RDAPExpertBot(Bot):
+class RDAPExpertBot(Bot, CacheMixin):
     """ Get RDAP data"""
     rdap_order: list = ['abuse', 'technical', 'administrative', 'registrant', 'registrar']
     rdap_bootstrapped_servers: dict = {}
@@ -31,13 +31,6 @@ class RDAPExpertBot(Bot):
 
         self.set_request_parameters()
         self.__session = create_request_session(self)
-
-        self.cache = Cache(self.redis_cache_host,
-                           self.redis_cache_port,
-                           self.redis_cache_db,
-                           self.redis_cache_ttl,
-                           self.redis_cache_password
-                           )
 
         # get overall rdap data from iana
         resp = self.__session.get('https://data.iana.org/rdap/dns.json')
@@ -72,7 +65,7 @@ class RDAPExpertBot(Bot):
         if 'source.fqdn' in event:
             url = event.get('source.fqdn')
             cache_key = "rdap_%s" % (url)
-            result = self.cache.get(cache_key)
+            result = self.cache_get(cache_key)
             if result:
                 event.add('source.abuse_contact', result, overwrite=self.overwrite)
             else:
@@ -141,7 +134,7 @@ class RDAPExpertBot(Bot):
                     for role in self.rdap_order:
                         if role in self.__rdap_order_dict:
                             if self.__rdap_order_dict[role]['email'] is not None:
-                                self.cache.set(cache_key, self.__rdap_order_dict[role]['email'], 86800)
+                                self.cache_set(cache_key, self.__rdap_order_dict[role]['email'], 86800)
                                 event.add('source.abuse_contact', self.__rdap_order_dict[role]['email'], overwrite=self.overwrite)
                                 break
 

intelmq/bots/experts/reverse_dns/expert.py
@@ -7,8 +7,8 @@ import dns.resolver
 import dns.reversename
 
 from intelmq.lib.bot import Bot
-from intelmq.lib.cache import Cache
 from intelmq.lib.harmonization import IPAddress
+from intelmq.lib.mixins import CacheMixin
 
 MINIMUM_BGP_PREFIX_IPV4 = 24
 MINIMUM_BGP_PREFIX_IPV6 = 128
@@ -19,7 +19,7 @@ class InvalidPTRResult(ValueError):
     pass
 
 
-class ReverseDnsExpertBot(Bot):
+class ReverseDnsExpertBot(Bot, CacheMixin):
     """Get the correspondent domain name for source and destination IP address"""
     cache_ttl_invalid_response: int = 60
     overwrite: bool = False
@@ -30,13 +30,6 @@ class ReverseDnsExpertBot(Bot):
     redis_cache_ttl: int = 86400
 
     def init(self):
-        self.cache = Cache(self.redis_cache_host,
-                           self.redis_cache_port,
-                           self.redis_cache_db,
-                           self.redis_cache_ttl,
-                           self.redis_cache_password
-                           )
-
         if not hasattr(self, 'overwrite'):
             self.logger.warning("Parameter 'overwrite' is not given, assuming 'True'. "
                                 "Please set it explicitly, default will change to "
@@ -66,7 +59,7 @@ class ReverseDnsExpertBot(Bot):
                 minimum = MINIMUM_BGP_PREFIX_IPV6
 
             cache_key = bin(ip_integer)[2: minimum + 2]
-            cachevalue = self.cache.get(cache_key)
+            cachevalue = self.cache_get(cache_key)
 
             result = None
             if cachevalue == DNS_EXCEPTION_VALUE:
@@ -87,12 +80,12 @@ class ReverseDnsExpertBot(Bot):
                 except (dns.exception.DNSException, InvalidPTRResult) as e:
                     # Set default TTL for 'DNS query name does not exist' error
                     ttl = None if isinstance(e, dns.resolver.NXDOMAIN) else self.cache_ttl_invalid_response
-                    self.cache.set(cache_key, DNS_EXCEPTION_VALUE, ttl)
+                    self.cache_set(cache_key, DNS_EXCEPTION_VALUE, ttl)
                     result = None
 
                 else:
                     ttl = datetime.fromtimestamp(expiration) - datetime.now()
-                    self.cache.set(cache_key, str(result),
+                    self.cache_set(cache_key, str(result),
                                    ttl=int(ttl.total_seconds()))
 
             if result is not None:

intelmq/bots/experts/ripe/expert.py
@@ -10,8 +10,8 @@ import warnings
 
 import intelmq.lib.utils as utils
 from intelmq.lib.bot import Bot
-from intelmq.lib.cache import Cache
 from intelmq.lib.exceptions import MissingDependencyError
+from intelmq.lib.mixins import CacheMixin
 
 try:
     import requests
@@ -37,7 +37,7 @@ def clean_geo(geo_data):
     return geo_data
 
 
-class RIPEExpertBot(Bot):
+class RIPEExpertBot(Bot, CacheMixin):
     """Fetch abuse contact and/or geolocation information for the source and/or destination IP addresses and/or ASNs of the events"""
     mode: str = "append"
     query_ripe_db_asn: bool = True
@@ -85,19 +85,10 @@ class RIPEExpertBot(Bot):
         }
 
         self.__initialize_http_session()
-        self.__initialize_cache()
 
     def __initialize_http_session(self):
         self.set_request_parameters()
         self.http_session = utils.create_request_session(self)
-
-    def __initialize_cache(self):
-        cache_host = self.redis_cache_host
-        cache_port = self.redis_cache_port
-        cache_db = self.redis_cache_db
-        cache_ttl = self.redis_cache_ttl
-        if cache_host and cache_port and cache_db and cache_ttl:
-            self.__cache = Cache(cache_host, cache_port, cache_db, cache_ttl, self.redis_cache_password)
 
     def process(self):
         event = self.receive_message()
@@ -132,7 +123,7 @@ class RIPEExpertBot(Bot):
         self.acknowledge_message()
 
     def __perform_cached_query(self, type, resource):
-        cached_value = self.__cache.get('{}:{}'.format(type, resource))
+        cached_value = self.cache_get('{}:{}'.format(type, resource))
         if cached_value:
             if cached_value == CACHE_NO_VALUE:
                 return {}
@@ -147,7 +138,7 @@ class RIPEExpertBot(Bot):
                     """ If no abuse contact could be found, a 404 is given. """
                     try:
                         if response.json()['message'].startswith('No abuse contact found for '):
-                            self.__cache.set('{}:{}'.format(type, resource), CACHE_NO_VALUE)
+                            self.cache_set('{}:{}'.format(type, resource), CACHE_NO_VALUE)
                             return {}
                     except ValueError:
                         pass
@@ -164,11 +155,11 @@ class RIPEExpertBot(Bot):
                                   '' % (type, status))
 
                 data = self.REPLY_TO_DATA[type](response_data)
-                self.__cache.set('{}:{}'.format(type, resource),
+                self.cache_set('{}:{}'.format(type, resource),
                                  (json.dumps(list(data) if isinstance(data, set) else data) if data else CACHE_NO_VALUE))
                 return data
             except (KeyError, IndexError):
-                self.__cache.set('{}:{}'.format(type, resource), CACHE_NO_VALUE)
+                self.cache_set('{}:{}'.format(type, resource), CACHE_NO_VALUE)
 
             return {}
 

intelmq/bots/collectors/microsoft/collector_azure.py
@@ -7,7 +7,7 @@ import io
 
 from intelmq.lib.bot import CollectorBot
 from intelmq.lib.exceptions import MissingDependencyError
-from intelmq.lib.cache import Cache
+from intelmq.lib.mixins import CacheMixin
 
 try:
     from azure.storage.blob import ContainerClient
@@ -19,7 +19,7 @@ except ImportError:
     create_configuration = None  # noqa
 
 
-class MicrosoftAzureCollectorBot(CollectorBot):
+class MicrosoftAzureCollectorBot(CollectorBot, CacheMixin):
     "Fetch data blobs from a Microsoft Azure container"
     connection_string: str = "<insert your connection string here>"
     container_name: str = "<insert the container name>"
@@ -42,19 +42,12 @@ class MicrosoftAzureCollectorBot(CollectorBot):
                 'https': self.https_proxy,
             }
 
-        self.cache = Cache(self.redis_cache_host,
-                           self.redis_cache_port,
-                           self.redis_cache_db,
-                           self.redis_cache_ttl,
-                           self.redis_cache_password
-                           )
-
     def process(self):
         container_client = ContainerClient.from_connection_string(conn_str=self.connection_string,
                                                                   container_name=self.container_name,
                                                                   _configuration=self.config)
         for blob in container_client.list_blobs():
-            if self.cache.get(blob.name):
+            if self.cache_get(blob.name):
                 self.logger.debug('Processed file %r already.', blob.name)
                 continue
             self.logger.debug('Processing blob %r.', blob.name)
@@ -64,7 +57,7 @@ class MicrosoftAzureCollectorBot(CollectorBot):
             report = self.new_report()
             report.add('raw', gzip.GzipFile(fileobj=blob_obj).read().decode())
             self.send_message(report)
-            self.cache.set(blob.name, 1)  # Redis-py >= 3.0.0 does not allow True
+            self.cache_set(blob.name, 1)  # Redis-py >= 3.0.0 does not allow True
 
 
 BOT = MicrosoftAzureCollectorBot

intelmq/bots/collectors/microsoft/collector_interflow.py
@@ -36,8 +36,7 @@ import pytz
 from dateutil import parser
 
 from intelmq.lib.bot import CollectorBot
-from intelmq.lib.cache import Cache
-from intelmq.lib.mixins import HttpMixin
+from intelmq.lib.mixins import HttpMixin, CacheMixin
 from intelmq.lib.utils import parse_relative
 from intelmq.lib.exceptions import MissingDependencyError
 
@@ -45,7 +44,7 @@ URL_LIST = 'https://interflow.azure-api.net/file/api/file/listsharedfiles'
 URL_DOWNLOAD = 'https://interflow.azure-api.net/file/api/file/download?fileName=%s'
 
 
-class MicrosoftInterflowCollectorBot(CollectorBot, HttpMixin):
+class MicrosoftInterflowCollectorBot(CollectorBot, HttpMixin, CacheMixin):
     "Fetch data from the Microsoft Interflow API"
     api_key: str = ""
     file_match = None  # TODO type
@@ -92,13 +91,6 @@ class MicrosoftInterflowCollectorBot(CollectorBot, HttpMixin):
         else:
             self.time_match = None
 
-        self.cache = Cache(self.redis_cache_host,
-                           self.redis_cache_port,
-                           self.redis_cache_db,
-                           self.redis_cache_ttl,
-                           self.redis_cache_password
-                           )
-
     def process(self):
         self.check_ttl_time()
         self.logger.debug('Downloading file list.')
@@ -106,7 +98,7 @@ class MicrosoftInterflowCollectorBot(CollectorBot, HttpMixin):
         files.raise_for_status()
         self.logger.debug('Downloaded file list, %s entries.', len(files.json()))
         for file in files.json():
-            if self.cache.get(file['Name']):
+            if self.cache_get(file['Name']):
                 self.logger.debug('Processed file %s already.', file['Name'])
                 continue
             if self.file_match and not self.file_match.match(file['Name']):
@@ -135,7 +127,7 @@ class MicrosoftInterflowCollectorBot(CollectorBot, HttpMixin):
             report.add('raw', raw)
             self.send_message(report)
             # redis-py >= 3.0.0 does no longer support boolean values, cast to string explicitly, also for backwards compatibility
-            self.cache.set(file['Name'], "True")
+            self.cache_set(file['Name'], "True")
 
     def print_filelist(self):
         """ Can be called from the debugger for example. """

commit af60b5837c3d430d865e156ba442ed831916db13

FIX: threshold expert: configure timeout using Redis cache mixin
When the Redis cache functionality was moved to a mixin, the code that
set the cache timeout from the "timeout" parameter was removed, but
the error checking and documentation remained. Change documentation and
configuration checking code to refer to the standard redis_cache_ttl
parameter instead.

Also remove some redundant default values which are now set in the
cache mixin.

Fixes: 2a1b12640947 ("FIX: Use CacheMixins instead of lib.cache")

Signed-off-by: Karl-Johan Karlsson <karl-johan.karlsson@liu.se>

intelmq/bots/experts/threshold/expert.py
@@ -20,6 +20,9 @@ Parameters:
 
     redis_cache_password: string.  default: {None}
 
+    redis_cache_ttl: int, number of seconds to keep counts of similar
+                     messages.
+
     filter_type: string ["whitelist", "blacklist"], when determining
                  whether two messages are similar, consider either
                  only the named fields, or all but the named fields
@@ -34,11 +37,6 @@ Parameters:
     threshold: int, number of messages after which one is sent on. As
                long as the count is above the threshold, no new
                messages will be sent.
-
-    timeout: int, number of seconds to keep counts of similar
-             messages. After this many seconds have elapsed, the count
-             is deleted and "threshold" number of new messages will
-             result in a new message being sent.
 
     add_keys: optional, array of strings to strings, keys to add to
               forwarded messages. Regardless of this setting, the
@@ -59,11 +57,8 @@ class ThresholdExpertBot(ExpertBot, CacheMixin):
     filter_keys: Iterable = ["raw", "time.observation"]
     filter_type: str = "blacklist"
     redis_cache_db: int = 11
-    redis_cache_host: str = "127.0.0.1"  # TODO: could be ipaddress
-    redis_cache_password: Optional[str] = None
-    redis_cache_port: int = 6379
+    redis_cache_ttl: int = 3600
     threshold: int = 100
-    timeout: int = 3600
 
     _message_processed_verb = 'Forwarded'
 
@@ -71,7 +66,7 @@ class ThresholdExpertBot(ExpertBot, CacheMixin):
     bypass = False
 
     def init(self):
-        if self.timeout <= 0:
+        if self.redis_cache_ttl <= 0:
             raise ConfigurationError('Timeout', 'Invalid timeout specified, use positive integer seconds.')
         if self.threshold <= 0:
             raise ConfigurationError('Threshold', 'Invalid threshold specified, use positive integer count.')