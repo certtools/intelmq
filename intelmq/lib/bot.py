a4636b33900e0330afc8a23b5328d24597f873c5
Sebastian Wagner <swagner@intevation.de>
2023-05-09 15:53:30 +0200

    bug: Bot: stop Bot on deletion

--- intelmq/lib/bot.py
+++ intelmq/lib/bot.py
@@ -525,6 +525,9 @@ class Bot:
             self.__handle_sighup()
             remaining = self.rate_limit - (time.time() - starttime)
 
+    def __del__(self):
+        return self.stop(exitcode=0)
+
     def stop(self, exitcode: int = 1):
         if not self.logger:
             print('Could not initialize logger, only logging to stdout.')
