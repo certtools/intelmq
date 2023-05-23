b2753cb9fe6ae15eb569b6d718f54333e476c62d
elsif2 <elsif@shadowserver.org>
2023-05-23 01:12:47 +0000

    Added schema download on startup and additional logging

--- intelmq/bots/parsers/shadowserver/update_schema.py
+++ intelmq/bots/parsers/shadowserver/update_schema.py
@@ -8,5 +8,4 @@ import os
 import intelmq.bots.parsers.shadowserver._config as config
 
 if __name__ == '__main__':  # pragma: no cover
-    exec(open(os.path.join(os.path.dirname(__file__), '../../../version.py')).read())  # defines __version__
-    config.update_schema(__version__)
+    config.update_schema()
